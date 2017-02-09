a0aa60d5b14d5e5986c4faf9f8f4c1ccbad857d2
Ethan Sharabi <ethans@wix.com>
2017-02-09 18:18:21 +0200

    improve connection status bar example screen

--- demo/screens/componentScreens/ConnectionStatusBarScreen.js
+++ demo/screens/componentScreens/ConnectionStatusBarScreen.js
@@ -1,8 +1,8 @@
 import React, {
   Component,
 } from 'react';
-import {View, Text} from 'react-native';
-import {ConnectionStatusBar} from 'react-native-ui-lib';//eslint-disable-line
+import {View, Text, StyleSheet} from 'react-native';
+import {ConnectionStatusBar, Typography, Colors} from 'react-native-ui-lib';//eslint-disable-line
 
 export default class ConnectionStatusBarScreen extends Component {
 
@@ -15,12 +15,24 @@ export default class ConnectionStatusBarScreen extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={styles.container}>
         <ConnectionStatusBar onConnectionChange={isConnected => this.setState({isConnected})} />
-        <Text>
+        <Text style={{textAlign: 'center', marginBottom: 10, ...Typography.text60, color: Colors.dark40}}>
+          Turn your wifi on/off to see the component in action
+        </Text>
+        <Text style={{...Typography.text40}}>
           {this.state.isConnected ? 'Connected! :)' : 'No connection :('}
         </Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 25,
+  },
+});
