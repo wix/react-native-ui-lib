5b1b0b885fcc62f0357095104ea2cbfb33106350
Ethan Sharabi <ethans@wix.com>
2017-02-09 11:22:33 +0200

    add getters to theme manager

--- src/style/themeManager.js
+++ src/style/themeManager.js
@@ -4,17 +4,21 @@ import Colors from './colors';
 class ThemeManager {
 
   theme = {
+    primaryColor: Colors.blue30,
     CTA: {
       textColor: Colors.white,
       backgroundColor: Colors.blue30,
     },
-    primaryColor: Colors.blue30,
     titleColor: Colors.dark10,
     subtitleColor: Colors.dark50,
   };
 
   setTheme(overrides) {
     this.theme = _.merge(this.theme, overrides);
+  }
+
+  get primaryColor() {
+    return this.theme.primaryColor;
   }
 
   get CTATextColor() {
@@ -24,6 +28,15 @@ class ThemeManager {
   get CTABackgroundColor() {
     return this.theme.CTA.backgroundColor;
   }
+
+  get titleColor() {
+    return this.theme.titleColor;
+  }
+
+  get subtitleColor() {
+    return this.theme.subtitleColor;
+  }
+
 }
 
 export default new ThemeManager();
