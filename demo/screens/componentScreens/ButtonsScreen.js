25652dfc4eb87b2319a874957f6099061cf4bcfe
Artal Druk <artald@wix.com>
2017-02-09 11:19:48 +0200

    button examples

--- demo/screens/componentScreens/ButtonsScreen.js
+++ demo/screens/componentScreens/ButtonsScreen.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
-import {Button} from 'react-native-ui-lib';//eslint-disable-line
+import {ScrollView, StyleSheet, Alert} from 'react-native';
+import {Button, Colors} from 'react-native-ui-lib';//eslint-disable-line
+
+const ButtonSpace = 30;
 
 export default class ButtonsScreen extends Component {
 
   render() {
     return (
       <ScrollView contentContainerStyle={styles.container}>
-        <Button label="Button" />
-        <Button label="Disabled Button" disabled />
+        <Button
+          label={'Publish'}
+          onPress={() => Alert.alert('Button #1')}
+          containerStyle={{marginBottom: ButtonSpace}}
+        />
+        <Button
+          label={'This is a button with long text'}
+          onPress={() => Alert.alert('Button #2')}
+          containerStyle={{backgroundColor: Colors.violet50, marginBottom: ButtonSpace}}
+          enableShadow
+        />
+        <Button
+          label={'Go!'}
+          onPress={() => Alert.alert('Button #3')}
+          labelStyle={{fontWeight: '800'}}
+          containerStyle={{marginBottom: ButtonSpace}}
+        />
       </ScrollView>
     );
   }
@@ -17,8 +34,9 @@ export default class ButtonsScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
     padding: 25,
   },
 });
