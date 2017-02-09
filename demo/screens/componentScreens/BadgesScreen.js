9d508bc2ef5deab036ac1d5bc33c747cf62c7b97
Artal Druk <artald@wix.com>
2017-02-09 11:59:42 +0200

    badges demo screen

---
+++ demo/screens/componentScreens/BadgesScreen.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import {Badge, Colors} from 'react-native-ui-lib';//eslint-disable-line
+
+const BadgesSpace = 30;
+
+export default class ButtonsScreen extends Component {
+
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>
+        <Badge label={'1'} containerStyle={{marginRight: BadgesSpace}} />
+        <Badge label={'10'} containerStyle={{marginRight: BadgesSpace}} />
+        <Badge label={'27'} containerStyle={{marginRight: BadgesSpace}} />
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 25,
+  },
+});
--- demo/screens/componentScreens/index.js
+++ demo/screens/componentScreens/index.js
@@ -1,4 +1,6 @@
 import {Navigation} from 'react-native-navigation';
 import ButtonsScreen from './ButtonsScreen';
+import BadgesScreen from './BadgesScreen';
 
 Navigation.registerComponent('example.components.ButtonsScreen', () => ButtonsScreen);
+Navigation.registerComponent('example.components.BadgesScreen', () => BadgesScreen);
