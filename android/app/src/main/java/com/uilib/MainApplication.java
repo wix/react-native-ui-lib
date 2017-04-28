5fdf074c4b4d304a79ef48d0c7e9291be9cff0a3
Lev Vidrak <levv@wix.com>
2017-04-29 00:35:22 +0300

    Fixed Android manual linking

--- android/app/src/main/java/com/uilib/MainApplication.java
+++ android/app/src/main/java/com/uilib/MainApplication.java
@@ -4,6 +4,9 @@ import android.support.annotation.Nullable;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import java.util.List;
+import com.wix.interactable.Interactable;
+import java.util.Arrays;
+
 
 public class MainApplication extends NavigationApplication {
     @Override
@@ -17,6 +20,8 @@ public class MainApplication extends NavigationApplication {
     public List<ReactPackage> createAdditionalReactPackages() {
         // Add the packages you require here.
         // No need to add RnnPackage and MainReactPackage
-        return null;
+        return Arrays.<ReactPackage>asList(
+                new Interactable()
+        );
     }
 }
