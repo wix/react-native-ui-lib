84f5cd83884c4479b9322e19ba6167f5ff29d1ee
Ethan Sharabi <ethans@wix.com>
2017-04-30 10:13:25 +0300

    improve custom handler

--- scripts/customHandler.js
+++ scripts/customHandler.js
@@ -5,78 +5,76 @@ const { utils } = require('react-docgen');
 const { getMemberValuePath, getNameOrValue } = utils;
 const {types: {namedTypes: types}} = recast;
 
-const DEFAULT_NAME = 'UnknownComponent';
+const DEFAULT_NAME = '';
+const PROP_NAME = 'custom';
 
-function getStaticDisplayName(path) {
-  let displayName = null;
-  const staticMember = getMemberValuePath(path, 'displayName');
+function getStaticMemberValue(path) {
+  let staticMemberValue = null;
+  const staticMember = getMemberValuePath(path, PROP_NAME);
   if (staticMember && types.Literal.check(staticMember.node)) {
-    displayName = getNameOrValue(staticMember);
+    staticMemberValue = getNameOrValue(staticMember);
   }
 
-  return displayName || null;
+  return staticMemberValue || null;
 }
 
-function getNodeIdentifier(path) {
-  let displayName = null;
-  if (
-    types.FunctionExpression.check(path.node) ||
-    types.FunctionDeclaration.check(path.node) ||
-    types.ClassExpression.check(path.node) ||
-    types.ClassDeclaration.check(path.node)
-  ) {
-    displayName = getNameOrValue(path.get('id'));
-  }
+// function getNodeIdentifier(path) {
+//   let displayName = null;
+//   if (
+//     types.FunctionExpression.check(path.node) ||
+//     types.FunctionDeclaration.check(path.node) ||
+//     types.ClassExpression.check(path.node) ||
+//     types.ClassDeclaration.check(path.node)
+//   ) {
+//     displayName = getNameOrValue(path.get('id'));
+//   }
 
-  return displayName || null;
-}
+//   return displayName || null;
+// }
 
-function getVariableIdentifier(path) {
-  let displayName = null;
-  let searchPath = path;
+// function getVariableIdentifier(path) {
+//   let displayName = null;
+//   let searchPath = path;
 
-  while (searchPath !== null) {
-    if (types.VariableDeclarator.check(searchPath.node)) {
-      displayName = getNameOrValue(searchPath.get('id'));
-      break;
-    }
-    searchPath = searchPath.parentPath;
-  }
+//   while (searchPath !== null) {
+//     if (types.VariableDeclarator.check(searchPath.node)) {
+//       displayName = getNameOrValue(searchPath.get('id'));
+//       break;
+//     }
+//     searchPath = searchPath.parentPath;
+//   }
 
-  return displayName || null;
-}
+//   return displayName || null;
+// }
 
-function getNameFromFilePath(filePath = '') {
-  let displayName = null;
+// function getNameFromFilePath(filePath = '') {
+//   let displayName = null;
 
-  const filename = path.basename(filePath, path.extname(filePath));
-  if (filename === 'index') {
-    const parts = path.dirname(filePath).split(path.sep);
-    displayName = parts[parts.length - 1];
-  } else {
-    displayName = filename;
-  }
+//   const filename = path.basename(filePath, path.extname(filePath));
+//   if (filename === 'index') {
+//     const parts = path.dirname(filePath).split(path.sep);
+//     displayName = parts[parts.length - 1];
+//   } else {
+//     displayName = filename;
+//   }
 
-  return displayName
-    .charAt(0).toUpperCase()
-    .concat(displayName.slice(1))
-    .replace(/-([a-z])/, (_, match) => match.toUpperCase());
-}
+//   return displayName
+//     .charAt(0).toUpperCase()
+//     .concat(displayName.slice(1))
+//     .replace(/-([a-z])/, (_, match) => match.toUpperCase());
+// }
 
-export function createDisplayNameHandler(filePath) {
+// export function createDisplayNameHandler(filePath) {
+function createCustomStaticMemberHandler() {
   return function displayNameHandler(documentation, path) {
-    let displayName = [
-      getStaticDisplayName,
-      getNodeIdentifier,
-      getVariableIdentifier,
+    const staticMemberValue = [
+      getStaticMemberValue,
+      // getNodeIdentifier,
+      // getVariableIdentifier,
     ].reduce((name, getDisplayName) => name || getDisplayName(path), '');
 
-    if (!displayName) {
-      displayName = getNameFromFilePath(filePath);
-    }
-
-    documentation.set('displayName', displayName || DEFAULT_NAME);
+    documentation.set(PROP_NAME, staticMemberValue || DEFAULT_NAME);
   };
 }
 
-module.exports = createDisplayNameHandler('');
+module.exports = createCustomStaticMemberHandler('');
