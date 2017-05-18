48cfab833278b353753893deb365270e7d52632d
Ethan Sharabi <ethans@wix.com>
2017-05-18 12:44:59 +0300

    remove unused import

--- src/screensComponents/modal/index.js
+++ src/screensComponents/modal/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Modal as RNModal} from 'react-native';
-import {LoaderScreen} from 'react-native-ui-lib';//eslint-disable-line
 import {BaseComponent} from '../../commons';
 import TopBar from './TopBar';
 
