41c7448abea04f768d3f62f9d9023f89ecf64281
Ethan Sharabi <ethans@wix.com>
2017-06-05 11:12:36 +0300

    fix lint issues

--- src/components/inputs/__tests__/TextInput.spec.js
+++ src/components/inputs/__tests__/TextInput.spec.js
@@ -1,17 +1,17 @@
-import uut from '../TextInput';
+import TextInput from '../TextInput';
 
 describe('TextInput', () => {
   it('should shouldFakePlaceholder', () => {
-    let textInput = new uut({});
-    expect(textInput.shouldFakePlaceholder()).toBe(false);
+    let uut = new TextInput({});
+    expect(uut.shouldFakePlaceholder()).toBe(false);
 
-    textInput = new uut({floatingPlaceholder: true});
-    expect(textInput.shouldFakePlaceholder()).toBe(true);
+    uut = new TextInput({floatingPlaceholder: true});
+    expect(uut.shouldFakePlaceholder()).toBe(true);
 
-    textInput = new uut({floatingPlaceholder: true, centered: true});
-    expect(textInput.shouldFakePlaceholder()).toBe(false);
+    uut = new TextInput({floatingPlaceholder: true, centered: true});
+    expect(uut.shouldFakePlaceholder()).toBe(false);
 
-    textInput = new uut({expandable: true});
-    expect(textInput.shouldFakePlaceholder()).toBe(true);
+    uut = new TextInput({expandable: true});
+    expect(uut.shouldFakePlaceholder()).toBe(true);
   });
 });
