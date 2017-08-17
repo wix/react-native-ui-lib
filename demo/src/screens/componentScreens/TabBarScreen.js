173a1186bcaa35116496bda4776521fcafd6b8ba
Ethan Sharabi <ethans@wix.com>
2017-08-17 18:25:52 +0300

    update TabBarScreen example

--- demo/src/screens/componentScreens/TabBarScreen.js
+++ demo/src/screens/componentScreens/TabBarScreen.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import _ from 'lodash';
 import {TabBar, View, Text} from 'react-native-ui-lib'; //eslint-disable-line
 
 export default class TabBarScreen extends Component {
   state = {
     snippet: '',
-  }
+    selectedIndex: 1,
+  };
 
   componentDidMount() {
     const snippet = this.tabbar.getSnippet();
@@ -15,19 +16,27 @@ export default class TabBarScreen extends Component {
   }
 
   render() {
-    const {snippet} = this.state;
+    const {snippet, selectedIndex} = this.state;
 
     return (
       <View flex bg-dark80>
         <View padding-18>
-          <Text text30 dark10>TabBar</Text>
+          <Text text30 dark10>
+            TabBar
+          </Text>
         </View>
 
-        <TabBar selectedIndex={1} ref={element => this.tabbar = element}>
-          <TabBar.Item label="FEED"/>
-          <TabBar.Item label="SERVICES"/>
-          <TabBar.Item label="CHAT"/>
-          <TabBar.Item label="ABOUT"/>
+        <TabBar
+          selectedIndex={selectedIndex}
+          onChangeIndex={index => this.setState({selectedIndex: index})}
+          ref={element => (this.tabbar = element)}
+        >
+          <TabBar.Item label="FEED" />
+          <TabBar.Item label="SERVICES" />
+          <TabBar.Item label="CHAT" />
+          <TabBar.Item>
+            <Text text90 purple30={selectedIndex === 3}>ABOUT</Text>
+          </TabBar.Item>
         </TabBar>
 
         <View padding-12 bg-dark20 marginT-5>
