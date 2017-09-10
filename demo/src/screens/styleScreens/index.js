1ee8e12dd8959131e060ddb9413fb310febc3024
Ethan Sharabi <ethans@wix.com>
2017-09-10 14:54:23 +0300

    register Spacings demo screen

--- demo/src/screens/styleScreens/index.js
+++ demo/src/screens/styleScreens/index.js
@@ -3,8 +3,10 @@ import ColorsScreen from './ColorsScreen';
 import BorderRadiusesScreen from './BorderRadiusesScreen';
 import TypographyScreen from './TypographyScreen';
 import ShadowsScreen from './ShadowsScreen';
+import SpacingsScreen from './SpacingsScreen';
 
 Navigation.registerComponent('unicorn.style.BorderRadiusesScreen', () => BorderRadiusesScreen);
 Navigation.registerComponent('unicorn.style.ColorsScreen', () => ColorsScreen);
 Navigation.registerComponent('unicorn.style.TypographyScreen', () => TypographyScreen);
 Navigation.registerComponent('unicorn.style.ShadowsScreen', () => ShadowsScreen);
+Navigation.registerComponent('unicorn.style.SpacingsScreen', () => SpacingsScreen);
