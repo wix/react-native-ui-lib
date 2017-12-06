70b9e0e41ba321b6b2fd34d346883ddce4e9a97c
Ethan Sharabi <ethans@wix.com>
2017-12-06 13:20:39 +0200

    BorderRadius - fix values for Android

--- src/style/BorderRadiuses.android.js
+++ src/style/BorderRadiuses.android.js
@@ -1,10 +1,11 @@
 const BorderRadiuses = {
   br0: 0,
-  br10: 3,
+  br10: 2,
   br20: 6,
-  br30: 9,
+  br30: 8,
   br40: 12,
-  br50: 15,
+  br50: 16,
+  br60: 20,
   br100: 999,
 };
 
---
+++ src/style/BorderRadiuses.ios.js
@@ -0,0 +1,12 @@
+const BorderRadiuses = {
+  br0: 0,
+  br10: 3,
+  br20: 6,
+  br30: 9,
+  br40: 12,
+  br50: 15,
+  br60: 20,
+  br100: 999,
+};
+
+export default BorderRadiuses;
