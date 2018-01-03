ae180578efcb1e4fb797b3f44451e519b8e16358
Guy Carmeli <guyca@users.noreply.github.com>
2018-01-03 10:11:34 +0200

    Depreacte createJSModules (#53)

--- lib/android/src/main/java/com/wix/reactnativeuilib/textinput/TextInputDelKeyHandlerPackage.java
+++ lib/android/src/main/java/com/wix/reactnativeuilib/textinput/TextInputDelKeyHandlerPackage.java
@@ -16,7 +16,7 @@ public class TextInputDelKeyHandlerPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new TextInputDelKeyHandlerModule(reactContext));
     }
 
-    @Override
+    // Deprecated in RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
