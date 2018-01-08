74258772e0b53af2cb2b37076ef4706665b34885
Yedidya Kennard <yedidyak@wix.com>
2018-01-08 13:44:57 +0200

    Linked android native BlurView package, supports Toast

--- android/app/src/main/java/com/uilib/MainApplication.java
+++ android/app/src/main/java/com/uilib/MainApplication.java
@@ -2,14 +2,13 @@ package com.uilib;
 
 import android.support.annotation.Nullable;
 
+import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
 import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
 
 import java.util.Arrays;
 import java.util.List;
-
-//import com.wix.interactable.Interactable;
 
 public class MainApplication extends NavigationApplication {
     @Override
@@ -21,8 +20,8 @@ public class MainApplication extends NavigationApplication {
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
         return Arrays.<ReactPackage>asList(
-                new TextInputDelKeyHandlerPackage()
-//                new Interactable()
+                new TextInputDelKeyHandlerPackage(),
+                new BlurViewPackage()
         );
     }
 }
