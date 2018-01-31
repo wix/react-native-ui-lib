2649ca1b29856b4e59e5e7f767b00330a969a6cc
inbalwix <33805983+inbalwix@users.noreply.github.com>
2018-01-31 12:16:25 +0200

    Fix for Badge stretch when label value change number of chars (#72)
    * Fix for Badge stretch when label value change number of chars
    
    * fix for PR comments
    
    * Modules version update

--- demo/src/screens/componentScreens/BadgesScreen.js
+++ demo/src/screens/componentScreens/BadgesScreen.js
@@ -1,25 +1,68 @@
 import React, {Component} from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
-import {View, Badge, Colors} from 'react-native-ui-lib';//eslint-disable-line
+import {Colors, View, Badge, Button, Text} from 'react-native-ui-lib';//eslint-disable-line
 
 const BadgesSpace = 30;
 
 export default class BadgesScreen extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: 10,
+    };
+  }
+
+  changeLabelValue(value) {
+    const currValue = this.state.value;
+    const newValue = currValue + value;
+    if (newValue >= 1) {
+      this.setState({ value: newValue});
+    }
+  }
 
   render() {
     return (
       <ScrollView contentContainerStyle={styles.container}>
         <View row>
           <Badge label={'1'} containerStyle={{marginRight: BadgesSpace}} />
-          <Badge label={'10'} containerStyle={{marginRight: BadgesSpace}} backgroundColor={Colors.red40} />
+          <Badge
+            label={this.state.value.toString()}
+            containerStyle={{marginRight: BadgesSpace}}
+            backgroundColor={Colors.red40}
+          />
           <Badge label={'272'} containerStyle={{marginRight: BadgesSpace}} />
         </View>
-        
+
         <View row marginT-20>
           <Badge size="small" label={'1'} containerStyle={{marginRight: BadgesSpace}} />
-          <Badge size="small" label={'10'} containerStyle={{marginRight: BadgesSpace}} backgroundColor={Colors.red40} />
+          <Badge
+            size="small"
+            label={this.state.value.toString()}
+            containerStyle={{marginRight: BadgesSpace}}
+            backgroundColor={Colors.red40}
+          />
           <Badge size="small" label={'272'} containerStyle={{marginRight: BadgesSpace}} />
         </View>
+
+        <View row marginT-50 marginB-15>
+          <Button
+            label={'+'} bg-dark60 text40 dark10 style={{marginRight: 15, width: 40, height: 40}}
+            avoidMinWidth
+            borderRadius={999}
+            onPress={() => this.changeLabelValue(1)}
+            onLongPress={() => this.changeLabelValue(10)}
+          />
+          <Button
+            label={'-'} bg-dark60 text40 dark10 style={{marginRight: BadgesSpace, width: 40, height: 40}}
+            avoidMinWidth
+            borderRadius={999}
+            onPress={() => this.changeLabelValue(-1)}
+            onLongPress={() => this.changeLabelValue(-10)}
+          />
+        </View>
+        <Text center>Press buttons to change red badge value by 1. Long press to change it by 10.</Text>
       </ScrollView>
     );
   }
--- src/components/badge/index.js
+++ src/components/badge/index.js
@@ -46,17 +46,26 @@ export default class Badge extends BaseComponent {
     this.styles = createStyles(this.props);
   }
 
+  getBadgeWidthStyle({label, size}) {
+    const isOneLetter = label.length < 2;
+    const isSmallBadge = size === 'small';
+    const width = isSmallBadge ? (isOneLetter ? 18 : 25) : (isOneLetter ? 21 : 30);
+    return {width};
+  }
+
   render() {
     const {size} = this.props;
     const containerStyle = this.extractContainerStyle(this.props);
     const backgroundStyle = this.props.backgroundColor && {backgroundColor: this.props.backgroundColor};
     const animationProps = this.extractAnimationProps();
     const isSmallBadge = size === 'small';
+    const width = this.getBadgeWidthStyle(this.props);
 
     return (
       <Animatable.View
         testID={this.props.testId}
         style={[
+          width,
           this.styles.badge,
           isSmallBadge && this.styles.badgeSmall,
           containerStyle,
@@ -77,11 +86,9 @@ export default class Badge extends BaseComponent {
   }
 }
 
-function createStyles({label}) {
-  const isOneLetter = label.length < 2;
+function createStyles() {
   return StyleSheet.create({
     badge: {
-      width: isOneLetter ? 21 : 30,
       height: 21,
       borderRadius: BorderRadiuses.br100,
       backgroundColor: ThemeManager.primaryColor,
@@ -89,7 +96,6 @@ function createStyles({label}) {
       justifyContent: 'center',
     },
     badgeSmall: {
-      width: isOneLetter ? 18 : 25,
       height: 18,
     },
     label: {
