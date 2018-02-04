162e68b38e7c37d20521000a239522dc38f76c74
d4vidi <amit.d4vidi@gmail.com>
2018-02-04 17:51:18 +0200

    Add callback-props to Avatar component to bind image loading (#73)

--- demo/src/screens/componentScreens/AvatarsScreen.js
+++ demo/src/screens/componentScreens/AvatarsScreen.js
@@ -9,6 +9,13 @@ const examples = [
   {title: 'Initials (online)', label: 'ES', isOnline: true, status: Avatar.modes.NONE},
   {title: 'Initials with Color', label: 'AD', backgroundColor: Colors.yellow60, labelColor: Colors.orange20, ribbonLabel: 'New', ribbonStyle: {backgroundColor: Colors.purple30}},
   {title: 'Image (online)', imageSource: {uri: 'https://lh3.googleusercontent.com/-cw77lUnOvmI/AAAAAAAAAAI/AAAAAAAAAAA/WMNck32dKbc/s181-c/104220521160525129167.jpg'}, isOnline: true},
+  {
+    title: 'Monitored Avatar (see logs)',
+    backgroundColor: Colors.red60,
+    imageSource: {uri: 'https://static.altomusic.com/media/catalog/product/M/A/MAJ100SBK_0.jpg'},
+    onImageLoadStart: () => console.log('Monitored image load STARTED...'), // eslint-disable-line
+    onImageLoadEnd: () => console.log('Monitored image load ENDED'), // eslint-disable-line
+  },
   {title: 'Smaller', size: 40, imageSource: {uri: 'https://lh3.googleusercontent.com/-CMM0GmT5tiI/AAAAAAAAAAI/AAAAAAAAAAA/-o9gKbC6FVo/s181-c/111308920004613908895.jpg'}},
   {title: 'Bigger', size: 60, backgroundColor: 'red', imageSource: {uri: 'https://lh3.googleusercontent.com/-NQvYunR8V0U/AAAAAAAAAAI/AAAAAAAAAAA/lGLfZ92rPeE/s181-c/108911355181327993622.jpg'}},
   {title: 'Empty Gravatar', backgroundColor: Colors.red60, imageSource: {uri: 'https://gravatar.com/avatar/2497473d558a37020c558bf26e380a7c?d=blank'}},
