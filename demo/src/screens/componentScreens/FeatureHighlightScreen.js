953cf5dae79e1f219c7e2be957d0eec1de66e9d5
inbalwix <33805983+inbalwix@users.noreply.github.com>
2018-02-07 14:30:49 +0200

    Feature highlighter (#78)
    * FeatureHighlight adjustments to guidelines
    
    * adding shape to the highlightFrame prop
    
    * fix PR comments
    
    * fix tests
    
    * fix bug when neither getTarget nor hightlightFrame props are sent to FeatureHighlight
    
    * Opening props for customization ; adding info for Docs
    
    * Fix Android Bug by adding style prop to target view; additional minor fixes

--- demo/src/screens/componentScreens/FeatureHighlightScreen.js
+++ demo/src/screens/componentScreens/FeatureHighlightScreen.js
@@ -20,15 +20,18 @@ class FeatureHighlightScreen extends Component {
     const {showFeatureHighlight} = this.state;
     return (
       <View flex center padding-25>
+        <View marginT-20>
+          <Button label="Keep Reading"/>
+        </View>
         <View>
-          <Text>
+          <Text marginT-20>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
             scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
             electronic typesetting, <Text>remaining</Text> essentially unchanged.
           </Text>
         </View>
-        <View marginT-20 ref={r => (this.target = r)}>
+        <View marginT-20 ref={r => (this.target = r)} style={{opacity: 1}}>
           <Button label="Keep Reading"/>
         </View>
         <FeatureHighlight
@@ -38,7 +41,7 @@ class FeatureHighlightScreen extends Component {
            Tap them to get more information about the most important things that you should pay attention to."
           confirmButtonProps={{label: 'Got It', onPress: this.closeHighlight}}
           getTarget={() => this.target}
-          highlightFrame={{x: 99.5, y: 200, width: 176.5, height: 53}}
+          highlightFrame={{x: 0, y: 200, width: 176.5, height: 53}}
         />
       </View>
     );
