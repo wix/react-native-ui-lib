87c35727cd70b49e2096de339d28ee5aee000db4
Artal Druk <artald@wix.com>
2018-03-08 11:22:34 +0200

    export SafeAreaInsetsManager + update version

--- src/nativeComponents/index.js
+++ src/nativeComponents/index.js
@@ -1,7 +1,9 @@
 import HighlighterOverlayView from './HighlighterOverlayView';
 import SafeAreaSpacerView from './SafeArea/SafeAreaSpacerView';
+import SafeAreaInsetsManager from './SafeArea/SafeAreaInsetsManager';
 
 module.exports = {
   HighlighterOverlayView,
   SafeAreaSpacerView,
+  SafeAreaInsetsManager,
 };
