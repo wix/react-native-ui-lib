0bbd6510e022d4d5caf0348496e399393a5804ad
Ethan Sharabi <ethans@wix.com>
2018-04-15 15:24:20 +0300

    unify border radius files and fix how we extract border radius modifiers

---
+++ src/style/borderRadiuses.js
@@ -0,0 +1,26 @@
+import _ from 'lodash';
+import {Constants} from '../helpers';
+
+class BorderRadiuses {
+  br0 = Constants.isIOS ? 0 : 0;
+  br10 = Constants.isIOS ? 3 : 2;
+  br20 = 6;
+  br30 = Constants.isIOS ? 9 : 8;
+  br40 = 12;
+  br50 = Constants.isIOS ? 15 : 16;
+  br60 = 20;
+  br100 = 999;
+
+  getKeysPattern() {
+    return new RegExp(
+      _.chain(this)
+        .keys()
+        .map(key => [`${key}`])
+        .flatten()
+        .join('|')
+        .value(),
+    );
+  }
+}
+
+export default new BorderRadiuses();
