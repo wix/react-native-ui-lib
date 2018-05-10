c9543a954dba5c79ff15c1a169ca0cf2030cfc8d
Shalom Yerushalmy <yershalom@gmail.com>
2018-05-10 12:36:02 +0300

    Fix release for demo

--- demo/scripts/release.js
+++ demo/scripts/release.js
@@ -32,15 +32,6 @@ function validateEnv() {
   return true;
 }
 
-function setupGit() {
-  execSyncSilently(`git config --global push.default simple`);
-  execSyncSilently(`git config --global user.email "${process.env.GIT_EMAIL}"`);
-  execSyncSilently(`git config --global user.name "${process.env.GIT_USERNAME}"`);
-  const remoteUrl = new RegExp(`https?://(\\S+)`).exec(execSyncRead(`git remote -v`))[1];
-  execSyncSilently(`git remote add deploy "https://${process.env.GIT_USERNAME}:${process.env.GIT_TOKEN}@${remoteUrl}"`);
-  execSync(`git checkout master`);
-}
-
 function calcNewVersion() {
   const latestVersion = execSyncRead(`npm view ${process.env.npm_package_name}@latest version`);
   console.log(`latest version is: ${latestVersion}`);
@@ -69,7 +60,6 @@ function run() {
   if (!validateEnv()) {
     return;
   }
-  setupGit();
   copyNpmRc();
   tagAndPublish(calcNewVersion());
 }
