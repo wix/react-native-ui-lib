89bb40398eb3b0eac212aa7c97396c1b03c627bd
Ethan Sharabi <ethans@wix.com>
2018-06-07 15:01:54 +0300

    clean PlaygroundScreen

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -3,55 +3,19 @@ import {StyleSheet} from 'react-native';
 import {Colors, Constants, View, Text, Button, Modal} from 'react-native-ui-lib'; //eslint-disable-line
 
 export default class PlaygroundScreen extends Component {
-
   static id = 'example.Playground';
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      showModal: false,
-    };
-
-    this.toggleModal = this.toggleModal.bind(this);
+    this.state = {};
   }
 
-  componentDidMount() {
-
-  }
-
-  toggleModal() {
-    const {showModal} = this.state;
-    this.setState({showModal: !showModal});
-  }
-
-  renderModal() {
-    return (
-      <Modal
-        animationType={'slide'}
-        visible
-        onRequestClose={() => this.toggleModal}
-        transparent
-        enableModalBlur
-      >
-        <Modal.TopBar
-          onCancel={this.toggleModal}
-          onDone={this.toggleModal}
-        />
-        <View flex>
-          <Text>MODAL</Text>
-        </View>
-      </Modal>
-    );
-  }
+  componentDidMount() {}
 
   render() {
-    const {showModal} = this.state;
-
     return (
       <View flex center style={styles.container}>
-        <Button onPress={this.toggleModal} label={'Show'}/>
-        {showModal && this.renderModal()}
+        <Text>Playground Screen</Text>
       </View>
     );
   }
