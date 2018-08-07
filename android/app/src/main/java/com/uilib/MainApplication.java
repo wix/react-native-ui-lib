af2d77643f6aa92239ed8e4c64eeef5c01c31dc8
Ethan Sharabi <ethans@wix.com>
2018-08-07 11:51:05 +0300

    this fix the android build

--- android/app/src/main/java/com/uilib/MainApplication.java
+++ android/app/src/main/java/com/uilib/MainApplication.java
@@ -5,7 +5,7 @@ import android.support.annotation.Nullable;
 import com.cmcewen.blurview.BlurViewPackage;
 import com.facebook.react.ReactPackage;
 import com.reactnativenavigation.NavigationApplication;
-import com.wix.reactnativeuilib.BuildConfig;
+// import com.wix.reactnativeuilib.BuildConfig;
 import com.wix.reactnativeuilib.highlighterview.HighlighterViewPackage;
 import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
 import com.wix.reactnativeuilib.wheelpicker.WheelPickerPackage;
