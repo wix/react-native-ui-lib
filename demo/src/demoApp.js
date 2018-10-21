ab19b1d7a9ff4c071334b633a37c5a8a58b5b794
Inbal Tish <inbalti@wix.com>
2018-10-21 14:19:36 +0300

    adding examples back

--- demo/src/demoApp.js
+++ demo/src/demoApp.js
@@ -1,8 +1,40 @@
 import {Navigation} from 'react-native-navigation';
 import * as Animatable from 'react-native-animatable';
-import {Constants, Assets, Colors, Typography} from 'react-native-ui-lib'; //eslint-disable-line
+import {ThemeManager, Constants, Assets, Colors, Typography} from 'react-native-ui-lib'; //eslint-disable-line
 import {registerScreens} from './screens';
 
+
+/** Examples - uncomment when needed */
+// Typography.loadTypographies({
+//   h1: {fontSize: 58, fontWeight: '300', lineHeight: 80},
+//   h2: {fontSize: 46, fontWeight: '300', lineHeight: 64},
+// });
+
+// Colors.loadColors({
+//   pink: '#FF69B4',
+//   gold: '#FFD700',
+// });
+
+// ThemeManager.setTheme({
+//   primaryColor: Colors.purple30,
+//   CTA: {
+//     backgroundColor: Colors.purple30,
+//     textColor: Colors.dark10,
+//   },
+//   titleColor: Colors.blue10,
+//   subtitleColor: Colors.blue40,
+// });
+
+// ThemeManager.setComponentTheme('Picker', (props) => {
+//   if (props.useNativePicker) {
+//     return {
+//       topBarProps: {
+//         doneLabel: Constants.isIOS ? 'Done2' : 'OK2',
+//         cancelLabel: Constants.isIOS ? 'Cancel2' : 'CANCEL2',
+//       },
+//     };
+//   }
+// });
 
 Assets.loadAssetsGroup('icons.general', {
   camera: require('./assets/icons/cameraSelected.png'),
