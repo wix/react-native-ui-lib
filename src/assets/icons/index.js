4c346b41ace250cc84811d059592c727fd849c84
Ethan Sharabi <ethans@wix.com>
2018-11-04 12:12:19 +0200

    add missing icon for the example project, change app main title

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,7 @@
 export const icons = {
   get check() { return require('./check.png'); },
   get checkSmall() { return require('./check-small.png'); },
+  get chevronRight() { return require('./chevronRight.png'); },
   get x() { return require('./x.png'); },
   get search() { return require('./search.png'); },
   get settings() { return require('./settings.png'); },
