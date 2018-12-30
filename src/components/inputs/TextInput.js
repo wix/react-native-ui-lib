63dce0e10ba70d9168e42c6213cd6ea0ad7a763f
Reza <r.ghorbani.f@gmail.com>
2018-12-30 12:50:57 +0330

    fb

--- src/components/inputs/TextInput.js
+++ src/components/inputs/TextInput.js
@@ -129,7 +129,7 @@ export default class TextInput extends BaseInput {
 
   constructor(props) {
     super(props);
-
+    
     console.warn('uilib TextInput component will be deprecated soon, please use TextField instead');
 
     this.updateFloatingPlaceholderState = this.updateFloatingPlaceholderState.bind(this);
