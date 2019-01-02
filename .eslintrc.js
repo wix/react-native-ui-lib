f51c683daa511485bbd778107a0287e001f02dbe
Inbal Tish <inbalti@wix.com>
2019-01-02 12:37:40 +0200

    Fix for depracated components to have second alert in import (unless it's sub-component deprecation or prop deprecation)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,59 @@
 // const validColors = require("./src/style/colorsPalette").colorsPalette;
 // const extraFixColorsMap = require("./src/style/colorsPalette").extraFixColorsMap;
 // const assetsDepJson = require('./eslint-rules/tests/assets_deprecation.json');
+// const deprecationsJson = require('./eslint-rules/tests/component_deprecation.json');
 
 module.exports = {
-  "parser": "babel-eslint",
-  "plugins": ["react-native", "uilib"],
-  "extends": ["airbnb"],
-  "rules": {
-    "arrow-body-style": "off",
-    "class-methods-use-this": "off",
-    "consistent-return": "off",
-    "global-require": "off",
-    "max-len": [2, 130, 4, {"ignoreUrls": true}],
-    "no-nested-ternary": "off",
-    "no-else-return": "off",
-    "no-plusplus": "off",
-    "no-return-assign": "off",
-    "no-trailing-spaces": "off",
-    "no-use-before-define": "off",
-    "no-unneeded-ternary": "off",
-    "object-curly-spacing": "off",
-    "react/forbid-prop-types": "off",
-    "react/jsx-filename-extension": "off",
-    "react/jsx-space-before-closing": "off",
-    "react/jsx-tag-spacing": "off",
-    "react/prefer-stateless-function": "off",
-    "react/require-default-props": "off",
+  'parser': 'babel-eslint',
+  'plugins': ['react-native', 'uilib'],
+  'extends': ['airbnb'],
+  'rules': {
+    'arrow-body-style': 'off',
+    'class-methods-use-this': 'off',
+    'consistent-return': 'off',
+    "comma-dangle": "off",
+    'global-require': 'off',
+    'jsx-quotes': 'off',
+    'max-len': [2, 130, 4, {'ignoreUrls': true}],
+    'no-nested-ternary': 'off',
+    'no-else-return': 'off',
+    'no-param-reassign': ['warn'],
+    'no-plusplus': 'off',
+    'no-return-assign': 'off',
+    'no-trailing-spaces': 'off',
+    'no-use-before-define': 'off',
+    'no-unneeded-ternary': 'off',
+    'object-curly-spacing': 'off',
+    'react/forbid-prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-space-before-closing': 'off',
+    'react/jsx-tag-spacing': 'off',
+    // "react/no-did-mount-set-state": "warn",
+    'react/prefer-stateless-function': 'off',
+    // "react/prop-types": "warn",
+    'react/require-default-props': 'off',
     'react/jsx-no-bind': ['warn', {
       ignoreRefs: true,
       allowArrowFunctions: false,
       allowBind: false,
     }],
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": true, "optionalDependencies": false, "peerDependencies": true}],
-    "import/prefer-default-export": "off",
-    // 'uilib/assets-deprecation': ['error', {deprecations: assetsDepJson, source: '../../assets', dueDate: '2 November, Friday'}]
-    // "uilib/no-hard-coded-color": ["error", validColors, extraFixColorsMap],
-    // "uilib/no-hard-coded-font": "warn"
+    'import/no-extraneous-dependencies': ['warn', {'devDependencies': true, 'optionalDependencies': false, 'peerDependencies': true}],
+    'import/prefer-default-export': 'off',
+    // 'uilib/assets-deprecation': ['error', {deprecations: assetsDepJson, source: '../../assets', dueDate: '2 November, Friday'}],
+    // 'uilib/component-deprecation': ['error', {deprecations: deprecationsJson, dueDate: 'Friday 21 December'}],
+    // 'uilib/no-direct-import': ['error', {origin: 'react-native-ui-lib', destination: 'some-other-source'}],
+    // 'uilib/no-hard-coded-color': ['error', validColors, extraFixColorsMap],
+    // 'uilib/no-hard-coded-font': 'error'
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "jest": true
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".ios.js", ".android.js"]
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.ios.js', '.android.js']
       }
     }
   }
--- eslint-rules/tests/lib/rules/component-deprecation.js
+++ eslint-rules/tests/lib/rules/component-deprecation.js
@@ -14,6 +14,7 @@ const validExample = "const test = <Avatar imageSource={{uri: 'some_uri_string'}
 const validImportExample = "import {Avatar} from 'another-module'; const test = <Avatar url={'some_uri_string'}/>";
 const invalidExample = "import {Avatar} from 'module-with-deprecations'; const test = <Avatar url={'some_uri_string'}/>";
 
+// NOTE: Deprecated components (not prop deprecation) will error at import and again if used as jsx tag.
 ruleTester.run('component-deprecation', rule, {
   valid: [
     {
@@ -47,16 +48,18 @@ ruleTester.run('component-deprecation', rule, {
       output: 'import {Button} from \'module-with-deprecations\'; <Button label="my button"/>',
       errors: [{message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
     },
-
     {
       options: ruleOptions,
       code: 'import {TextInput} from \'module-with-deprecations\'; <TextInput placeholder="first name"/>',
-      errors: [{message: "The 'TextInput' component is deprecated. Please use the 'TextField' component instead"}],
+      errors: [
+        {message: "The 'TextInput' component is deprecated. Please use the 'TextField' component instead."},
+        {message: "The 'TextInput' component is deprecated. Please use the 'TextField' component instead."},
+      ],
     },
     {
       options: ruleOptions,
       code: "import {List} from 'module-with-deprecations'; <List.Part/>",
-      errors: [{message: "The 'List.Part' component is deprecated. Please use the 'List.Item' component instead"}],
+      errors: [{message: "The 'List.Part' component is deprecated. Please use the 'List.Item' component instead."}],
     },
     {
       options: ruleOptions,
