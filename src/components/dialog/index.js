834b2f975e447f49e98d83b4f8993853d72a9b1b
Inbal Tish <inbalti@wix.com>
2019-01-10 12:24:58 +0200

    moving pan gesture to PanGesture component

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -1,13 +1,14 @@
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
-import {StyleSheet, TouchableWithoutFeedback, SafeAreaView, PanResponder, Animated, Easing} from 'react-native';
+import {StyleSheet, TouchableWithoutFeedback, SafeAreaView, Animated, Easing} from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import {Constants} from '../../helpers';
 import {AnimatableManager, Colors} from '../../style';
 import {BaseComponent} from '../../commons';
 import Modal from '../../screensComponents/modal';
 import View from '../view';
+import PanGestureView from '../panGestureView';
 
 
 /*eslint-disable*/
@@ -77,94 +78,16 @@ class Dialog extends BaseComponent {
 
     this.state = {
       alignments: this.state.alignments,
-      deltaY: new Animated.Value(0),
       mainDeltaY: new Animated.Value(this.initialPosition)
     };
 
     if (props.dismissSwipeDirection) {
       console.warn('Dialog component\'s prop \'dismissSwipeDirection\' is deprecated, please remove it');
     }
-
-    this.panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
-      onMoveShouldSetPanResponder: this.handleMoveShouldSetPanResponder,
-      onPanResponderGrant: this.handlePanResponderGrant,
-      onPanResponderMove: this.handlePanResponderMove,
-      onPanResponderRelease: this.handlePanResponderEnd,
-      onPanResponderTerminate: this.handlePanResponderEnd
-    });
   }
 
   generateStyles() {
     this.styles = createStyles(this.props);
-  }
-
-  handleStartShouldSetPanResponder = () => {
-    return true;
-  };
-  handleMoveShouldSetPanResponder = () => {
-    return true;
-  };
-  handlePanResponderGrant = () => {
-    this.swipe = false;
-  };
-  handlePanResponderMove = (e, gestureState) => {
-    const {top} = this.props;
-    const {deltaY} = this.state;
-    let newValue = 0;
-    
-    if (Math.abs(gestureState.vy) >= 1.8) {
-      if ((top && gestureState.vy < 0) || (!top && gestureState.vy > 0)) {
-        // Swipe
-        this.swipe = true;
-      }
-    } else if ((top && gestureState.dy < 0) || (!top && gestureState.dy > 0)) {
-      // Drag
-      newValue = gestureState.dy;
-      Animated.spring(deltaY, {
-        toValue: Math.round(newValue),
-        speed: 20
-      }).start();
-    }
-  };
-  handlePanResponderEnd = () => {
-    if (!this.swipe) {
-      const {top} = this.props;
-      const {deltaY} = this.state;
-      const threshold = this.layout.height / 2;
-      const endValue = Math.round(deltaY._value); // eslint-disable-line
-      
-      if ((top && endValue <= -threshold) || (!top && endValue >= threshold)) {
-        // close
-        this.animateDismiss();
-      } else {
-        // back to initial position
-        Animated.spring(deltaY, {
-          toValue: 0,
-          speed: 20
-        }).start();
-      }
-    } else {
-      // close
-      this.animateDismiss();
-    }
-  };
-
-  animateDismiss() {
-    const {top} = this.props;
-    const {deltaY} = this.state;
-    const newValue = top ? -this.layout.height -this.layout.y - 1 : deltaY._value + (Constants.screenHeight - this.layout.y); // eslint-disable-line
-    
-    Animated.timing(deltaY, {
-      toValue: Math.round(newValue),
-      duration: 280
-    }).start(this.onAnimatedFinished);
-  }
-
-  onAnimatedFinished = ({finished}) => {
-    if (finished) {
-      this.onDismiss();
-    }
   }
 
   onDismiss = () => {
@@ -174,13 +97,8 @@ class Dialog extends BaseComponent {
 
   initPositions() {
     this.setState({
-      deltaY: new Animated.Value(0), 
       mainDeltaY: new Animated.Value(this.initialPosition)
     });
-  }
-
-  onLayout = (event) => {
-    this.layout = event.nativeEvent.layout;
   }
 
   onModalShow = () => {
@@ -211,25 +129,16 @@ class Dialog extends BaseComponent {
   }
 
   renderDraggableContainer() {
-    const {style} = this.getThemeProps();
-    const {deltaY} = this.state;
+    const {style, top} = this.getThemeProps();
 
     return (
-      <Animated.View
-        style={[
-          this.styles.dialogContainer,
-          style,
-          {
-            transform: [{
-              translateY: deltaY
-            }]
-          }
-        ]} 
-        {...this.panResponder.panHandlers}
-        onLayout={this.onLayout}
+      <PanGestureView
+        style={[this.styles.dialogContainer, style]}
+        direction={top && PanGestureView.directions.TOP}
+        onDismiss={this.onDismiss}
       >
         {this.renderContent()}
-      </Animated.View>
+      </PanGestureView>
     );
   }
 
---
+++ src/components/panGestureView/index.js
@@ -0,0 +1,167 @@
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import React from 'react';
+import {PanResponder, Animated} from 'react-native';
+import {Constants} from '../../helpers';
+import {BaseComponent} from '../../commons';
+
+
+const DIRECTIONS = {
+  // VERTICAL
+  TOP: 'top',
+  BOTTOM: 'bottom'
+};
+
+/**
+ * @description: PanGestureView component for drag and swipe gestures (supports only vertival gestures at the moment)
+ */
+export default class PanGestureView extends BaseComponent {
+  static displayName = 'PanGestureView'
+  static propTypes = {
+    /**
+     * onDismiss callback
+     */
+    onDismiss: PropTypes.func,
+    /**
+     * The direction of the allowed pan (default is bottom)
+     */
+    direction: PropTypes.oneOf(Object.values(DIRECTIONS))
+  };
+
+  static defaultProps = {
+    direction: DIRECTIONS.BOTTOM
+  };
+  
+  static directions = DIRECTIONS;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      deltaY: new Animated.Value(0)
+    };
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: this.handleStartShouldSetPanResponder,
+      onMoveShouldSetPanResponder: this.handleMoveShouldSetPanResponder,
+      onPanResponderGrant: this.handlePanResponderGrant,
+      onPanResponderMove: this.handlePanResponderMove,
+      onPanResponderRelease: this.handlePanResponderEnd,
+      onPanResponderTerminate: this.handlePanResponderEnd
+    });
+  }
+
+  handleStartShouldSetPanResponder = () => {
+    return true;
+  };
+  handleMoveShouldSetPanResponder = () => {
+    return true;
+  };
+  handlePanResponderGrant = () => {
+    this.swipe = false;
+  };
+  handlePanResponderMove = (e, gestureState) => {
+    const {direction} = this.props;
+    let newValue = 0;
+    
+    // VERTICAL
+    const {deltaY} = this.state;
+
+    if (Math.abs(gestureState.vy) >= 1.8) {
+      if ((direction.TOP && gestureState.vy < 0) || (!direction.TOP && gestureState.vy > 0)) {
+        // Swipe
+        this.swipe = true;
+      }
+    } else if ((direction.TOP && gestureState.dy < 0) || (!direction.TOP && gestureState.dy > 0)) {
+      // Drag
+      newValue = gestureState.dy;
+      Animated.spring(deltaY, {
+        toValue: Math.round(newValue),
+        speed: 20
+      }).start();
+    }
+  };
+  handlePanResponderEnd = () => {
+    if (!this.swipe) {
+      const {direction} = this.props;
+
+      // VERTICAL
+      const {deltaY} = this.state;
+      const threshold = this.layout.height / 2;
+      const endValue = Math.round(deltaY._value); // eslint-disable-line
+      
+      if ((direction.TOP && endValue <= -threshold) || (!direction.TOP && endValue >= threshold)) {
+        // close
+        this.animateDismiss();
+      } else {
+        // back to initial position
+        Animated.spring(deltaY, {
+          toValue: 0,
+          speed: 20
+        }).start();
+      }
+    } else {
+      // close
+      this.animateDismiss();
+    }
+  };
+
+  animateDismiss() {
+    const {direction} = this.props;
+
+    // VERTICAL
+    const {deltaY} = this.state;
+    const newValue = direction.TOP ? -this.layout.height -this.layout.y - 1 : deltaY._value + (Constants.screenHeight - this.layout.y); // eslint-disable-line
+
+    Animated.timing(deltaY, {
+      toValue: Math.round(newValue),
+      duration: 280
+    }).start(this.onAnimatedFinished);
+  }
+
+  onAnimatedFinished = ({finished}) => {
+    if (finished) {
+      this.onDismiss();
+    }
+  }
+
+  onDismiss = () => {
+    this.initPositions();
+    _.invoke(this.props, 'onDismiss');
+  }
+
+  initPositions() {
+    this.setState({
+      deltaY: new Animated.Value(0)
+    });
+  }
+
+  onLayout = (event) => {
+    this.layout = event.nativeEvent.layout;
+  }
+
+  render() {
+    const {style} = this.getThemeProps();
+
+    // VERTICAL
+    const {deltaY} = this.state;
+
+    return (
+      <Animated.View
+        style={[
+          style,
+          {
+            transform: [{
+              translateY: deltaY
+            }]
+          }
+        ]} 
+        {...this.panResponder.panHandlers}
+        onLayout={this.onLayout}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+
+}
