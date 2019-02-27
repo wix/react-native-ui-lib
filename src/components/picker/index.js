ecbf341b4bb99b2bd8fa648b825a6d78da79c1f0
Inbal Tish <inbalti@wix.com>
2019-02-27 16:22:27 +0200

    render TextField instead of Text as an extendable-input

--- src/components/picker/index.js
+++ src/components/picker/index.js
@@ -1,20 +1,16 @@
-// TODO: depreacte value allowing passing an object, allow only string or number
-// TODO: extract picker labels from children in order to obtain the
-// correct label to render (similar to what we do in NativePicker)
-// TODO: simplify this component, stop inherit from TextField
+// TODO: deprecate value allowing passing an object, allow only string or number
+// TODO: extract picker labels from children in order to obtain the correct label to render (similar to NativePicker)
 
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {Constants} from '../../helpers';
 import {Colors, Typography} from '../../style';
 import {BaseComponent} from '../../commons';
 import TouchableOpacity from '../../components/touchableOpacity';
 import View from '../../components/view';
 import Modal from '../../screensComponents/modal';
 import Image from '../../components/image';
-import Text from '../../components/text';
 import Button from '../../components/button';
 import {TextField} from '../inputs';
 import * as PickerPresenter from './PickerPresenter';
@@ -27,8 +23,10 @@ const PICKER_MODES = {
   SINGLE: 'SINGLE',
   MULTI: 'MULTI',
 };
-
-const ItemType = PropTypes.shape({value: PropTypes.any, label: PropTypes.string});
+const ItemType = PropTypes.shape({
+  value: PropTypes.any, 
+  label: PropTypes.string
+});
 
 /**
  * @description: Picker Component, support single or multiple selection, blurModel and nativePicker
@@ -37,7 +35,6 @@ const ItemType = PropTypes.shape({value: PropTypes.any, label: PropTypes.string}
  */
 class Picker extends BaseComponent {
   static displayName = 'Picker';
-  static modes = PICKER_MODES;
   static propTypes = {
     ...TextField.propTypes,
     /**
@@ -53,7 +50,7 @@ class Picker extends BaseComponent {
      */
     mode: PropTypes.oneOf(Object.keys(PICKER_MODES)),
     /**
-     * Adds blur effect to picker modal (only iOS)
+     * Adds blur effect to picker modal (iOS only)
      */
     enableModalBlur: PropTypes.bool,
     /**
@@ -73,7 +70,7 @@ class Picker extends BaseComponent {
      */
     onPress: PropTypes.func,
     /**
-     * A function that extract the unique value out of the value prop in case value has a custom structure.
+     * A function that extract the unique value out of the value prop in case value has a custom structure
      */
     getItemValue: PropTypes.func,
     /**
@@ -101,15 +98,15 @@ class Picker extends BaseComponent {
      */
     searchPlaceholder: PropTypes.string,
     /**
-     * callback for picker modal search input text change
+     * callback for picker modal search input text change (only when passing showSearch)
      */
     onSearchChange: PropTypes.func,
     /**
-     * Render custom search input
+     * Render custom search input (only when passing showSearch)
      */
     renderCustomSearch: PropTypes.func,
     /**
-     * Allow to use the native picker solution (different for iOS and Android)
+     * Allow to use the native picker solution (different style for iOS and Android)
      */
     useNativePicker: PropTypes.bool,
     /**
@@ -117,22 +114,21 @@ class Picker extends BaseComponent {
      */
     renderNativePicker: PropTypes.func,
     /**
-     * Icon asset source for showing on the right side, appropriate for dropdown icon and such
+     * Icon asset source for showing on the right side, appropriate for drop-down icon and such
      */
     rightIconSource: PropTypes.oneOfType([PropTypes.object, PropTypes.number]),
     /**
      * Pass props to the list component that wraps the picker options (allows to control FlatList behavior)
      */
-    listProps: PropTypes.object,
+    listProps: PropTypes.object
   };
 
   static defaultProps = {
     ...TextField.defaultProps,
-    // expandable: true,
-    // text70: true,
-    // enableErrors: false,
     mode: PICKER_MODES.SINGLE
   };
+
+  static modes = PICKER_MODES;
 
   constructor(props) {
     super(props);
@@ -209,7 +205,6 @@ class Picker extends BaseComponent {
 
   onDoneSelecting = (item) => {
     this.setState({searchValue: '', value: item}); // clean search when done selecting
-    // this.onChangeText(item);
     this.toggleExpandableModal(false);
     _.invoke(this.props, 'onChange', item);
   }
@@ -252,29 +247,22 @@ class Picker extends BaseComponent {
   }
 
   renderExpandableInput() {
-    const {value} = this.state;
-    const {placeholder, rightIconSource, style} = this.props;
-    const typography = this.getTypography();
-    const color = this.extractColorValue() || Colors.dark10;
+    const {rightIconSource} = this.props;
+    const textInputProps = TextField.extractOwnProps(this.props, ['rightIconSource', 'hideUnderline', 'underlineColor']);
     const label = this.getLabel();
-    const shouldShowPlaceholder = _.isEmpty(value);
 
     return (
       <TouchableOpacity style={this.styles.pickerInputWrapper} activeOpacity={1} onPress={this.handlePickerOnPress}>
-        <Text
-          style={[
-            this.styles.input,
-            typography,
-            {color},
-            style,
-            {height: Constants.isAndroid ? typography.lineHeight : undefined},
-            shouldShowPlaceholder && this.styles.placeholder,
-          ]}
-          numberOfLines={3}
-        >
-          {shouldShowPlaceholder ? placeholder : label}
-        </Text>
-        {rightIconSource && <Image source={rightIconSource}/>}
+        <TextField
+          {...textInputProps}
+          hideUnderline
+          enableErrors={false}
+          expandable={false}
+          value={label}
+          editable={false}
+          pointerEvents={'none'}
+        />
+        {rightIconSource && <Image source={rightIconSource} style={{marginLeft: 16}}/>}
       </TouchableOpacity>
     );
   }
@@ -333,23 +321,14 @@ class Picker extends BaseComponent {
   }
 }
 
-function createStyles({placeholderTextColor, centered}) {
+function createStyles({hideUnderline, underlineColor}) {
   return StyleSheet.create({
     pickerInputWrapper: {
-      flex: 1,
       flexDirection: 'row',
       alignItems: 'center',
-    },
-    input: {
-      flexGrow: 1,
-      marginBottom: Constants.isIOS ? 10 : 5,
-      padding: 0,
-      textAlign: centered ? 'center' : undefined,
-      backgroundColor: 'transparent',
-    },
-    placeholder: {
-      color: placeholderTextColor,
-    },
+      borderBottomWidth: hideUnderline ? 0 : 1,
+      borderBottomColor: underlineColor ? underlineColor.default : Colors.dark70
+    }
   });
 }
 
