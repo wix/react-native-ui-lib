084139670b91af50cca75e665d1840b1cf3a282a
Inbal Tish <inbalti@wix.com>
2019-03-27 14:17:57 +0200

    Removing deprecated  `animationProps` usage in demo

--- demo/src/screens/listScreens/BasicListScreen.js
+++ demo/src/screens/listScreens/BasicListScreen.js
@@ -24,31 +24,32 @@ export default class BasicListScreen extends Component {
     const imageAnimationProps = AnimatableManager.getRandomDelay();
 
     return (
-      <ListItem
-        activeBackgroundColor={Colors.dark60}
-        activeOpacity={0.3}
-        height={77.5}
-        onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
-        {...animationProps}
-      >
-        <ListItem.Part left>
-          <Animatable.Image
-            source={{uri: row.mediaUrl}}
-            style={styles.image}
-            {...imageAnimationProps}
-          />
-        </ListItem.Part>
-        <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
-          <ListItem.Part containerStyle={{marginBottom: 3}}>
-            <Text dark10 text70 style={{flex: 1, marginRight: 10}} numberOfLines={1}>{row.name}</Text>
-            <Text dark10 text70 style={{marginTop: 2}}>{row.formattedPrice}</Text>
+      <Animatable.View {...animationProps}>
+        <ListItem
+          activeBackgroundColor={Colors.dark60}
+          activeOpacity={0.3}
+          height={77.5}
+          onPress={() => Alert.alert(`pressed on order #${id + 1}`)}
+        >
+          <ListItem.Part left>
+            <Animatable.Image
+              source={{uri: row.mediaUrl}}
+              style={styles.image}
+              {...imageAnimationProps}
+            />
           </ListItem.Part>
-          <ListItem.Part>
-            <Text style={{flex: 1, marginRight: 10}} text90 dark40 numberOfLines={1}>{`${row.inventory.quantity} item`}</Text>
-            <Text text90 color={statusColor} numberOfLines={1}>{row.inventory.status}</Text>
+          <ListItem.Part middle column containerStyle={[styles.border, {paddingRight: 17}]}>
+            <ListItem.Part containerStyle={{marginBottom: 3}}>
+              <Text dark10 text70 style={{flex: 1, marginRight: 10}} numberOfLines={1}>{row.name}</Text>
+              <Text dark10 text70 style={{marginTop: 2}}>{row.formattedPrice}</Text>
+            </ListItem.Part>
+            <ListItem.Part>
+              <Text style={{flex: 1, marginRight: 10}} text90 dark40 numberOfLines={1}>{`${row.inventory.quantity} item`}</Text>
+              <Text text90 color={statusColor} numberOfLines={1}>{row.inventory.status}</Text>
+            </ListItem.Part>
           </ListItem.Part>
-        </ListItem.Part>
-      </ListItem>
+        </ListItem>
+      </Animatable.View>
     );
   }
 
--- demo/src/screens/listScreens/ContactsListScreen.js
+++ demo/src/screens/listScreens/ContactsListScreen.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {StyleSheet, Alert, FlatList} from 'react-native';
+import {View as AnimatableView} from 'react-native-animatable';
 import {AnimatableManager, Colors, ThemeManager, ListItem, Text, Avatar, AvatarHelper} from 'react-native-ui-lib'; //eslint-disable-line
 import conversations from '../../data/conversations';
 
@@ -22,23 +23,24 @@ export default class ContactsListScreen extends Component {
     const animationProps = AnimatableManager.getEntranceByIndex(id);
 
     return (
-      <ListItem
-        key={id}
-        onPress={() => Alert.alert(`pressed on contact #${id + 1}`)}
-        {...animationProps}
-      >
-        <ListItem.Part left>
-          <Avatar
-            imageSource={row.thumbnail ? {uri: row.thumbnail} : null}
-            label={initials}
-            badgeProps={{backgroundColor: Number(id) % 3 === 0 ? Colors.green30 : undefined}}
-            containerStyle={{marginHorizontal: 18}}
-          />
-        </ListItem.Part>
-        <ListItem.Part middle containerStyle={styles.border}>
-          <Text text70>{row.name}</Text>
-        </ListItem.Part>
-      </ListItem>
+      <AnimatableView {...animationProps}>
+        <ListItem
+          key={id}
+          onPress={() => Alert.alert(`pressed on contact #${id + 1}`)}
+        >
+          <ListItem.Part left>
+            <Avatar
+              imageSource={row.thumbnail ? {uri: row.thumbnail} : null}
+              label={initials}
+              badgeProps={{backgroundColor: Number(id) % 3 === 0 ? Colors.green30 : undefined}}
+              containerStyle={{marginHorizontal: 18}}
+            />
+          </ListItem.Part>
+          <ListItem.Part middle containerStyle={styles.border}>
+            <Text text70>{row.name}</Text>
+          </ListItem.Part>
+        </ListItem>
+      </AnimatableView>
     );
   }
 
