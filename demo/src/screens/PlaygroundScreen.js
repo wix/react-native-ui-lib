6f393238aad63ab07507cc6bff0dd925533f2add
Ethan Sharabi <ethan.shar@gmail.com>
2019-03-29 16:17:37 +0300

    update example and create a POC for shared element

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-import {StyleSheet, YellowBox} from 'react-native';
-import {Colors, Button, View, Text, Constants} from 'react-native-ui-lib'; //eslint-disable-line
+import {StyleSheet, YellowBox, processColor} from 'react-native';
+import {Colors, Button, View, Text, Constants, TouchableOpacity} from 'react-native-ui-lib'; //eslint-disable-line
 import Animated, {Easing} from 'react-native-reanimated';
+import {TapGestureHandler, State} from 'react-native-gesture-handler';
 import _ from 'lodash';
 
 YellowBox.ignoreWarnings([
@@ -10,16 +11,34 @@ YellowBox.ignoreWarnings([
   'Require cycle:', // doesn't work
 ]);
 
-const {Clock, Value, set, cond, startClock, clockRunning, timing, debug, stopClock, block} = Animated;
+const {
+  Clock,
+  Value,
+  set,
+  cond,
+  eq,
+  startClock,
+  clockRunning,
+  timing,
+  debug,
+  stopClock,
+  block,
+  event,
+  interpolate,
+} = Animated;
 
 export default class PlaygroundScreen extends Component {
+  state = {
+    show: false,
+  };
+
   componentDidMount() {
     // this.slow();
   }
 
   slow() {
     setTimeout(() => {
-      _.times(10000, () => {
+      _.times(1000, () => {
         console.log('ethan - slow logg');
       });
 
@@ -79,19 +98,136 @@ export default class PlaygroundScreen extends Component {
     return (
       <View center style={styles.container}>
         <Animated.View style={[styles.box, {width: this.width}]} />
-        <Button label="press me" activeBackgroundColor={'red'} />
-        <NativeButton/>
+        <Button marginV-20 label="JS BUTTON" activeBackgroundColor={'red'} />
+
+        <NativeButton />
+        <TouchableOpacity onPress={() => this.setState({show: !this.state.show})}>
+          <SharedElement
+            show={this.state.show}
+            startLayout={{x: 100, y: 300, width: 50, height: 60}}
+            endLayout={{x: 120, y: 340, width: 250, height: 160}}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+class SharedElement extends Component {
+  clock = new Clock();
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.show !== this.props.show) {
+      const values = this.props.show ? [0, 100] : [100, 0];
+      this._transition = this.runTiming(this.clock, ...values);
+    }
+  }
+
+  getAnimatedStyle() {
+    if (!this._transition) {
+      return undefined;
+    }
+    const {startLayout, endLayout} = this.props;
+    return {
+      position: 'absolute',
+      top: interpolate(this._transition, {
+        inputRange: [0, 100],
+        outputRange: [startLayout.y, endLayout.y],
+      }),
+      left: interpolate(this._transition, {
+        inputRange: [0, 100],
+        outputRange: [startLayout.x, endLayout.x],
+      }),
+      height: interpolate(this._transition, {
+        inputRange: [0, 100],
+        outputRange: [startLayout.height, endLayout.height],
+      }),
+      width: interpolate(this._transition, {
+        inputRange: [0, 100],
+        outputRange: [startLayout.width, endLayout.width],
+      }),
+    };
+  }
+
+  runTiming(clock, value, dest) {
+    const state = {
+      finished: new Value(0),
+      position: new Value(0),
+      time: new Value(0),
+      frameTime: new Value(0),
+    };
+
+    const config = {
+      duration: 300,
+      toValue: new Value(0),
+      easing: Easing.inOut(Easing.ease),
+      useNativeDriver: true,
+    };
+
+    return block([
+      cond(
+        clockRunning(clock),
+        [
+          // if the clock is already running we update the toValue, in case a new dest has been passed in
+          set(config.toValue, dest),
+        ],
+        [
+          // if the clock isn't running we reset all the animation params and start the clock
+          set(state.finished, 0),
+          set(state.time, 0),
+          set(state.position, value),
+          set(state.frameTime, 0),
+          set(config.toValue, dest),
+          startClock(clock),
+        ],
+      ),
+      // we run the step here that is going to update position
+      timing(clock, state, config),
+      // if the animation is over we stop the clock
+      cond(state.finished, debug('stop clock', stopClock(clock))),
+      // we made the block return the updated position
+      state.position,
+    ]);
+  }
+
+  render() {
+    return <Animated.View style={[{backgroundColor: 'black', width: 100, height: 100}, this.getAnimatedStyle()]} />;
+  }
+}
+
 class NativeButton extends Component {
+  constructor(props) {
+    super(props);
+
+    const state = new Value(-1);
+
+    this._onGestureEvent = event([{nativeEvent: {state}}], {listener: this.onPress});
+
+    this.onStateChange = event(
+      [
+        {
+          nativeEvent: {state},
+        },
+      ],
+      {listener: this.onPress},
+    );
+
+    this._color = cond(eq(state, State.BEGAN), processColor(Colors.orange40), processColor(Colors.blue30));
+
+    this.onPress = () => {
+      console.warn('press native button');
+    };
+  }
+
   render() {
     return (
-      <View>
-        <Text>NATIVE BUTTOn</Text>
-      </View>
+      <TapGestureHandler onHandlerStateChange={this.onStateChange} onGestureEvent={this.onPress}>
+        <Animated.View style={[styles.button, {backgroundColor: this._color}]}>
+          <Text white text70>
+            NATIVE BUTTON
+          </Text>
+        </Animated.View>
+      </TapGestureHandler>
     );
   }
 }
@@ -101,6 +237,11 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     backgroundColor: 'red',
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 999,
   },
   container: {},
 });
