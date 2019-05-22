a9c9d3342f5104474ed93ac52bdbfd1e6a9dd1fa
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2019-05-22 08:53:58 +0300

    Add testID to ActionSheet (#444)
    * Add testID to ActionSheet
    
    * Move testID from renderContent to renderAnimationContainer

--- src/components/dialog/index.js
+++ src/components/dialog/index.js
@@ -167,7 +167,7 @@ class Dialog extends BaseComponent {
   }
 
   renderAnimationContainer() {
-    const {animationConfig, top} = this.getThemeProps();
+    const {animationConfig, top, testID} = this.getThemeProps();
     const {alignments, deltaY} = this.state;
     const centerByDefault = _.isEmpty(alignments);
     const hasCustomAnimation = (animationConfig && animationConfig.animation);
@@ -176,7 +176,8 @@ class Dialog extends BaseComponent {
     const animation = hasCustomAnimation ? Object.assign(defaultAnimation, animationConfig) : {};
 
     return (
-      <Container 
+      <Container
+        testID={testID}
         style={[
           this.styles.overlay,
           {...alignments},
