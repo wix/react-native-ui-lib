14c17e0e1d2bf0c53beb2b3df6950abb40ab0552
Miki Leib <michaelle@wix.com>
2019-06-26 10:41:13 +0300

    Revert changes to PlaygroundScreen

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,45 +1,26 @@
 import React, {Component} from 'react';
-import {TouchableOpacity, ScrollView, Platform, StyleSheet} from 'react-native';
-import {Colors, View, Text, PanListenerView} from 'react-native-ui-lib'; //eslint-disable-line
+import {StyleSheet} from 'react-native';
+import {Colors, View, Text} from 'react-native-ui-lib'; //eslint-disable-line
 
 export default class PlaygroundScreen extends Component {
+  
   constructor(props) {
     super(props);
-    this.state = {
-      locationText: '',
-    };
+    this.state = {};
   }
 
   componentDidMount() {}
 
-  onDragListener = (dragDirections) => {
-    this.setState({locationText: `Dragged: ${dragDirections}`});
-  };
-
-  onSwipeListener = (swipeDirections) => {
-    this.setState({locationText: `Swiped: ${swipeDirections}`});
-  };
-
   render() {
-    const {locationText} = this.state;
-
     return (
       <View flex bg-dark80 center style={styles.container}>
         <Text>Unicorn Playground Screen</Text>
-        <PanListenerView
-          bg-red30
-          height={300}
-          width="100%"
-          // directions={[PanListenerView.directions.UP, PanListenerView.directions.DOWN]}
-          onDragListener={this.onDragListener}
-          onSwipeListener={this.onSwipeListener}
-        />
-        <Text>{locationText}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+  }
 });
