d310e3c542d2b21e79ca7a3f48b25a8416db84ce
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2019-07-28 10:52:24 +0300

    Add documentation part 1 (#487)

--- src/components/progressBar/index.js
+++ src/components/progressBar/index.js
@@ -10,6 +10,8 @@ import {Colors} from '../../style';
  * @description: Animated progress bar
  * @gif:https://media.giphy.com/media/3o752o08oY0oCvOxR6/giphy.gif
  * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/animationScreens/ProgressBarScreen.js
+ * @extends: AnimatedScanner
+ * @extendsLink: https://wix-private.github.io/wix-react-native-ui-lib/docs/public/AnimatedScanner/
  */
 export default class ProgressBar extends BaseComponent {
   static displayName = 'ProgressBar';
