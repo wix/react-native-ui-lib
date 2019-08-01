b4550f70910d6397aed787bfaba9722bc9f26090
Inbal Tish <inbalti@wix.com>
2019-08-01 16:28:33 +0300

    updating docs comments

--- src/screensComponents/loaderScreen/index.js
+++ src/screensComponents/loaderScreen/index.js
@@ -11,8 +11,6 @@ import Text from '../../components/text';
 
 /**
  * @description: Component that shows a full screen with an activity indicator
- * @extends: Animatable.View
- * @gif: https://media.giphy.com/media/3o75212iau1oK8hznG/giphy.gif
  * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreenScreens/LoadingScreen.js
  */
 export default class LoaderScreen extends BaseComponent {
