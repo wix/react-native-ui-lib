f50e2234b5694abb520223db955aa93f987feb9d
Miki Leib <michaelle@wix.com>
2019-10-23 10:17:15 +0300

    Add missing testIDs

--- demo/src/screens/MainScreen.js
+++ demo/src/screens/MainScreen.js
@@ -44,6 +44,7 @@ export default class MainScreen extends Component {
     const navigationStyle = this.getSearchNavigationStyle();
     navigationStyle.topBar.rightButtons.push({
       id: 'uilib.settingsButton',
+      testID: 'uilib.settingsButton',
       enabled: true,
       icon: Assets.icons.settings,
     });
@@ -73,6 +74,7 @@ export default class MainScreen extends Component {
         rightButtons: [
           {
             id: 'uilib.searchButton',
+            testID: 'uilib.searchButton',
             enabled: true,
             icon: Assets.icons.search,
           },
@@ -199,7 +201,7 @@ export default class MainScreen extends Component {
         <TextField
           ref={r => (this.input = r)}
           value={this.state.filterText}
-          placeholder="Search your component.."
+          placeholder="Search for your component..."
           onChangeText={this.filterExplorerScreens}
           onBlur={this.onSearchBoxBlur}
           style={{
@@ -211,6 +213,7 @@ export default class MainScreen extends Component {
           hideUnderline
         />
         <Button
+          testID={'SearchButton'}
           style={{marginRight: 16, marginTop: Constants.isIOS ? Constants.statusBarHeight : 0}}
           iconSource={Assets.icons.search}
           size={'small'}
