d493295227ec5e60e9a9e7a9936d9a8c122db06b
Ethan Sharabi <ethans@wix.com>
2019-11-05 12:22:55 +0200

    wip - make android build works

--- android/app/src/main/java/com/rnuilib/MainActivity.java
+++ android/app/src/main/java/com/rnuilib/MainActivity.java
@@ -1,30 +1,22 @@
 package com.rnuilib;
 
-
-import com.facebook.react.ReactActivity;
+//import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends NavigationActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "rnuilib";
-  }
-}
+public class MainActivity extends NavigationActivity {}
 
-// DEFAULT RN CODE
-// public class MainActivity extends ReactActivity {
+//public class MainActivity extends ReactActivity {
+//
+//  /**
+//   * Returns the name of the main component registered from JavaScript. This is used to schedule
+//   * rendering of the component.
+//   */
+//  @Override
+//  protected String getMainComponentName() {
+//    return "rnuilib";
+//  }
+//}
 
-//   /**
-//    * Returns the name of the main component registered from JavaScript. This is used to schedule
-//    * rendering of the component.
-//    */
-//   @Override
-//   protected String getMainComponentName() {
-//     return "rnuilib";
-//   }
-// }
+
+
