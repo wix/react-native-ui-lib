20afdbb0f6d555b8ebd77207b96be00d3ce3c036
Ethan Sharabi <ethans@wix.com>
2019-11-18 11:22:10 +0200

    WIP - add the ability to control activeScale

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,21 +1,45 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import {View, Text} from 'react-native-ui-lib'; //eslint-disable-line
-
+import {View, Text, Colors, Incubator} from 'react-native-ui-lib'; //eslint-disable-line
 
 export default class PlaygroundScreen extends Component {
+  componentDidMount() {
+    // this.slow(10);
+  }
+
+  slow(iterations = 10) {
+    if (iterations === 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      _.times(5000, () => {
+        console.log('slow log');
+      });
+
+      this.slow(iterations - 1);
+    }, 10);
+  }
 
   render() {
     return (
-      <View center bg-dark80 flex>
-        <Text text50>Playground Screen</Text>
+      <View bg-dark80 flex center>
+        <Incubator.TouchableOpacity
+          onPress={() => console.warn('ethan - presss')}
+          backgroundColor={Colors.blue40}
+          feedbackColor={Colors.blue20}
+          activeScale={0.95}
+          style={{paddingHorizontal: 20, paddingVertical: 8, borderRadius: 20}}
+          activeOpacity={1}
+        >
+          <Text white>TEXT</Text>
+        </Incubator.TouchableOpacity>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-  }
+  container: {}
 });
--- src/incubator/TouchableOpacity.js
+++ src/incubator/TouchableOpacity.js
@@ -1,25 +1,44 @@
 // TODO: support hitSlop
+// TODO: fix issue where passing backgroundColor thru style doesn't work
+// TODO: fix issue with the default value of feedbackColor 'transparent'
 import React, {Component} from 'react';
 import {processColor} from 'react-native';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import Reanimated from 'react-native-reanimated';
+import Reanimated, {Easing} from 'react-native-reanimated';
 import {TapGestureHandler, State} from 'react-native-gesture-handler';
 
-const {cond, eq, Value, call, block, event} = Reanimated;
+const {
+  Clock,
+  Code,
+  cond,
+  eq,
+  Value,
+  call,
+  block,
+  event,
+  timing,
+  debug,
+  clockRunning,
+  set,
+  startClock,
+  stopClock
+} = Reanimated;
 
 export default class TouchableOpacity extends Component {
   static propTypes = {
-    feedbackColor: PropTypes.string,
     backgroundColor: PropTypes.string,
+    feedbackColor: PropTypes.string,
     activeOpacity: PropTypes.number,
+    activeScale: PropTypes.number,
     onPress: PropTypes.func,
     pressState: PropTypes.object
   };
 
   static defaultProps = {
     activeOpacity: 0.2,
-    feedbackColor: 'transparent',
+    activeScale: 1,
+    // feedbackColor: 'transparent',
     onPress: _.noop
   };
 
@@ -36,25 +55,87 @@ export default class TouchableOpacity extends Component {
       nativeEvent: {state: this.pressState}
     }
   ],
-  {useNativeDriver: true},);
+  {useNativeDriver: true});
 
+  clock = new Clock();
+  colorClock = new Clock();
+  _scale = new Value(1);
+  _color = new Value(1);
   _opacity = block([
-    cond(eq(this.pressState, State.END), call([], () => this.props.onPress(this.props))),
+    // cond(eq(this.pressState, State.END),
+    //   call([], () => this.props.onPress(this.props))),
     cond(eq(this.pressState, State.BEGAN), this.props.activeOpacity, 1)
   ]);
 
   _color = cond(eq(this.pressState, State.BEGAN),
     processColor(this.props.feedbackColor || this.props.backgroundColor),
-    processColor(this.props.backgroundColor),);
+    processColor(this.props.backgroundColor));
 
   render() {
-    const {style, ...others} = this.props;
+    const {style, activeScale, onPress, ...others} = this.props;
+
     return (
       <TapGestureHandler onHandlerStateChange={this.onStateChange} shouldCancelWhenOutside>
-        <Reanimated.View {...others} style={[style, {backgroundColor: this._color, opacity: this._opacity}]}>
+        <Reanimated.View
+          {...others}
+          style={[style, {backgroundColor: this._color, opacity: this._opacity, transform: [{scale: this._scale}]}]}
+        >
           {this.props.children}
+
+          <Code>
+            {() =>
+              block([
+                cond(eq(this.pressState, State.END),
+                  call([], () => onPress(this.props))),
+                // TODO: investigate why this code trigger onPress callback too
+                // many times
+                cond(eq(this.pressState, State.BEGAN),
+                  block([runTiming(this.clock, this._scale, 1, activeScale)]),
+                  block([runTiming(this.clock, this._scale, activeScale, 1)]))
+              ])
+            }
+          </Code>
         </Reanimated.View>
       </TapGestureHandler>
     );
   }
+}
+
+function runTiming(clock, position, value, dest) {
+  const state = {
+    finished: new Value(0),
+    // position: new Value(0),
+    position,
+    time: new Value(0),
+    frameTime: new Value(0)
+  };
+
+  const config = {
+    duration: 100,
+    toValue: new Value(0),
+    easing: Easing.inOut(Easing.ease)
+  };
+
+  return block([
+    cond(clockRunning(clock),
+      [
+        // if the clock is already running we update the toValue, in case a new dest has been passed in
+        set(config.toValue, dest)
+      ],
+      [
+        // if the clock isn't running we reset all the animation params and start the clock
+        set(state.finished, 0),
+        set(state.time, 0),
+        set(state.position, value),
+        set(state.frameTime, 0),
+        set(config.toValue, dest),
+        startClock(clock)
+      ]),
+    // we run the step here that is going to update position
+    timing(clock, state, config),
+    // if the animation is over we stop the clock
+    cond(state.finished, debug('stop clock', stopClock(clock))),
+    // we made the block return the updated position
+    state.position
+  ]);
 }
