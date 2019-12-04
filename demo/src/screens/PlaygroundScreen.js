c93c69db22d9abcdf7b61dc6377681a6d5a8f671
Ethan Sharabi <ethans@wix.com>
2019-12-04 11:57:19 +0200

    reset PlaygroundScreen

--- demo/src/screens/PlaygroundScreen.js
+++ demo/src/screens/PlaygroundScreen.js
@@ -1,45 +1,21 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import {View, Text, Colors, Incubator} from 'react-native-ui-lib'; //eslint-disable-line
+import {View, Text} from 'react-native-ui-lib'; //eslint-disable-line
+
 
 export default class PlaygroundScreen extends Component {
-  componentDidMount() {
-    // this.slow(10);
-  }
-
-  slow(iterations = 10) {
-    if (iterations === 0) {
-      return;
-    }
-
-    setTimeout(() => {
-      _.times(5000, () => {
-        console.log('slow log');
-      });
-
-      this.slow(iterations - 1);
-    }, 10);
-  }
 
   render() {
     return (
-      <View bg-dark80 flex center>
-        <Incubator.TouchableOpacity
-          onPress={() => console.warn('ethan - presss')}
-          backgroundColor={Colors.blue40}
-          feedbackColor={Colors.blue20}
-          activeScale={0.95}
-          style={{paddingHorizontal: 20, paddingVertical: 8, borderRadius: 20}}
-          activeOpacity={1}
-        >
-          <Text white>TEXT</Text>
-        </Incubator.TouchableOpacity>
+      <View center bg-dark80 flex>
+        <Text text50>Playground Screen</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+  }
 });
