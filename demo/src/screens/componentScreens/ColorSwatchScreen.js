9778022a98daced57698db65b962babc0804261a
Inbal Tish <inbalti@wix.com>
2019-12-08 12:57:40 +0200

    adding example screen from ColorSwatch and ColorPalette

---
+++ demo/src/screens/componentScreens/ColorSwatchScreen.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import {ScrollView} from 'react-native';
+import {Colors, View, Text, ColorSwatch, ColorPalette} from 'react-native-ui-lib'; //eslint-disable-line
+import {colorsPalette} from '../../../../src/style/colorsPalette';
+
+
+export default class ColorSwatchScreen extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.colors = ['transparent', Colors.green30, Colors.yellow30, Colors.red30];
+    this.mainColors = ['#66737C', '#459FED', '#1D5382', '#3CC7C5', '#65C888', '#FAAD4D', '#F27052', '#F2564D', '#B13DAC', '#733CA6', '#79838A', '#5847FF', '#00BBF2', '#00CD8B', '#FF563D', '#ffb600'];
+    this.allColors = colorsPalette;
+
+    this.state = {
+      color: this.colors[0],
+      color1: this.mainColors[this.mainColors.length - 1],
+      color2: this.allColors[20],
+      selected: false
+    };
+  }
+
+  onPress = () => {
+    this.setState({selected: !this.state.selected});
+  }
+
+  onValueChange = (value) => {
+    this.setState({color: value});
+  }
+  onValueChange1 = (value) => {
+    this.setState({color1: value});
+  }
+  onValueChange2 = (value) => {
+    this.setState({color2: value});
+  }
+
+  render() {
+    const {color, color1, color2, selected} = this.state;
+    
+    return (
+      <ScrollView style={{backgroundColor: Colors.dark80}}>
+        <View flex center useSafeArea>
+          <Text margin-5 text60 dark10>Single ColorSwatch</Text>
+          <View row>
+            <ColorSwatch selected={selected} onPress={this.onPress}/>
+            <View>
+              <ColorSwatch selected color={Colors.orange60}/>
+              <Text>Disabled</Text>
+            </View>
+          </View>
+          
+          <Text marginT-20 text60 dark10>ColorPalette</Text>
+          <Text marginB-10 text70 style={{color}}>Slected Color: {color}</Text>
+          <ColorPalette value={color} onValueChange={this.onValueChange} colors={this.colors}/>
+
+          <Text margin-10 text60 dark10>Scrollable</Text>
+          <ColorPalette value={color1} onValueChange={this.onValueChange1} colors={this.mainColors}/>
+          
+          <Text margin-10 text60 dark10>Pagination</Text>
+          <ColorPalette numberOfRows={4} value={color2} onValueChange={this.onValueChange2} colors={this.allColors}/>
+        </View>
+      </ScrollView>
+    );
+  }
+}
