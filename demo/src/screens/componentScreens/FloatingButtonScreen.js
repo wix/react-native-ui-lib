56424321e3da93a8ee0df6be15bf3e7ecdf5d7d7
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2019-12-25 14:24:06 +0200

    Adding 'hideBackgroundOverlay' prop (#599)

--- demo/src/screens/componentScreens/FloatingButtonScreen.js
+++ demo/src/screens/componentScreens/FloatingButtonScreen.js
@@ -66,15 +66,34 @@ export default class FloatingButtonScreen extends Component {
             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             Contrary to popular belief, Lorem Ipsum is not simply random text. 
             It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            Lorem Ipsum has been the industry standard dummy text ever since the 1500s, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+            It has survived not only five centuries, but also the leap into electronic typesetting, 
+            remaining essentially unchanged. 
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            Contrary to popular belief, Lorem Ipsum is not simply random text. 
+            It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
             </Text>
           </View>
         </ScrollView>
 
         <FloatingButton
           visible={this.state.showButton}
-          button={{label: 'Approve', onPress: this.close}}
-          secondaryButton={{label: 'Not now', onPress: this.notNow}}
+          button={{
+            label: 'Approve', 
+            onPress: this.close, 
+            labelStyle: {fontWeight: '400'}
+          }}
+          secondaryButton={{
+            label: 'Not now', 
+            onPress: this.notNow, 
+            color: Colors.red30, 
+            labelStyle: {fontWeight: '400'}
+          }}
           // bottomMargin={80}
+          // hideBackgroundOverlay
         />
       </View>
     );
