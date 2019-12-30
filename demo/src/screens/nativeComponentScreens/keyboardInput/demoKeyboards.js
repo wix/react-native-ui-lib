b8587d012f9bb2a9790dd7716e335b7d4d9ae0fd
Miki Leib <michaelle@wix.com>
2019-12-30 13:33:25 +0200

    Refactor KeyboardView

--- demo/src/screens/nativeComponentScreens/keyboardInput/demoKeyboards.js
+++ demo/src/screens/nativeComponentScreens/keyboardInput/demoKeyboards.js
@@ -1,71 +1,52 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {ScrollView, StyleSheet} from 'react-native';
-import {Keyboard, Text, TouchableOpacity} from 'react-native-ui-lib';
+import {Keyboard, Colors} from 'react-native-ui-lib';
 const KeyboardRegistry = Keyboard.KeyboardRegistry;
+import KeyboardView from './KeyboardView';
 
-class KeyboardView extends Component {
+class KeyboardView1 extends Component {
   static propTypes = {
     title: PropTypes.string
   };
 
   onButtonPress() {
-    KeyboardRegistry.onItemSelected('KeyboardView', {
+    KeyboardRegistry.onItemSelected('KeyboardView1', {
       message: 'item selected from KeyboardView'
     });
   }
 
   render() {
+    const {title} = this.props;
     return (
-      <ScrollView contentContainerStyle={[styles.keyboardContainer, {backgroundColor: 'purple'}]}>
-        <Text style={{color: 'white'}}>HELOOOO!!!</Text>
-        <Text style={{color: 'white'}}>{this.props.title}</Text>
-        <TouchableOpacity
-          testID={'click-me'}
-          style={{padding: 20, marginTop: 30, backgroundColor: 'white'}}
-          onPress={() => this.onButtonPress()}
-        >
-          <Text>Click Me!</Text>
-        </TouchableOpacity>
-      </ScrollView>
+      <KeyboardView
+        title={title}
+        backgroundColor={Colors.violet10}
+        onPress={this.onButtonPress}
+      />
     );
   }
 }
 
-class AnotherKeyboardView extends Component {
+class KeyboardView2 extends Component {
   static propTypes = {
     title: PropTypes.string
   };
 
   onButtonPress() {
-    KeyboardRegistry.toggleExpandedKeyboard('AnotherKeyboardView');
+    KeyboardRegistry.toggleExpandedKeyboard('KeyboardView2');
   }
 
   render() {
+    const {title} = this.props;
     return (
-      <ScrollView contentContainerStyle={[styles.keyboardContainer, {backgroundColor: 'orange'}]}>
-        <Text>*** ANOTHER ONE ***</Text>
-        <Text>{this.props.title}</Text>
-        <TouchableOpacity
-          testID={'toggle-fs'}
-          style={{padding: 20, marginTop: 30, backgroundColor: 'white'}}
-          onPress={() => this.onButtonPress()}
-        >
-          <Text>Toggle Full-Screen!</Text>
-        </TouchableOpacity>
-      </ScrollView>
+      <KeyboardView
+        title={title}
+        backgroundColor={Colors.yellow20}
+        onPress={this.onButtonPress}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  keyboardContainer: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
-
-KeyboardRegistry.registerKeyboard('KeyboardView', () => KeyboardView);
-KeyboardRegistry.registerKeyboard('AnotherKeyboardView', () => AnotherKeyboardView);
+KeyboardRegistry.registerKeyboard('KeyboardView1', () => KeyboardView1);
+KeyboardRegistry.registerKeyboard('KeyboardView2', () => KeyboardView2);
