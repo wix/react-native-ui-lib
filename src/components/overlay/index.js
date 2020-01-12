3b99ed4a9b091932122394429d50edb590415312
arnonz <arnonz@wix.com>
2020-01-12 02:26:42 +0200

    Cosmetics

--- src/components/overlay/index.js
+++ src/components/overlay/index.js
@@ -56,7 +56,7 @@ export default class Overlay extends PureBaseComponent {
   renderImage(typeStyle) {
     const {type, customContent} = this.props;
     const image = type !== OVERLY_TYPES.SOLID ? gradientImage : undefined;
-    console.warn('az', [styles.container])
+
     return (
       <View style={styles.container}>
         {type && <Image style={[styles.container, typeStyle]} resizeMode={'stretch'} source={image}/>}
