84433ad04b97cb7d6e8d7bc51149848f7d048648
Ethan Sharabi <ethans@wix.com>
2020-01-23 19:11:10 +0200

    lazy load all example screen to improve loading time of demo app

--- demo/src/screens/incubatorScreens/index.js
+++ demo/src/screens/incubatorScreens/index.js
@@ -1,9 +1,6 @@
 import {Navigation} from 'react-native-navigation';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
-import TabControllerScreen from './TabControllerScreen';
-import TouchableOpacityScreen from './TouchableOpacityScreen';
-
-Navigation.registerComponent('unicorn.incubator.TabControllerScreen', () => gestureHandlerRootHOC(TabControllerScreen));
+Navigation.registerComponent('unicorn.incubator.TabControllerScreen', () => gestureHandlerRootHOC(require('./TabControllerScreen').default));
 Navigation.registerComponent('unicorn.incubator.TouchableOpacityScreen', () =>
-  gestureHandlerRootHOC(TouchableOpacityScreen));
+  gestureHandlerRootHOC(require('./TouchableOpacityScreen').default));
