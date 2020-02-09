653e287bb5148d53550b3bf24d59045067ea22d5
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2020-02-09 19:34:56 +0200

    Fix/wizard minor fixes accessibility and export screens (#645)
    * Wizard - fix connector width & allow passing circleSize
    
    * Export all screens
    
    * Wizard - add accessibility
    
    * Remove inaccessible from Text
    
    * Remove inaccessible from Text
    
    * Remove changes from Text

--- src/components/wizard/WizardStates.js
+++ src/components/wizard/WizardStates.js
@@ -13,8 +13,14 @@ export const States = {
 export const StatesConfig = {
   enabled: {color: Colors.dark30, circleColor: Colors.dark60, enabled: true},
   disabled: {color: Colors.dark50, circleColor: Colors.dark60},
-  error: {color: Colors.red30, icon: exclamationSmall, enabled: true},
-  skipped: {color: Colors.red30, enabled: true},
-  completed: {color: Colors.dark30, circleColor: Colors.dark60, icon: checkMarkSmall, enabled: true},
+  error: {color: Colors.red30, icon: exclamationSmall, enabled: true, accessibilityInfo: 'Validation Error'},
+  skipped: {color: Colors.red30, enabled: true, accessibilityInfo: 'Not completed'},
+  completed: {
+    color: Colors.dark30,
+    circleColor: Colors.dark60,
+    icon: checkMarkSmall,
+    enabled: true,
+    accessibilityInfo: 'Completed'
+  },
   active: {color: Colors.blue10, circleColor: Colors.blue10}
 };
