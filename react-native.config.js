bce41862f88e88fca001b41eab835cc9cf82371b
Miki Leib <michaelle@wix.com>
2020-03-04 18:32:33 +0200

    Fix auto link in iOS

--- react-native.config.js
+++ react-native.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   dependency: {
     platforms: {
+      ios: {
+        project: './lib/ios/'
+      },
       android: {
         sourceDir: './lib/android/',
         packageImportPath: `
-  import com.wix.reactnativeuilib.highlighterview.HighlighterViewPackage;
-  import com.wix.reactnativeuilib.keyboardinput.KeyboardInputPackage;
-  import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
-  import com.wix.reactnativeuilib.wheelpicker.WheelPickerPackage;`
+    import com.wix.reactnativeuilib.highlighterview.HighlighterViewPackage;
+    import com.wix.reactnativeuilib.keyboardinput.KeyboardInputPackage;
+    import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
+    import com.wix.reactnativeuilib.wheelpicker.WheelPickerPackage;`
       }
     }
   }
