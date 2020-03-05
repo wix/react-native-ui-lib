fdcd064fd6e450b8df4aeb104fd689641a80b72c
Ethan Sharabi <ethans@wix.com>
2020-03-05 11:31:16 +0200

    Support auto link for Android (#682)
    * Support auto link for Android
    
    * Fix auto link in iOS

---
+++ react-native.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  dependency: {
+    platforms: {
+      ios: {
+        project: './lib/ios/'
+      },
+      android: {
+        sourceDir: './lib/android/',
+        packageImportPath: `
+    import com.wix.reactnativeuilib.highlighterview.HighlighterViewPackage;
+    import com.wix.reactnativeuilib.keyboardinput.KeyboardInputPackage;
+    import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
+    import com.wix.reactnativeuilib.wheelpicker.WheelPickerPackage;`
+      }
+    }
+  }
+};
