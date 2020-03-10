4ba5b9da38d4824eafb15ad67af5d35118880c82
Ethan Sharabi <ethans@wix.com>
2020-03-10 16:13:30 +0200

    remove native keyboard package from Android auto-link

--- react-native.config.js
+++ react-native.config.js
@@ -7,13 +7,11 @@ module.exports = {
       android: {
         sourceDir: './lib/android/',
         packageImportPath: `import com.wix.reactnativeuilib.highlighterview.HighlighterViewPackage;
-import com.wix.reactnativeuilib.keyboardinput.KeyboardInputPackage;
 import com.wix.reactnativeuilib.textinput.TextInputDelKeyHandlerPackage;
 import com.wix.reactnativeuilib.wheelpicker.WheelPickerPackage;`,
         packageInstance: `new HighlighterViewPackage(),
       new WheelPickerPackage(),
-      new TextInputDelKeyHandlerPackage(),
-      new KeyboardInputPackage(application)`
+      new TextInputDelKeyHandlerPackage()`
       }
     }
   }
