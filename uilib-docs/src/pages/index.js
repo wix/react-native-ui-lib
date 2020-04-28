68505f23500a3c73759944096d8a6826a36a3bc0
Ethan Sharabi <ethans@wix.com>
2020-04-28 15:40:41 +0300

    add link to our expo-snak

--- uilib-docs/src/pages/index.js
+++ uilib-docs/src/pages/index.js
@@ -24,7 +24,15 @@ const IndexPage = props => {
             <p className="description">UI Toolset & Components Library for React Native</p>
 
             <div className="qr">
-              <img src={'https://user-images.githubusercontent.com/1780255/76164023-f2171400-6153-11ea-962d-d57b64a08a80.png'} />
+              <img
+                src={
+                  'https://user-images.githubusercontent.com/1780255/76164023-f2171400-6153-11ea-962d-d57b64a08a80.png'
+                }
+              />
+
+              <a href="https://snack.expo.io/@ethanshar/rnuilib_snack" target="_blank" rel="noopener noreferrer">
+                Or try our demo on Expo-Snack
+              </a>
             </div>
           </div>
         </div>
