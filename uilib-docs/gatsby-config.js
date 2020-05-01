9fd38fd651282cd5708a78373b939711d77b5a23
Ethan Sharabi <ethans@wix.com>
2020-05-01 10:47:37 +0300

    move configurations and page creators to separate files

--- uilib-docs/gatsby-config.js
+++ uilib-docs/gatsby-config.js
@@ -1,3 +1,5 @@
+const {manifestPlugin, markdownPagesPlugin, componentsDocgenPlugin, imagesPlugin} = require('./configurations/plugins');
+
 module.exports = {
   pathPrefix: '/react-native-ui-lib',
   siteMetadata: {
@@ -6,50 +8,12 @@ module.exports = {
     author: `@gatsbyjs`
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/../markdowns/`
-      }
-    },
-    `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    'gatsby-transformer-react-docgen',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/../src/components/`
-      }
-    },
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     path: `${__dirname}/../src/incubator/`
-    //   }
-    // },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
+    ...markdownPagesPlugin,
+    ...componentsDocgenPlugin,
+    ...imagesPlugin,
+    ...manifestPlugin
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
