1e9c9197d05ee8687f40225a2d1f72dd42f14228
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2020-05-04 11:19:56 +0300

    Feat/lint add typography deprecation (#762)
    * Update package.json (reduce security vulnerabilities)
    
    * Lint - add Typography deprecation rules

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -4,10 +4,13 @@ module.exports = {
     'no-direct-import': require('./lib/rules/no-direct-import'),
     'component-deprecation': require('./lib/rules/component-deprecation'),
     'assets-deprecation': require('./lib/rules/assets-deprecation'),
+    'typography-deprecation': require('./lib/rules/typography-deprecation'),
     // for duplicate rules usage
     'component-deprecation_warn': require('./lib/rules/component-deprecation'),
     'assets-deprecation_warn': require('./lib/rules/assets-deprecation'),
+    'typography-deprecation_warn': require('./lib/rules/typography-deprecation'),
     'component-deprecation_error': require('./lib/rules/component-deprecation'),
     'assets-deprecation_error': require('./lib/rules/assets-deprecation'),
+    'typography-deprecation_error': require('./lib/rules/typography-deprecation'),
   },
 };
