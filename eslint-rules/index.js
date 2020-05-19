56a52e9c2d38ed1617b81c4faab9d9c4f85d44d8
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2020-05-19 12:30:12 +0300

    Lint - add function-deprecation (#780)
    * Lint - add function-deprecation (rebase)
    
    * Fix missing function-deprecation_warn

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -5,12 +5,15 @@ module.exports = {
     'component-deprecation': require('./lib/rules/component-deprecation'),
     'assets-deprecation': require('./lib/rules/assets-deprecation'),
     'typography-deprecation': require('./lib/rules/typography-deprecation'),
+    'function-deprecation': require('./lib/rules/function-deprecation'),
     // for duplicate rules usage
     'component-deprecation_warn': require('./lib/rules/component-deprecation'),
     'assets-deprecation_warn': require('./lib/rules/assets-deprecation'),
     'typography-deprecation_warn': require('./lib/rules/typography-deprecation'),
+    'function-deprecation_warn': require('./lib/rules/function-deprecation'),
     'component-deprecation_error': require('./lib/rules/component-deprecation'),
     'assets-deprecation_error': require('./lib/rules/assets-deprecation'),
     'typography-deprecation_error': require('./lib/rules/typography-deprecation'),
+    'function-deprecation_error': require('./lib/rules/function-deprecation'),
   },
 };
