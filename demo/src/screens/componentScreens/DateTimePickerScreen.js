2421421d0f7a41f26393f34927ad667745db8b60
arnonz <arnonz@wix.com>
2020-05-20 01:20:58 +0300

    Updating DateTimePickerScreen

--- demo/src/screens/componentScreens/DateTimePickerScreen.js
+++ demo/src/screens/componentScreens/DateTimePickerScreen.js
@@ -3,24 +3,38 @@ import {DateTimePicker, View, Text} from 'react-native-ui-lib'; // eslint-disabl
 
 
 export default class DateTimePickerScreen extends Component {
-  
+
   render() {
     return (
-      <View flex padding-20>
-        <Text text40 marginB-40>Date Time Picker</Text>
-        
-        <DateTimePicker 
-          title={'Date'} 
-          placeholder={'Select a date'} 
+      <View flex padding-page>
+        <Text text40>Date Time Picker</Text>
+        <DateTimePicker
+          title={'Date'}
+          placeholder={'Select a date'}
           // dateFormat={'MMM D, YYYY'}
-          // value={new Date('October 13, 2014')} 
+          // value={new Date('October 13, 2014')}
         />
-        <DateTimePicker 
-          mode={'time'} 
-          title={'Time'} 
+        <DateTimePicker
+          mode={'time'}
+          title={'Time'}
           placeholder={'Select time'}
           // timeFormat={'h:mm A'}
           // value={new Date('2015-03-25T12:00:00-06:30')}
+        />
+
+        <Text text60R marginT-40>Disabled</Text>
+        <DateTimePicker
+          containerStyle={{marginBottom: 20}}
+          editable={false}
+          title={'Date'}
+          placeholder={'Select a date'}
+        />
+        <DateTimePicker
+          editable={false}
+          mode={'time'}
+          title={'Time'}
+          placeholder={'Select time'}
+          value={new Date('2015-03-25T12:00:00-06:30')}
         />
       </View>
     );
