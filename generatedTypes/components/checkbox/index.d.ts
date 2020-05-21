14418098b60256f4c9b4072d97a5efac08cb9d5d
arnonz <arnonz@wix.com>
2020-05-21 23:10:52 +0300

    Updating Checkbox generatedTypes

---
+++ generatedTypes/components/checkbox/index.d.ts
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleProp, ViewStyle } from 'react-native';
+interface CheckboxProps {
+    /**
+     * The value of the Checkbox. If true the switch will be turned on. Default value is false.
+     */
+    value?: boolean;
+    /**
+     * Invoked with the new value when the value changes.
+     */
+    onValueChange?: Function;
+    /**
+     * Whether the checkbox should be disabled
+     */
+    disabled?: boolean;
+    /**
+     * The Checkbox color
+     */
+    color?: string;
+    /**
+     * The size of the checkbox. affect both width and height
+     */
+    size?: number;
+    /**
+     * The Checkbox border radius
+     */
+    borderRadius?: number;
+    /**
+     * The icon asset to use for the selected indication (accept only local assets)
+     */
+    selectedIcon?: number;
+    /**
+     * The selected icon color
+     */
+    iconColor?: string;
+    /**
+     * Additional styling
+     */
+    style?: StyleProp<ViewStyle>;
+}
+declare const _default: React.ComponentType<CheckboxProps>;
+export default _default;
