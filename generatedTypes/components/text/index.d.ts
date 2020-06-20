9983f273f32efd1936a99a1bd36ebb862795c103
Ethan Sharabi <ethans@wix.com>
2020-06-20 10:45:10 +0300

    Fix #817 - Remove redundant declaration of ref type

--- generatedTypes/components/text/index.d.ts
+++ generatedTypes/components/text/index.d.ts
@@ -26,7 +26,6 @@ export declare type TextPropTypes = TextProps & TypographyModifiers & ColorsModi
      * Use Animated.Text as a container
      */
     animated?: boolean;
-    ref?: any;
     textAlign?: string;
 };
 declare type PropsTypes = BaseComponentInjectedProps & ForwardRefInjectedProps & TextPropTypes;
