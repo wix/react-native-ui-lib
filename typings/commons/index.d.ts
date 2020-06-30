69d78c75bc693e17ed1bae0e1b0b2a3e858f0721
ykravv <yevheniikr@wix.com>
2020-06-30 23:38:49 +0300

    Add common types to BaseComponent

--- typings/commons/index.d.ts
+++ typings/commons/index.d.ts
@@ -43,6 +43,10 @@ export type BaseComponentModifiers =
 export class BaseComponent<P = {}, S = {}, SS = any> extends React.Component<P & BaseComponentModifiers, S, SS> {
   static propTypes?: any;
   static defaultProps?: Partial<P>;
+
+  // TODO: There's more methods here, need to create more precise types
+  getThemeProps: () => P & ThemeProps;
+  extractAccessibilityProps: () => AccessibilityProps;
 }
 
 export type _B = Partial<Record<BaseComponentColorModifierVariations, boolean>>;
