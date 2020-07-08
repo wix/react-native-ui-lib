5d69a78db4a5c44c9324949e1a0f5450eac0fdf2
Ethan Sharabi <ethans@wix.com>
2020-07-08 15:06:50 +0300

    Update ThemeManager API typing

--- generatedTypes/style/themeManager.d.ts
+++ generatedTypes/style/themeManager.d.ts
@@ -29,8 +29,8 @@ export declare class ThemeManager {
     };
     setItem(key: string, value: string): void;
     getItem(key: string): any;
-    setComponentTheme(componentName: string, overrides: Dictionary<string | number> | Function): void;
-    setComponentForcedTheme(componentName: string, overrides: Dictionary<string> | Function): void;
+    setComponentTheme(componentName: string, overrides: Dictionary<any> | Function): void;
+    setComponentForcedTheme(componentName: string, overrides: Dictionary<any> | Function): void;
     get components(): Extendable;
     get forcedThemeComponents(): Extendable;
     get primaryColor(): string;
