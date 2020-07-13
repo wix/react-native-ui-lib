35433ccc7940ad99629918578da9f623b8cd7055
Miki <38354019+M-i-k-e-l@users.noreply.github.com>
2020-07-13 16:11:12 +0300

    withScrollEnabler - fix hooks and typings (#832)

--- generatedTypes/commons/withScrollEnabler.d.ts
+++ generatedTypes/commons/withScrollEnabler.d.ts
@@ -1,14 +1,13 @@
 import React from 'react';
-import { FlatListProps, ScrollViewProps, LayoutChangeEvent } from 'react-native';
+import { LayoutChangeEvent } from 'react-native';
 export declare type ScrollEnablerProps = {
     onContentSizeChange: (contentWidth: number, contentHeight: number) => void;
     onLayout: (event: LayoutChangeEvent) => void;
     scrollEnabled: boolean;
 };
-declare type SupportedViews = FlatListProps<any> | ScrollViewProps;
-export declare type WithScrollEnablerProps = SupportedViews & {
+export declare type WithScrollEnablerProps = {
     scrollEnablerProps: ScrollEnablerProps;
     ref?: any;
 };
-declare function withScrollEnabler<PROPS extends SupportedViews>(WrappedComponent: React.ComponentType<WithScrollEnablerProps>): React.ComponentType<PROPS>;
+declare function withScrollEnabler<PROPS>(WrappedComponent: React.ComponentType<PROPS & WithScrollEnablerProps>): React.ComponentType<PROPS>;
 export default withScrollEnabler;
