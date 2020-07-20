6f4eee0b3a5a593eebaa7b0b15e6c8e03897e126
Ethan Sharabi <ethans@wix.com>
2020-07-20 17:30:09 +0300

    normalize android input spacings

--- src/incubator/TextField/Input.tsx
+++ src/incubator/TextField/Input.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import {TextInput, TextInputProps} from 'react-native';
+import {TextInput, TextInputProps, StyleSheet, Platform} from 'react-native';
 
 const Input = ({style, ...props}: TextInputProps, ref) => {
-  return <TextInput style={style} {...props} ref={ref} />;
+  return (
+    <TextInput
+      style={[styles.input, style]}
+      {...props}
+      ref={ref}
+      underlineColorAndroid="transparent"
+    />
+  );
 };
 
 export default React.forwardRef(Input);
+
+const styles = StyleSheet.create({
+  input: {
+    ...Platform.select({
+      // This reset android input inner spacing
+      android: {
+        padding: 0,
+        textAlignVertical: 'center'
+      }
+    })
+  }
+});
