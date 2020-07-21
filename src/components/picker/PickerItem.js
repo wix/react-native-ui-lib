a2ccee093c6b448bd74775bf02e45a47ce0402ea
Ethan Sharabi <ethans@wix.com>
2020-07-21 16:50:15 +0300

    Fix docs broken example links

--- src/components/picker/PickerItem.js
+++ src/components/picker/PickerItem.js
@@ -15,7 +15,7 @@ import TouchableOpacity from '../touchableOpacity';
  * @description: Picker.Item, for configuring the Picker's selectable options
  * @extends: TouchableOpacity
  * @extendslink: docs/TouchableOpacity
- * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreens/FormScreen.js
+ * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreens/PickerScreen.js
  */
 class PickerItem extends BaseComponent {
   static displayName = 'Picker.Item';
