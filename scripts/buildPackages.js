580af379be113c7ba8c6c6a1738ad84c0143d911
Ethan Sharabi <ethans@wix.com>
2020-07-23 12:42:42 +0300

    build separate package per each component

--- scripts/buildPackages.js
+++ scripts/buildPackages.js
@@ -1,12 +1,46 @@
 const fs = require('fs');
+const _ = require('lodash');
 
 const packages = [
   {
     filename: 'keyboard.js',
     content: `module.exports = require('./lib/components/Keyboard').default;\n`
+  },
+  {
+    filename: 'core.js',
+    components: ['View', 'Text', 'Image', 'TouchableOpacity', 'Button']
   }
 ];
 
+// Write packages
 packages.forEach((package) => {
-  fs.writeFileSync(package.filename, package.content);
+  let content = package.content || '';
+
+  if (package.components) {
+    content += 'module.exports = {\n';
+    package.components.forEach((component) => {
+      content += `get ${component}() {\n`;
+      content += `return require('./src/components/${_.camelCase(
+        component
+      )}').default;`;
+      content += `},\n`;
+    });
+    content += '};\n';
+  }
+
+  fs.writeFileSync(package.filename, content);
 });
+
+// Write separate components
+const path = './src/components';
+fs.readdir(path, (err, files) => {
+  if (!err) {
+    files.filter(f => f !== 'index.js').forEach((file) => {
+      fs.writeFileSync(
+        `${file}.js`,
+        `module.exports = require('${path}/${file}').default;\n`
+      );
+    });
+  }
+});
+
