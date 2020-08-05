8a811d2c2aa7937a3c129eb32ccb1060d119e57d
Lidor Dafna <66782556+lidord-wix@users.noreply.github.com>
2020-08-05 16:08:33 +0300

    add testID (#880)
    * add testID
    
    * handle modal's testID
    
    * pass testId to Modal

--- src/components/hint/index.js
+++ src/components/hint/index.js
@@ -398,7 +398,7 @@ class Hint extends BaseComponent {
   }
 
   render() {
-    const {visible, onBackgroundPress} = this.props;
+    const {visible, onBackgroundPress, testID} = this.props;
 
     if (!visible) {
       return this.props.children;
@@ -414,6 +414,7 @@ class Hint extends BaseComponent {
             transparent
             onBackgroundPress={onBackgroundPress}
             onRequestClose={onBackgroundPress}
+            testID={`${testID}.modal`}
           >
             {this.renderHintContainer()}
           </Modal>
