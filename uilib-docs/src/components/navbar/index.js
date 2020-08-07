63d811ea685cebdca0e96a54a4fcc12afa8ed237
Ethan Sharabi <ethans@wix.com>
2020-08-07 09:46:05 +0300

    Update navbar with components hierarchy

--- uilib-docs/src/components/navbar/index.js
+++ uilib-docs/src/components/navbar/index.js
@@ -5,27 +5,27 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import _ from 'lodash';
 
-import './navbar.scss';
-import searchIcon from '../images/search.svg';
+import './index.scss';
+import searchIcon from '../../images/search.svg';
+import Item from './item';
 
 class Navbar extends Component {
-  static propTypes = {
-    components: PropTypes.array
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.setFilter = this.setFilter.bind(this);
-  }
-
   state = {
     filter: ''
   };
 
-  setFilter({target: {value}}) {
+  setFilter = ({target: {value}}) => {
     this.setState({filter: value});
-  }
+  };
+
+  getCurrentPage = () => {
+    const path = window.location.href;
+    return _.chain(path)
+      .split('/')
+      .filter(item => !_.isEmpty(item))
+      .last()
+      .value();
+  };
 
   getMarkdownPages(data) {
     const markdownPages = data.allFile.edges;
@@ -48,36 +48,33 @@ class Navbar extends Component {
   }
 
   renderNavbar = data => {
+    const currentPage = this.getCurrentPage();
     const {filter} = this.state;
     const markdowns = this.getMarkdownPages(data);
     const components = this.getNavbarComponents(data);
 
     const filteredComponents = _.filter(components, component =>
-      _.includes(_.lowerCase(component.node.displayName), _.lowerCase(filter)));
+      _.includes(_.lowerCase(component.node.displayName), _.lowerCase(filter))
+    );
+
+    const componentsByGroups = _.groupBy(
+      filteredComponents,
+      c => _.split(c.node.displayName, '.')[0]
+    );
+
     return (
       <div className="navbar">
         <div className="search">
-          <img src={searchIcon}/>
-          <input placeholder="Search..." onChange={this.setFilter}/>
+          <img src={searchIcon} />
+          <input placeholder="Search..." onChange={this.setFilter} />
         </div>
         <ul>
           {_.map(markdowns, page => {
-            return (
-              <li>
-                <Link to={page.path}>{page.title}</Link>
-              </li>
-            );
+            return <Item id={page.title} link={page.path} />;
           })}
-          <li className="separator"/>
-          {_.map(filteredComponents, (component, index) => {
-            return (
-              <li key={index}>
-                <Link key={component.node.displayName} to={`/docs/${component.node.displayName}/`}>
-                  {component.node.displayName}
-                  {/* {component.node.isPublic && <span className="public">public</span>} */}
-                </Link>
-              </li>
-            );
+          <li className="separator" />
+          {_.map(componentsByGroups, (components, key) => {
+            return <Item id={key} components={components} currentPage={currentPage}></Item>;
           })}
         </ul>
       </div>
