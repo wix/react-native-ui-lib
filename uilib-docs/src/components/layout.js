ec8713d1c3b730395ace888f62b1e9731b663ed5
Ethan Sharabi <ethans@wix.com>
2020-08-08 10:21:08 +0300

    Update SEO meta tags

--- uilib-docs/src/components/layout.js
+++ uilib-docs/src/components/layout.js
@@ -12,13 +12,25 @@ const pathPrefix = '/react-native-ui-lib';
 const Layout = ({children, location}) => {
   const {pathname} = location;
   const showSidebar = _.replace(pathname, pathPrefix, '') !== '/';
+
+  const metaTitle = 'RNUILib';
+  const metaDescription = 'React Native UI Toolset and Components Library';
+
   return (
     <div className="layout">
       <Helmet>
         <meta charSet="utf-8" />
-        <title>RNUILib</title>
-        <meta name="description" content="React Native UI Toolset and Components Library"></meta>
-        <link rel="canonical" href="https://wix.github.io/react-native-ui-lib/" />
+        <title>{metaTitle}</title>
+        <meta name="title" content={metaTitle} />
+        <meta name="description" content={metaDescription} />
+        <meta property="og:title" content={metaTitle} />
+        <meta property="og:description" content={metaDescription} />
+        <meta property="twitter:title" content={metaTitle} />
+        <meta property="twitter:description" content={metaDescription} />
+        <link
+          rel="canonical"
+          href="https://wix.github.io/react-native-ui-lib/"
+        />
       </Helmet>
       <Header />
       {/* <div className={`main ${!showSidebar ? 'fill' : ''}`}> */}
