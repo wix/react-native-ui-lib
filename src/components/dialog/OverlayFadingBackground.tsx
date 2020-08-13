9a6f7db37de0029299f9b527cd22f97911835d80
Ethan Sharabi <ethans@wix.com>
2020-08-13 16:37:13 +0300

    Add hook dependencies according to the lint recommendations

--- src/components/dialog/OverlayFadingBackground.tsx
+++ src/components/dialog/OverlayFadingBackground.tsx
@@ -15,32 +15,32 @@ export default ({
 }: Props) => {
   const fadeAnimation = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    if (!dialogVisibility) {
-      animateFading(0);
-    }
-  }, [dialogVisibility]);
-
-  useEffect(() => {
-    if (modalVisibility) {
-      animateFading(1);
-    }
-  }, [modalVisibility]);
-
   const animateFading = useCallback((toValue) => {
     Animated.timing(fadeAnimation, {
       toValue,
       duration: 400,
       useNativeDriver: true
     }).start();
-  }, []);
+  }, [fadeAnimation]);
+
+  useEffect(() => {
+    if (!dialogVisibility) {
+      animateFading(0);
+    }
+  }, [dialogVisibility, animateFading]);
+
+  useEffect(() => {
+    if (modalVisibility) {
+      animateFading(1);
+    }
+  }, [modalVisibility, animateFading]);
 
   const style = useMemo(() => {
     return {
       opacity: fadeAnimation,
       backgroundColor: overlayBackgroundColor
     };
-  }, [overlayBackgroundColor]);
+  }, [overlayBackgroundColor, fadeAnimation]);
 
   return <View absF animated style={style} pointerEvents="none"/>;
 };
