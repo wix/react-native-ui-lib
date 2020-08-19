b9ebdc381690b5fd99cd6db632012732430a2ee9
Inbal Tish <inbalti@wix.com>
2020-08-19 11:51:23 +0300

    testID to badge

--- demo/src/screens/componentScreens/DrawerScreen.tsx
+++ demo/src/screens/componentScreens/DrawerScreen.tsx
@@ -194,7 +194,7 @@ class DrawerScreen extends Component {
         style={{borderBottomWidth: 1, borderColor: Colors.grey60}}
         testID="drawer_item"
       >
-        {this.state.unread && <Badge size={'pimpleSmall'} backgroundColor={Colors.red30} containerStyle={{marginRight: 8}}/>}
+        {this.state.unread && <Badge testID="drawer_item_badge" size={'pimpleSmall'} backgroundColor={Colors.red30} containerStyle={{marginRight: 8}}/>}
         <Avatar source={{uri: conversations[0].thumbnail}}/>
         <View marginL-20>
           <Text text70R={!this.state.unread} text70BO={this.state.unread}>{conversations[0].name}</Text>
