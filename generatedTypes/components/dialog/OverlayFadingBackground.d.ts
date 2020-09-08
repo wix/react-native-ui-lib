95b2d2b1739e6da2c954a940fdb2b0b107ae7e6c
Miki Leib <michaelle@wix.com>
2020-09-08 20:19:44 +0300

    Migrate Dialog to typescript

--- generatedTypes/components/dialog/OverlayFadingBackground.d.ts
+++ generatedTypes/components/dialog/OverlayFadingBackground.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="react" />
 interface Props {
-    dialogVisibility: boolean;
-    modalVisibility: boolean;
-    overlayBackgroundColor: string;
+    dialogVisibility?: boolean;
+    modalVisibility?: boolean;
+    overlayBackgroundColor?: string;
 }
 declare const OverlayFadingBackground: {
     ({ dialogVisibility, modalVisibility, overlayBackgroundColor }: Props): JSX.Element;
