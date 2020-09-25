16d80013b4d9cd97dc91185cd8fb17b3be0103af
Ethan Sharabi <ethans@wix.com>
2020-09-25 09:16:58 +0300

    export WheelPicker in incubator index

--- src/incubator/index.ts
+++ src/incubator/index.ts
@@ -2,3 +2,4 @@
 export {default as TabController} from './TabController';
 export {default as TextField} from './TextField';
 export {default as TouchableOpacity} from './TouchableOpacity';
+export {default as WheelPicker} from './WheelPicker';
