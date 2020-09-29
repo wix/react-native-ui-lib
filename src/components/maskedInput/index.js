1c5cb7856315f031bb16955d336005f487e47246
arnonz <arnonz@wix.com>
2020-09-29 16:05:54 +0300

    Updating extends link

--- src/components/maskedInput/index.js
+++ src/components/maskedInput/index.js
@@ -10,6 +10,7 @@ import Text from '../text';
 /**
  * @description: Mask Input to create custom looking inputs with custom formats
  * @extends: TextInput
+ * @extendslink: docs/TextInput
  * @gif: https://camo.githubusercontent.com/61eedb65e968845d5eac713dcd21a69691571fb1/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4b5a5a7446666f486f454b334b2f67697068792e676966
  * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreens/MaskedInputScreen.js
  */
