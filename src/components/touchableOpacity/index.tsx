050ddabd9c6888bf46e75a504082e6f6f117a54b
Miki Leib <michaelle@wix.com>
2020-10-20 13:41:31 +0300

    Move PageControl to typescript - initial commit

--- src/components/touchableOpacity/index.tsx
+++ src/components/touchableOpacity/index.tsx
@@ -18,7 +18,7 @@ import {
 import IncubatorTouchableOpacity from '../../incubator/TouchableOpacity';
 
 
-export type TouchableOpacityProps = Omit<RNTouchableOpacityProps, 'style'> &
+export type TouchableOpacityProps = Omit<RNTouchableOpacityProps, 'style' | 'onPress'> &
   ContainerModifiers & {
     /**
      * background color for TouchableOpacity
@@ -46,6 +46,7 @@ export type TouchableOpacityProps = Omit<RNTouchableOpacityProps, 'style'> &
     customValue?: any;
     ref?: any;
     style?: StyleProp<ViewStyle> | Animated.AnimatedProps<StyleProp<ViewStyle>>;
+    onPress?: (props: TouchableOpacityProps) => void;
   };
 
 type Props = BaseComponentInjectedProps &
