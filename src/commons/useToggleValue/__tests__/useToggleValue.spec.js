ff361c91968a127fbd7aea23c6f24e2dc362b63e
Mendy <mendye@wix.com>
2020-11-04 09:17:59 +0200

    fix test wrong import

--- src/commons/useToggleValue/__tests__/useToggleValue.spec.js
+++ src/commons/useToggleValue/__tests__/useToggleValue.spec.js
@@ -1,5 +1,5 @@
 import {renderHook, act} from '@testing-library/react-hooks';
-import useToggleValue from '../useToggleValue';
+import useToggleValue from '../index';
 
 describe('useOrientation hook tests', () => {
   const makeSUT = (initial, secondary) => {
