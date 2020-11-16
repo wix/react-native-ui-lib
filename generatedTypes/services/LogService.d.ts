2a31e81be907c03f08bebb3ca5a4e6a09fc415aa
Miki Leib <michaelle@wix.com>
2020-11-16 10:54:59 +0200

    LogService - add a deprecation log without the newProp

--- generatedTypes/services/LogService.d.ts
+++ generatedTypes/services/LogService.d.ts
@@ -1,9 +1,9 @@
 declare function warn(message: string): void;
-declare function deprecationWarn({ component, oldProp, newProp }: ({
+declare function deprecationWarn({ component, oldProp, newProp }: {
     component: string;
     oldProp: string;
-    newProp: string;
-})): void;
+    newProp?: string;
+}): void;
 declare const _default: {
     warn: typeof warn;
     deprecationWarn: typeof deprecationWarn;
--- src/services/LogService.ts
+++ src/services/LogService.ts
@@ -4,8 +4,11 @@ function warn(message: string) {
   }
 }
 
-function deprecationWarn({component, oldProp, newProp}: ({component: string, oldProp: string, newProp: string})) {
-  const message = `uilib's ${component} "${oldProp}" prop will be deprecated soon, please use the "${newProp}" prop instead`;
+function deprecationWarn({component, oldProp, newProp}: {component: string; oldProp: string; newProp?: string}) {
+  const message = newProp
+    ? `uilib's ${component} "${oldProp}" prop will be deprecated soon, please use the "${newProp}" prop instead`
+    : `uilib's ${component} "${oldProp}" prop will be deprecated soon, please stop using it`
+    ;
   warn(message);
 }
 
