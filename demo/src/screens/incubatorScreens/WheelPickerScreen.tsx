9ba7be13bacded7682b13e0e803430ccf4fe0db8
Mendy <mendye@wix.com>
2020-12-02 18:29:44 +0200

    update demo screen according to the new api

--- demo/src/screens/incubatorScreens/WheelPickerScreen.tsx
+++ demo/src/screens/incubatorScreens/WheelPickerScreen.tsx
@@ -1,7 +1,7 @@
 import React, {useCallback} from 'react';
-import {View, Text, Incubator, Typography, Colors} from 'react-native-ui-lib';
+import {View, Text, Incubator, Colors} from 'react-native-ui-lib';
 import _ from 'lodash';
-import { ItemProps } from 'src/incubator/WheelPicker/Item';
+import {ItemProps} from 'src/incubator/WheelPicker/Item';
 
 // Months
 const months = [
@@ -28,18 +28,17 @@ export default () => {
     console.log(item, index);
   }, []);
 
-  const activeFont = Typography.text60BO;
-  const inactiveFont = Typography.text60R;
-  const activeTextColor = Colors.primary;
-  const inactiveTextColor = Colors.grey20;
-
   return (
     <View flex padding-page>
       <Text h1>Wheel Picker</Text>
       <View flex centerV centerH paddingT-page>
-        
         <Text h3>Months</Text>
-        <Incubator.WheelPicker onChange={onChange} activeItemTextStyle={{...activeTextColor, ...activeFont}} inactiveItemTextStyle={{color: inactiveTextColor, ...inactiveFont}} items={_.map(months, i => ({text: i, value: i}))} />
+        <Incubator.WheelPicker
+          onChange={onChange}
+          selectedTextColor={Colors.primary}
+          unselectedTextStyle={Colors.grey20}
+          items={_.map(months, i => ({text: i, value: i}))}
+        />
 
         <Text h3 marginT-s5>Years</Text>
         <Incubator.WheelPicker onChange={onChange} items={_.map(years, i => ({text: '' + i, value: i}))} />
