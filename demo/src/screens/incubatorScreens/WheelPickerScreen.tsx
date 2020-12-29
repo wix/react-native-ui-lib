1ce49581e123b3adfe4c2e3a084e31e72b90b7f4
Mendy <mendye@wix.com>
2020-12-29 16:37:08 +0200

    Fixing demo screen

--- demo/src/screens/incubatorScreens/WheelPickerScreen.tsx
+++ demo/src/screens/incubatorScreens/WheelPickerScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import {View, Text, Incubator, Colors, Typography} from 'react-native-ui-lib';
 import _ from 'lodash';
 import {ItemProps} from 'src/incubator/WheelPicker/Item';
@@ -23,8 +23,18 @@ const months = [
 const years = _.times(2020, i => i);
 
 export default () => {
+  const [selectedValue, setSelectedValue] = useState<string | undefined>('February');
+
   const onChange = useCallback((item: string | undefined, index: number) => {
-    console.log(item, index);
+    setSelectedValue(item);
+  }, []);
+
+  const getMonths = useCallback(() => {
+    return _.map(months, value => ({label: value, value: value}));
+  }, []);
+
+  const getYears = useCallback(() => {
+    return _.map(years, i => ({label: '' + i, value: i}));
   }, []);
 
   return (
@@ -38,13 +48,14 @@ export default () => {
             onValueChange={onChange}
             activeTextColor={Colors.primary}
             inactiveTextColor={Colors.grey20}
-            items={_.map(months, i => ({label: i, value: i}))}
-            textStyle={{...Typography.text60R}}
+            items={getMonths()}
+            textStyle={Typography.text60R}
+            selectedValue={selectedValue}
           />
         
         <Text h3>Years</Text>
         <View height={300} width={'100%'}>
-          <Incubator.WheelPicker onValueChange={onChange} items={_.map(years, i => ({label: '' + i, value: i}))} />
+          <Incubator.WheelPicker onValueChange={onChange} items={getYears()} />
         </View>
       </View>
     </View>
