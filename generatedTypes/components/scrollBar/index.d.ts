875ad4befea3521c414ed17f117da99e72abe634
Inbal Tish <inbalti@wix.com>
2020-12-30 14:24:59 +0200

    generating types

--- generatedTypes/components/scrollBar/index.d.ts
+++ generatedTypes/components/scrollBar/index.d.ts
@@ -9,7 +9,7 @@ export interface ScrollBarProps extends FlatListProps<any> {
     /**
      * The element to use as a container, instead of a View
      */
-    containerView?: JSX.Element;
+    containerView?: object;
     /**
      * The props to pass the container
      */
