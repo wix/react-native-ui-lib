de9e3e9f9308412169c20a3def698d490c5e1684
Mendy <mendye@wix.com>
2020-12-31 09:49:59 +0200

    added unittests to cover useDidUpdate hook

---
+++ src/hooks/useDidUpdate/__tests__/useDidUpdate.spec.js
@@ -0,0 +1,69 @@
+import {renderHook, act} from '@testing-library/react-hooks';
+import useDidUpdate from '../index';
+
+describe('useDidUpdate hook tests', () => {
+  it('Expect action to not be called when the dependencies array value hasn\'t change', () => {
+    const action = jest.fn();
+    const dependencies = [false, 'false'];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    rerender();
+    rerender();
+    rerender();
+    expect(action.mock.calls.length).toBe(0);
+  });
+
+  it('Expect action to be called when dependencies array has change', () => {
+    const action = jest.fn();
+    let dependencies = [false, 'YES YES', 1];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    dependencies = [false, 'NO NO', 1];
+    rerender();
+    expect(action.mock.calls.length).toBe(1);
+  });
+
+  it('Expect action to be called when dependencies array has change', () => {
+    const action = jest.fn();
+    let dependencies = [false, 'YES YES', 1];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    dependencies = [false, 'NO NO', 1];
+    rerender();
+    expect(action.mock.calls.length).toBe(1);
+  });
+
+  it('Expect action to be called when dependencies array has change', () => {
+    const action = jest.fn();
+    let dependencies = [false, 'YES YES', 1];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    dependencies = [false, 'NO NO', 1];
+    rerender();
+    expect(action.mock.calls.length).toBe(1);
+  });
+
+  it('Expect action to be called when object value in dependencies array has change', () => {
+    const action = jest.fn();
+    let dependencies = [false, [1, 2], {name: 'AAA', address: {number: 10}}];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    dependencies = [false, [1, 2], {name: 'BBB'}];
+    rerender();
+    expect(action.mock.calls.length).toBe(1);
+    
+    dependencies = [false, [1, 2], {name: 'AAA', address: {number: 20}}];
+    rerender();
+    expect(action.mock.calls.length).toBe(2);
+  });
+
+  it('Expect action to be called when value in array dependencies array has change', () => {
+    const action = jest.fn();
+    let dependencies = [false, [1, [2]], {name: 'AAA'}];
+    const {rerender} = renderHook(() => useDidUpdate(action, dependencies));
+
+    dependencies = [false, [1, [3]], {name: 'AAA', address: {number: 20}}];
+    rerender();
+    expect(action.mock.calls.length).toBe(1);
+  });
+});
