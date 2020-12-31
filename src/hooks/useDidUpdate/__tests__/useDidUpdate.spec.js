3cc666fbd53e825376e6f7852e7e6f9f1689735b
Mendy <mendye@wix.com>
2020-12-31 10:12:50 +0200

    remove unused

--- src/hooks/useDidUpdate/__tests__/useDidUpdate.spec.js
+++ src/hooks/useDidUpdate/__tests__/useDidUpdate.spec.js
@@ -1,4 +1,4 @@
-import {renderHook, act} from '@testing-library/react-hooks';
+import {renderHook} from '@testing-library/react-hooks';
 import useDidUpdate from '../index';
 
 describe('useDidUpdate hook tests', () => {
