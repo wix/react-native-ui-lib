b6d28e97997a0af096dc51ed76a968c38b806a88
Mendy <mendye@wix.com>
2021-01-05 14:05:34 +0200

    Fix dependencies array to be flat

--- src/hooks/useDidUpdate/index.ts
+++ src/hooks/useDidUpdate/index.ts
@@ -5,18 +5,13 @@ import _ from 'lodash';
  * This hook avoid calling useEffect on the initial value of his dependency array
  */
 const useDidUpdate = (callback: () => void, dep: [any]) => {
-  const isMounted = useRef<boolean>(false);
+  const didMountRef = useRef(false)
 
   useEffect(() => {
-    if (!isMounted.current) {
-      return;
-    }
-    callback();
-  }, [dep]);
-
-  useEffect(() => {
-    isMounted.current = true;
-  }, []);
+    if (didMountRef.current) {
+      callback()
+    } else didMountRef.current = true
+  }, dep);
 };
 
 export default useDidUpdate;
