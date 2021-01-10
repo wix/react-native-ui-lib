7bbd545a652240117176363c2c0f3008f2ef7821
Mendy <mendye@wix.com>
2021-01-10 18:09:45 +0200

    added datetime-picker custom input demo

--- demo/src/screens/componentScreens/DateTimePickerScreen.js
+++ demo/src/screens/componentScreens/DateTimePickerScreen.js
@@ -1,7 +1,32 @@
 import React, {Component} from 'react';
-import {DateTimePicker, View, Text} from 'react-native-ui-lib'; // eslint-disable-line
+import {DateTimePicker, View, Text, Button} from 'react-native-ui-lib'; // eslint-disable-line
 
 export default class DateTimePickerScreen extends Component {
+
+  getCustomInputValue = value => {
+    if (!value) {
+      return '';
+    }
+    return value.includes(new Date().getFullYear() + 1) ? 'Next Year' : value;
+  };
+
+  renderCustomInput = (props, toggle) => {
+    const {value} = props;
+    return (
+      <View>
+        <Button
+          label={'Select a date'}
+          style={{backgroundColor: 'red', height: 40, width: '100%'}}
+          {...props}
+          onPress={() => {
+            toggle(true);
+          }}
+        />
+        <Text>{this.getCustomInputValue(value)}</Text>
+      </View>
+    );
+  };
+
   render() {
     return (
       <View flex padding-s5>
@@ -10,8 +35,8 @@ export default class DateTimePickerScreen extends Component {
           containerStyle={{marginVertical: 20}}
           title={'Date'}
           placeholder={'Select a date'}
-          // dateFormat={'MMM D, YYYY'}
-          // value={new Date('October 13, 2014')}
+          // timeFormat={'h:mm A'}
+          // value={new Date('2015-03-25T12:00:00-06:30')}
         />
         <DateTimePicker
           mode={'time'}
@@ -36,6 +61,17 @@ export default class DateTimePickerScreen extends Component {
           title={'Time'}
           placeholder={'Select time'}
           value={new Date('2015-03-25T12:00:00-06:30')}
+        />
+        <Text text60R marginT-40>
+          Custom Input
+        </Text>
+        <DateTimePicker
+          containerStyle={{marginVertical: 20}}
+          title={'Date'}
+          placeholder={'Select a date'}
+          renderExpandableInput={this.renderCustomInput}
+          // timeFormat={'h:mm A'}
+          // value={new Date('2015-03-25T12:00:00-06:30')}
         />
       </View>
     );
