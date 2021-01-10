b7c32c3da12248965f828f7ff4a9102eb2709a40
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-01-10 16:10:04 +0200

    Add more tests for component deprecation (commented for now)

--- eslint-rules/tests/lib/rules/component-deprecation.js
+++ eslint-rules/tests/lib/rules/component-deprecation.js
@@ -10,8 +10,6 @@ RuleTester.setDefaultConfig({
 const ruleTester = new RuleTester();
 
 const ruleOptions = [{deprecations: deprecationsJson}];
-const validExample = "const test = <Avatar source={{uri: 'some_uri_string'}}/>";
-const validImportExample = "import {Avatar} from 'another-module'; const test = <Avatar url={'some_uri_string'}/>";
 const invalidExample = "import {Avatar} from 'module-with-deprecations'; const test = <Avatar url={'some_uri_string'}/>";
 
 // NOTE: Deprecated components (not prop deprecation) will error at import and again if used as jsx tag.
@@ -19,11 +17,40 @@ ruleTester.run('component-deprecation', rule, {
   valid: [
     {
       options: ruleOptions,
-      code: validExample,
+      code: `
+const Avatar = require('another-module').Avatar;
+const test = <Avatar source={{uri: 'some_uri_string'}}/>;
+`,
     },
     {
       options: ruleOptions,
-      code: validImportExample,
+      code: `
+const {Avatar, List} = require('another-module');
+const test = <Avatar source={{uri: 'some_uri_string'}}/>;
+`,
+    },
+    {
+      options: ruleOptions,
+      code: `
+const module = require('another-module');
+const {Avatar, TextField} = module;
+const test1 = <Avatar source={{uri: 'some_uri_string'}}/>;
+const test2 = <TextField>Bla</TextField>;
+`,
+    },
+    {
+      options: ruleOptions,
+      code: `
+import {Avatar, TextField} from 'another-module';
+const test1 = <Avatar url={'some_uri_string'}/>;
+const test2 = <TextField>Bla</TextField>;
+`,
+    },
+    {
+      options: ruleOptions,
+      code: `
+import {List} from 'module-with-deprecations';
+<List/>`,
     },
   ],
   invalid: [
@@ -63,9 +90,63 @@ ruleTester.run('component-deprecation', rule, {
     },
     {
       options: ruleOptions,
-      code:
-        'import {Button} from \'module-with-deprecations\'; const props = {text: "button", color: "red"}; <Button {...props} value="value"/>', // eslint-disable-line
+      code: `
+import {Button} from 'module-with-deprecations';
+const props = {text: "button", color: "red"};
+<Button {...props} value="value"/>`,
       errors: [{message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import {Button as B} from 'module-with-deprecations';
+// const props = {text: "button", color: "red"};
+// <B {...props} value="value"/>`,
+//       errors: [{message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
+//     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import * as module from 'module-with-deprecations';
+// const {Button} = module;
+// const props = {text: "button", color: "red"};
+// <Button {...props} value="value"/>`,
+//       errors: [{message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
+//     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import * as module from 'module-with-deprecations';
+// const props = {text: "button", color: "red"};
+// <module.Button {...props} value="value"/>`,
+//       errors: [{message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
+//     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import * as module from 'module-with-deprecations';
+// <module.List.Part/>`,
+//       errors: [{message: "The 'List.Part' component is deprecated. Please use the 'List.Item' component instead."}],
+//     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import {List} from 'another-module-with-deprecations';
+// <List/>`,
+//       errors: [{message: "The 'List' component is deprecated. Please use the 'ListList' component instead."}],
+//     },
+//     {
+//       options: ruleOptions,
+//       code: `
+// import {View, Button, TextInput} from 'module-with-deprecations';
+// <View>
+//   <Button text="my button"/>
+//   <TextInput placeholder="first name"/>
+// </View>`,
+//       errors: [
+//         {message: "The 'TextInput' component is deprecated. Please use the 'TextField' component instead."},
+//         {message: "The 'TextInput' component is deprecated. Please use the 'TextField' component instead."},
+//         {message: "The 'Button' component's prop 'text' is deprecated. Please use the 'label' prop instead."}],
+//     },
   ],
 });
