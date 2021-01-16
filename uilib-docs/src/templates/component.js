d565c14c4315def48826279dafcac26052e2eb53
Ethan Sharabi <ethans@wix.com>
2021-01-16 10:55:21 +0200

    Update components page UI and reorganize

--- uilib-docs/src/templates/component.js
+++ uilib-docs/src/templates/component.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 import Link from 'gatsby-link';
 
 import './components.scss';
+import importantIcon from '../images/important.svg';
 
 const IMAGE_TYPES = {
   GIF: 'GIF',
@@ -47,24 +48,14 @@ export default class ComponentTemplate extends Component {
 
     return _.map(extendedComponents, (component, index) => {
       const isLast = index === _.size(extendedComponents) - 1;
-      const text = <b>{`${component}${!isLast ? ', ' : ''}`}</b>;
-      const extendedComponent = _.find(
-        allComponents,
-        c => c.node.displayName.trim() === component.trim()
-      );
-      const path =
-        !extendedComponent && componentInfo.extendsLink
-          ? componentInfo.extendsLink
-          : `/docs/${component}`;
+      const text = `${component}${!isLast ? ', ' : ''}`;
+      const extendedComponent = _.find(allComponents, c => c.node.displayName.trim() === component.trim());
+      const path = !extendedComponent && componentInfo.extendsLink ? componentInfo.extendsLink : `/docs/${component}`;
 
       return (
         <span className="inline" key={component}>
           {!extendedComponent && componentInfo.extendsLink ? (
-            <a
-              href={componentInfo.extendsLink}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
+            <a href={componentInfo.extendsLink} rel="noopener noreferrer" target="_blank">
               {text}
             </a>
           ) : (
@@ -78,25 +69,11 @@ export default class ComponentTemplate extends Component {
   }
 
   renderImage(image, index) {
-    return (
-      <img
-        key={index}
-        alt={''}
-        src={image}
-        style={{marginRight: 20, width: 320, border: '1px solid black'}}
-      />
-    );
+    return <img key={index} alt={''} src={image} style={{marginRight: 20, width: 320, border: '1px solid black'}} />;
   }
 
   renderGif(image, index) {
-    return (
-      <img
-        key={index}
-        alt={''}
-        src={image}
-        style={{marginRight: 20, width: 320}}
-      />
-    );
+    return <img key={index} alt={''} src={image} style={{marginRight: 20, width: 320}} />;
   }
 
   renderImages(images, type) {
@@ -108,99 +85,129 @@ export default class ComponentTemplate extends Component {
     }
   }
 
+  renderModifiers(componentInfo) {
+    const modifiers = _.split(componentInfo.modifiers, ',');
+
+    if (!_.isEmpty(modifiers)) {
+      return (
+        <div className="modifiers">
+          <span className="title">
+            Supported <Link to="/foundation/modifiers">Modifiers</Link>
+            <span className="tooltip">
+              Quick useful props that help with styling your component. Read more at the link.
+              {/* Read more about modifiers <Link to="/foundation/modifiers">here</Link>. */}
+            </span>
+          </span>
+          <ul className="modifiers-list">
+            {_.map(modifiers, modifier => (
+              <li>{modifier}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+
+  renderExtends(componentInfo, selectedComponent) {
+    const extendLinks = this.renderLink(componentInfo);
+
+    if (!_.isEmpty(extendLinks)) {
+      return (
+        <div className="extend-section">
+          <span className="title">
+            Extends
+            <span className="tooltip">
+              {selectedComponent.displayName} support passing these components' props as well.
+            </span>
+          </span>
+
+          <ul>
+            {_.map(extendLinks, link => (
+              <li className="link">{link}</li>
+            ))}
+          </ul>
+
+          {/* {this.renderLink(componentInfo)} */}
+          {/* <div>(meaning you can pass the super component's props as well).</div> */}
+        </div>
+      );
+    }
+  }
+
   renderImportant(componentInfo) {
-    return (
-      <div alt={''} style={{marginBottom: 10}}>
-        <span style={{fontWeight: '700'}}>IMPORTANT: </span>{' '}
-        {componentInfo.important} &nbsp;
-        {componentInfo.importantLink && (
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={componentInfo.importantLink}
-          >
-            here
-          </a>
-        )}
-      </div>
-    );
+    if (componentInfo.important) {
+      return (
+        <div alt={''} style={{marginBottom: 10}}>
+          {componentInfo.important}
+          {componentInfo.importantLink && (
+            <a target="_blank" rel="noopener noreferrer" href={componentInfo.importantLink}>
+              here
+            </a>
+          )}
+        </div>
+      );
+    }
   }
 
-  renderNote(note, index) {
-    return (
-      <div key={index} alt={''} style={{marginBottom: 10}}>
-        {note}
-      </div>
-    );
-  }
+  renderNotes(componentInfo) {
+    const notes = componentInfo.notes ? componentInfo.notes.split(';') : undefined;
+    const shouldRenderNotes = !_.isEmpty(notes) || componentInfo.important;
+    if (shouldRenderNotes) {
+      return (
+        <div className="notes">
+          <span className="title">
+            <img src={importantIcon} /> Important
+          </span>
+          {_.map(notes, (note, i) => (
+            <div key={i}>{note}</div>
+          ))}
 
-  renderNotes(notes) {
-    return notes.map(this.renderNote);
+          {this.renderImportant(componentInfo)}
+        </div>
+      );
+    }
   }
 
   renderComponentPage() {
     const {pageContext} = this.props;
     const selectedComponent = pageContext.componentNode;
     const componentInfo = this.extractComponentsInfo(selectedComponent);
-    const componentProps = _.orderBy(_.get(selectedComponent, 'props'), prop =>
-      prop.name.toLowerCase()
-    );
+    const componentProps = _.orderBy(_.get(selectedComponent, 'props'), prop => prop.name.toLowerCase());
     const gifs = componentInfo.gif ? componentInfo.gif.split(',') : undefined;
-    const imgs = componentInfo.image
-      ? componentInfo.image.split(',')
-      : undefined;
-    const notes = componentInfo.notes
-      ? componentInfo.notes.split(';')
-      : undefined;
-      
+    const imgs = componentInfo.image ? componentInfo.image.split(',') : undefined;
     const examples = _.split(componentInfo.example, ',');
+
+    const shouldRenderRightPart = componentInfo.modifiers || componentInfo.extends;
+
     return (
       <div className="docs-page">
         <div className="docs-page__content">
-          <h1 className="title inline">{selectedComponent.displayName}</h1>
-          {_.map(examples, example => {
-            return <span className="code-example">
-            (
-            <a
-              className="inline"
-              target="_blank"
-              rel="noopener noreferrer"
-              href={example}
-            >
-              code example
-            </a>
-            )
-          </span>
-          })}
-
-          <h3>{componentInfo.description}</h3>
-          {componentInfo.extends && (
+          <div className="component-header">
             <div>
-              Extends: {this.renderLink(componentInfo)}
+              <h1 className="title inline">{selectedComponent.displayName}</h1>
+              {_.map(examples, example => {
+                return (
+                  <span className="code-example">
+                    (
+                    <a className="inline" target="_blank" rel="noopener noreferrer" href={example}>
+                      code example
+                    </a>
+                    )
+                  </span>
+                );
+              })}
+
+              <p className="description">{componentInfo.description}</p>
+              {this.renderNotes(componentInfo)}
+            </div>
+            {shouldRenderRightPart && (
               <div>
-                (meaning you can pass the super component's props as well).
+                {this.renderModifiers(componentInfo)}
+                {this.renderExtends(componentInfo, selectedComponent)}
               </div>
-            </div>
-          )}
-          {componentInfo.modifiers && (
-            <div>
-              <p>
-                Supported modifiers: <b>{componentInfo.modifiers}</b>. <br />
-                Read more about modifiers{' '}
-                <Link to="/foundation/modifiers">here</Link>.
-              </p>
-            </div>
-          )}
+            )}
+          </div>
 
-          {componentInfo.notes && (
-            <div>
-              <h4 style={{marginBottom: 10}}>NOTES</h4>
-              {this.renderNotes(notes)}
-            </div>
-          )}
-          {componentInfo.important && (
-            <div>{this.renderImportant(componentInfo)}</div>
-          )}
           {componentProps.length > 0 && (
             <div>
               <h3>PROPS</h3>
@@ -212,9 +219,7 @@ export default class ComponentTemplate extends Component {
             <div className="container">
               <h3>EXAMPLE</h3>
               <div className="row">
-                <div className="col-sm-12 text-center">
-                  {this.renderImages(imgs, IMAGE_TYPES.PNG)}
-                </div>
+                <div className="col-sm-12 text-center">{this.renderImages(imgs, IMAGE_TYPES.PNG)}</div>
               </div>
             </div>
           )}
@@ -223,9 +228,7 @@ export default class ComponentTemplate extends Component {
             <div className="container">
               <h3>LIVE EXAMPLE</h3>
               <div className="row">
-                <div className="col-sm-12 text-center">
-                  {this.renderImages(gifs, IMAGE_TYPES.GIF)}
-                </div>
+                <div className="col-sm-12 text-center">{this.renderImages(gifs, IMAGE_TYPES.GIF)}</div>
               </div>
             </div>
           )}
