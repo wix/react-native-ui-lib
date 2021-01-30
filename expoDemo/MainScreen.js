e8f3ed54c2922fbe48e3b305bda5e5c9af04db0d
Ethan Sharabi <ethans@wix.com>
2021-01-30 10:27:09 +0200

    use useCallback for renderItem and renderSectionHeader

--- expoDemo/MainScreen.js
+++ expoDemo/MainScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import {SectionList, StyleSheet} from 'react-native';
 import {Colors, View, Text, TouchableOpacity, Spacings, Image, Assets, TextField} from 'react-native-ui-lib';
 import {menuStructure} from 'unicorn-demo-app';
@@ -13,6 +13,50 @@ const sections = _.map(menuStructure, (section, key) => {
 
 export default function MainScreen({navigation}) {
   const [searchText, setSearchText] = React.useState('');
+
+  const renderSectionHeader = useCallback(({section}) => {
+    if (!_.find(section.data, screen => _.includes(screen.title.toLowerCase(), searchText.toLowerCase()))) {
+      return null;
+    }
+
+    return (
+      <View paddingV-s1 paddingH-s5 bg-primary>
+        <Text text90BO uppercase grey70>
+          {section.key}
+        </Text>
+      </View>
+    );
+  },
+  [searchText]);
+
+  const renderItem = useCallback(({item}) => {
+    if (!_.includes(item.title.toLowerCase(), searchText.toLowerCase())) {
+      return null;
+    }
+
+    if (!item.title) {
+      return <View height={Spacings.s2} bg-grey70/>;
+    }
+
+    return (
+      <TouchableOpacity
+        activeOpacity={1}
+        key={item.title}
+        style={styles.sectionItem}
+        activeBackgroundColor={Colors.violet80}
+        onPress={() => {
+          // convert "unicorn.components.ActionBarScreen" -> "ActionBar"
+          const screenId = _.chain(item.screen).split('.').last().replace('Screen', '').value();
+          navigation.navigate(screenId);
+        }}
+      >
+        <Text grey10 text70M>
+          {item.title}
+        </Text>
+      </TouchableOpacity>
+    );
+  },
+  [searchText]);
 
   return (
     <View flex bg-white>
@@ -30,46 +74,8 @@ export default function MainScreen({navigation}) {
           />
         }
         sections={sections}
-        renderSectionHeader={({section}) => {
-          if (!_.find(section.data, screen => _.includes(screen.title.toLowerCase(), searchText.toLowerCase()))) {
-            return null;
-          }
-
-          return (
-            <View paddingV-s1 paddingH-s5 bg-primary>
-              <Text text90BO uppercase grey70>
-                {section.key}
-              </Text>
-            </View>
-          );
-        }}
-        renderItem={({item}) => {
-          if (!_.includes(item.title.toLowerCase(), searchText.toLowerCase())) {
-            return null;
-          }
-
-          if (!item.title) {
-            return <View height={Spacings.s2} bg-grey70/>;
-          }
-
-          return (
-            <TouchableOpacity
-              activeOpacity={1}
-              key={item.title}
-              style={styles.sectionItem}
-              activeBackgroundColor={Colors.violet80}
-              onPress={() => {
-                // convert "unicorn.components.ActionBarScreen" -> "ActionBar"
-                const screenId = _.chain(item.screen).split('.').last().replace('Screen', '').value();
-                navigation.navigate(screenId);
-              }}
-            >
-              <Text grey10 text70M>
-                {item.title}
-              </Text>
-            </TouchableOpacity>
-          );
-        }}
+        renderSectionHeader={renderSectionHeader}
+        renderItem={renderItem}
       />
     </View>
   );
