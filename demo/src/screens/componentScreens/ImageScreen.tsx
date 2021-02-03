9a5e5fbfcb7b4dd0fdfb9e9e17e9beaea6cac9c2
Inbal Tish <inbalti@wix.com>
2021-02-03 07:54:15 -0500

    ImageScreen - adding 'errorSource' example

--- demo/src/screens/componentScreens/ImageScreen.tsx
+++ demo/src/screens/componentScreens/ImageScreen.tsx
@@ -1,19 +1,19 @@
 import React, {Component} from 'react';
-import {View, Text, Image, Colors} from 'react-native-ui-lib';
+import {View, Text, Image, Colors, Assets} from 'react-native-ui-lib';
 import {renderBooleanOption, renderRadioGroup, renderSliderOption} from '../ExampleScreenPresenter';
 
-import cameraIcon from '../../assets/icons/cameraSelected.png';
 
 const IMAGE_URL =
   'https://images.pexels.com/photos/748837/pexels-photo-748837.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-
+const BROKEN_URL = 'file:///Desktop/website/img/cupcake.jpg';
 const DEFAULT_SIZE = 100;
 class ImageScreen extends Component {
   state = {
     cover: true,
     showOverlayContent: false,
     overlayType: 'none',
-    margin: 0
+    margin: 0,
+    showErrorImage: false
   };
 
   renderOverlayContent() {
@@ -35,19 +35,20 @@ class ImageScreen extends Component {
           </View>
         );
       } else {
-        return <Image style={{margin: 5}} source={cameraIcon}/>;
+        return <Image style={{margin: 5}} source={Assets.icons.demo.camera}/>;
       }
     }
   }
 
   render() {
-    const {cover, overlayType, margin} = this.state;
+    const {cover, overlayType, margin, showErrorImage} = this.state;
 
     return (
       <View flex>
         <View centerH height={250}>
           <Image
-            source={{uri: IMAGE_URL}}
+            source={{uri: showErrorImage ? BROKEN_URL : IMAGE_URL}}
+            errorSource={Assets.images.demo.brokenImage}
             cover={cover}
             overlayType={overlayType !== 'none' ? overlayType : undefined}
             style={!cover && {width: DEFAULT_SIZE, height: DEFAULT_SIZE}}
@@ -63,6 +64,7 @@ class ImageScreen extends Component {
               {renderBooleanOption.call(this, 'Show Overlay Content', 'showOverlayContent')}
               {renderRadioGroup.call(this, 'Overlay Type', 'overlayType', {none: 'none', ...Image.overlayTypes})}
               {renderSliderOption.call(this, 'Margin(margin-XX)', 'margin', {step: 4, min: 0, max: 40})}
+              {renderBooleanOption.call(this, 'Show Error Image', 'showErrorImage')}
             </View>
             <Text text40>Image Screen</Text>
           </View>
