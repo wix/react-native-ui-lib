c8ceb6ae801ed0d9a52bbffc2381759f58731c4f
Jonathan Moskovich <jonathanmo@wix.com>
2021-03-14 14:21:00 +0200

    Fix snippet test

--- snippetGenerator/src/test/suite/extension.test.ts
+++ snippetGenerator/src/test/suite/extension.test.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert';
-import '@types/jest';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
