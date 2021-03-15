b783de9ffebff5112f011536ff10136fde5c25af
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-03-15 20:04:04 +0200

    Mark as internal function

--- eslint-rules/lib/utils/debugUtils.js
+++ eslint-rules/lib/utils/debugUtils.js
@@ -1,18 +1,18 @@
 // stringify with no circular error
 function stringify(object) {
   if (object && typeof object === 'object') {
-    object = copyWithoutCircularReferences([object], object);
+    object = _copyWithoutCircularReferences([object], object);
   }
   return JSON.stringify(object);
 
-  function copyWithoutCircularReferences(references, object) {
+  function _copyWithoutCircularReferences(references, object) {
     const cleanObject = {};
     Object.keys(object).forEach(key => {
       const value = object[key];
       if (value && typeof value === 'object') {
         if (references.indexOf(value) < 0) {
           references.push(value);
-          cleanObject[key] = copyWithoutCircularReferences(references, value);
+          cleanObject[key] = _copyWithoutCircularReferences(references, value);
           references.pop();
         } else {
           cleanObject[key] = '###_Circular_###';
