54b7ccaf022d8dcdadb5388bd9f1a17e18f87331
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-03-16 13:37:42 +0200

    Infra/eslint v2 (#1193)
    * Add more tests for component deprecation (commented for now)
    
    * Lint rules major refactor (start of V2)
    
    * Remove OrganizationType
    
    * Split for readability
    
    * Remove extra line
    
    * Mark as internal function
    
    * Add a TODO to remove value
    
    * Review fixes

---
+++ eslint-rules/lib/utils/noHardCodedUtils.js
@@ -0,0 +1,22 @@
+const {findValueNodeOfIdentifier} = require('./generalUtils');
+
+function _isLiteral(type) {
+  return (type === 'Literal' || type === 'TemplateLiteral');
+}
+
+function findAndReportHardCodedValues(value, reporter, context, depthOfSearch = 4) {
+  if (depthOfSearch === 0) return;
+  if (value === undefined || value === false) return;
+  if (_isLiteral(value.type)) {
+    reporter(value);
+  } else if (value.type === 'ConditionalExpression') {
+    findAndReportHardCodedValues(value.consequent, reporter, context, depthOfSearch - 1);
+    findAndReportHardCodedValues(value.alternate, reporter, context, depthOfSearch - 1);
+  } else if (value.type === 'Identifier') {
+    findAndReportHardCodedValues(findValueNodeOfIdentifier(value.name, context), reporter, context, depthOfSearch - 1);
+  }
+}
+
+module.exports = {
+  findAndReportHardCodedValues
+};
