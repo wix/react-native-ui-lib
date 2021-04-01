25293a7de097451157f20ef554d4a224b124506d
Ethan Sharabi <ethans@wix.com>
2021-04-01 10:54:20 +0300

    Configure module-resolver plugin

--- babel.config.js
+++ babel.config.js
@@ -6,5 +6,18 @@ module.exports = {
     }
   },
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin']
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        // extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+        alias: {
+          style: './src/style',
+          helpers: './src/helpers'
+        }
+      }
+    ],
+    'react-native-reanimated/plugin'
+  ]
 };
