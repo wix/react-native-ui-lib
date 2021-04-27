591899238ad67ef9e81a190a452588c485b13244
Lidor Dafna <lidord@wix.com>
2021-04-27 13:10:42 +0300

    Add HapticType

--- src/services/HapticService.ts
+++ src/services/HapticService.ts
@@ -5,17 +5,17 @@ const options = {
   ignoreAndroidSystemSettings: false
 };
 
-const HapticMethods = [
-  'selection',
-  'impactLight',
-  'impactMedium',
-  'impactHeavy',
-  'notificationSuccess',
-  'notificationWarning',
-  'notificationError'
-];
+export enum HapticType {
+  selection = 'selection',
+  impactLight = 'impactLight',
+  impactMedium = 'impactMedium',
+  impactHeavy = 'impactHeavy',
+  notificationSuccess = 'notificationSuccess',
+  notificationWarning = 'notificationWarning',
+  notificationError = 'notificationError'
+}
 
-function triggerHaptic(hapticMethod: string, componentName: string) {
+function triggerHaptic(hapticMethod: HapticType, componentName: string) {
   if (HapticFeedbackPackage) {
     HapticFeedbackPackage.trigger(hapticMethod, options);
   } else {
@@ -24,6 +24,6 @@ function triggerHaptic(hapticMethod: string, componentName: string) {
 }
 
 export default {
-  HapticMethods,
+  HapticType,
   triggerHaptic
 };
