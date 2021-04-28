d4501f10bd27a3ab6675e47d78a4938e5df7c8ae
Ethan Sharabi <ethans@wix.com>
2021-04-28 10:25:25 +0300

    use PropsWithChildren in Carousel

--- generatedTypes/components/carousel/CarouselPresenter.d.ts
+++ generatedTypes/components/carousel/CarouselPresenter.d.ts
@@ -1,13 +1,9 @@
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import { CarouselProps, CarouselState } from './types';
-declare type PropsWithChildren = CarouselProps & {
-    children?: ReactNode;
-};
-export declare function getChildrenLength(props: PropsWithChildren): number;
+export declare function getChildrenLength(props: PropsWithChildren<CarouselProps>): number;
 export declare function calcOffset(props: CarouselProps, state: Omit<CarouselState, 'initialOffset' | 'prevProps'>): {
     x: number;
     y: number;
 };
 export declare function calcPageIndex(offset: number, props: CarouselProps, pageSize: number): number;
 export declare function isOutOfBounds(offset: number, props: CarouselProps, pageWidth: number): boolean;
-export {};
