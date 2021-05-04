977f6b6d9811cb53605b5ec98f7622cdd214356e
Ethan Sharabi <ethans@wix.com>
2021-05-04 17:32:50 +0300

    Fix issue with FloatingPlaceholderColor ignore default preset

--- src/incubator/TextField/Presenter.ts
+++ src/incubator/TextField/Presenter.ts
@@ -5,7 +5,7 @@ import {ColorType, Validator} from './types';
 import formValidators from './validators';
 
 export function getColorByState(color: ColorType, context?: ContextType) {
-  let finalColor: string | undefined = Colors.grey10;
+  let finalColor: string | undefined;
   if (_.isString(color)) {
     finalColor = color;
   } else if (_.isPlainObject(color)) {
