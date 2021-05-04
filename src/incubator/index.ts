5208133c5684367ad8a42aa59eaf68cd7364ca5a
Ethan Sharabi <ethans@wix.com>
2021-05-04 17:20:04 +0300

    Export Incubator.TextField typings (#1287)

--- src/incubator/index.ts
+++ src/incubator/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
 export {default as TabController} from './TabController';
-export {default as TextField} from './TextField';
+export {default as TextField, TextFieldProps} from './TextField';
 export {default as TouchableOpacity, TouchableOpacityProps} from './TouchableOpacity';
 export {default as WheelPicker, WheelPickerProps} from './WheelPicker';
