34aaae8e442c97a841c33d8e86a59cc92d6225be
Mendy <mendye@wix.com>
2021-05-05 16:34:21 +0300

    Remove duplicated test, change test to match new api

--- src/components/featureHighlight/__tests__/index.spec.js
+++ src/components/featureHighlight/__tests__/index.spec.js
@@ -45,48 +45,22 @@ describe('FeatureHighlight', () => {
       const getTargetMock = jest.fn();
       getTargetMock.mockReturnValue(mockTarget);
       const uut = new FeatureHighlight({getTarget: getTargetMock});
-      jest.spyOn(uut, 'findTargetNode').mockImplementation(() => 23);
-      jest.spyOn(uut, 'setState').mockImplementation(() => {});
+      jest.spyOn(FeatureHighlight, 'getDerivedStateFromProps');
+      jest.spyOn(FeatureHighlight, 'findTargetNode').mockImplementation(() => 23);
+
       jest.useFakeTimers();
 
       // Act
-      uut.setTargetPosition();
+      FeatureHighlight.getDerivedStateFromProps({getTarget: getTargetMock});
+      uut.setTargetPosition({getTarget: getTargetMock});
 
       // Assert
-      expect(uut.findTargetNode).toHaveBeenCalledWith(mockTarget);
-      expect(uut.setState).toHaveBeenCalledWith({node: 23});
-
+      expect(FeatureHighlight.getDerivedStateFromProps).toHaveBeenCalledWith({getTarget: getTargetMock});
       expect(mockTarget.measureInWindow).not.toBeCalled();
-      // expect(setTimeout).toHaveBeenCalledTimes(0);
+      expect(FeatureHighlight.findTargetNode).toHaveBeenCalledWith(mockTarget);
+
       jest.runAllTimers();
-      // jest.advanceTimersByTime(0); // available in Jest 21
-      expect(setTimeout).toHaveBeenCalledTimes(1);
-      expect(mockTarget.measureInWindow).toBeCalled();
-    });
-  });
-
-  describe('setTargetPosition', () => {
-    it('targetPosition should be equal to component position', () => {
-      // Arrange
-      const mockTarget = {measureInWindow: jest.fn()};
-      const getTargetMock = jest.fn();
-      getTargetMock.mockReturnValue(mockTarget);
-      const uut = new FeatureHighlight({getTarget: getTargetMock});
-      jest.spyOn(uut, 'findTargetNode').mockImplementation(() => 23);
-      jest.spyOn(uut, 'setState').mockImplementation(() => {});
-      jest.useFakeTimers();
-
-      // Act
-      uut.setTargetPosition();
-
-      // Assert
-      expect(uut.findTargetNode).toHaveBeenCalledWith(mockTarget);
-      expect(uut.setState).toHaveBeenCalledWith({node: 23});
-
-      expect(mockTarget.measureInWindow).not.toBeCalled();
-      // expect(setTimeout).toHaveBeenCalledTimes(0);
-      jest.runAllTimers();
-      // jest.advanceTimersByTime(0); // available in Jest 21
+      
       expect(setTimeout).toHaveBeenCalledTimes(1);
       expect(mockTarget.measureInWindow).toBeCalled();
     });
