ab60fff3b2a40513caae04cfd3ca127b1c0f3c33
Ethan Sharabi <ethans@wix.com>
2021-05-11 10:34:07 +0300

    Fix various typing issues (#1291)
    * Fix various typing issues
    
    * Omit RNText style for our own style prop

--- src/components/text/index.tsx
+++ src/components/text/index.tsx
@@ -1,5 +1,5 @@
 import React, {PureComponent} from 'react';
-import {Text as RNText, StyleSheet, TextProps as RNTextProps, TextStyle, Animated} from 'react-native';
+import {Text as RNText, StyleSheet, TextProps as RNTextProps, TextStyle, Animated, StyleProp} from 'react-native';
 import _ from 'lodash';
 import {
   asBaseComponent,
@@ -12,7 +12,7 @@ import {
 } from '../../commons/new';
 import {Colors} from 'style';
 
-export type TextProps = RNTextProps & TypographyModifiers & ColorsModifiers & MarginModifiers & {
+export type TextProps = Omit<RNTextProps, 'style'> & TypographyModifiers & ColorsModifiers & MarginModifiers & {
   /**
    * color of the text
    */
@@ -38,6 +38,7 @@ export type TextProps = RNTextProps & TypographyModifiers & ColorsModifiers & Ma
    */
   animated?: boolean;
   textAlign?: string;
+  style?: StyleProp<TextStyle | Animated.AnimatedProps<TextStyle>>;
 }
 export type TextPropTypes = TextProps; //TODO: remove after ComponentPropTypes deprecation;
 
