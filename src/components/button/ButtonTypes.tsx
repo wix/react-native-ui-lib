5090482deacf7e0c1c9a9b1ffb0bbb71705af49e
Ethan Sharabi <ethans@wix.com>
2021-05-30 11:40:49 +0300

    Show docs showcase on mobile and use carousel for showcase (#1316)

--- src/components/button/ButtonTypes.tsx
+++ src/components/button/ButtonTypes.tsx
@@ -161,7 +161,8 @@ export const DEFAULT_PROPS = {
  * @extends: TouchableOpacity
  * @extendsLink: docs/TouchableOpacity
  * @modifiers: margin, background
- * @gif: https://media.giphy.com/media/xULW8j5WzsuPytqklq/giphy.gif
+ * @image: https://user-images.githubusercontent.com/1780255/119218638-173e3b00-baea-11eb-8e66-f5a23714871c.png, https://user-images.githubusercontent.com/1780255/119218639-173e3b00-baea-11eb-8b75-b6dde4e922c8.png, https://user-images.githubusercontent.com/1780255/119218636-16a5a480-baea-11eb-8501-33eebfe1f3e2.png, https://user-images.githubusercontent.com/1780255/119218637-16a5a480-baea-11eb-99d0-3620f2160ec5.png, https://user-images.githubusercontent.com/1780255/119218634-160d0e00-baea-11eb-8538-e78005cb3fa5.png, https://user-images.githubusercontent.com/1780255/119221003-d9471400-baf5-11eb-9066-0aabbe8d9022.png
+ * @gif: https://user-images.githubusercontent.com/1780255/119218630-11e0f080-baea-11eb-8e32-4470195cc42f.gif
  * @example: https://github.com/wix/react-native-ui-lib/blob/master/demo/src/screens/componentScreens/ButtonsScreen.tsx
  */
 // @ts-ignore 
