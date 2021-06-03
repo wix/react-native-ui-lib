6978c12e6f819e78286c2f7ccb43a45da6fcead6
Mendy <mendye@wix.com>
2021-06-03 10:44:10 +0300

    export action-sheet, typescript version

--- src/index.ts
+++ src/index.ts
@@ -51,6 +51,7 @@ export {default as Modal, ModalProps, ModalTopBarProps} from './components/modal
 export {default as PanGestureView, PanGestureViewProps} from './components/panningViews/panGestureView';
 export {default as PanningContext} from './components/panningViews/panningContext';
 export {default as asPanViewConsumer} from './components/panningViews/asPanViewConsumer';
+export {default as ActionSheet} from './components/actionSheet';
 export {
   default as PanningProvider,
   PanningDirections,
@@ -69,7 +70,7 @@ export {default as Hint, HintProps} from './components/hint';
 
 //================ Manual typings (all those exports should be removed one day) ==========
 export {
-  ActionSheet, ConnectionStatusBar, ChipsInput,
+  ConnectionStatusBar, ChipsInput,
   FeatureHighlight, BaseInput, TextArea, TextField, MaskedInput, ListItem, Picker,
   PickerProps, ProgressBar, Slider, GradientSlider, ColorSliderGroup, Stepper,
   TagsInput, SharedTransition, StackAggregator, Toast, WheelPickerDialog, Assets,
