93d5f7fd250996c50652aeed88afaf6cace12184
Ethan Sharabi <ethans@wix.com>
2021-07-26 14:50:29 +0300

    Fix typings and failing build
    (cherry picked from commit 3ebe272d9d92ba092079d51d6d8e8608d45b4e8a)

--- src/components/stackAggregator/index.tsx
+++ src/components/stackAggregator/index.tsx
@@ -103,10 +103,10 @@ class StackAggregator extends PureComponent<StackAggregatorProps, State> {
     }
   }
 
-  getAnimatedScales = (): Animated.Value[] => {
+  getAnimatedScales = () => {
     return React.Children.map(this.props.children, (_item, index) => {
       return new Animated.Value(this.getItemScale(index));
-    });
+    }) as Animated.Value[];
   }
 
   getItemScale = (index: number) => {
@@ -299,7 +299,7 @@ class StackAggregator extends PureComponent<StackAggregatorProps, State> {
           </Animated.View>
 
           {React.Children.map(children, (item, index) => {
-            return this.renderItem(item, index);
+            return this.renderItem(item as JSX.Element | JSX.Element[], index);
           })}
 
           {collapsed && (
