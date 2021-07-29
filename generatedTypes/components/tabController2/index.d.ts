4dfc2104ac06c42a3dd063646bfa0ca8cc2f556f
Mendy <mendye@wix.com>
2021-07-29 11:09:15 +0300

    ts build changes

--- generatedTypes/components/tabController2/index.d.ts
+++ generatedTypes/components/tabController2/index.d.ts
@@ -13,7 +13,7 @@ export interface TabControllerProps {
     /**
      * DEPRECATED: use initialIndex instead
      */
-     selectedIndex?: number;
+    selectedIndex?: number;
     /**
      * callback for when index has change (will not be called on ignored items)
      */
@@ -34,7 +34,7 @@ export interface TabControllerProps {
  * @important: On Android, if using react-native-navigation, make sure to wrap your screen with gestureHandlerRootHOC
  * @importantLink: https://kmagiera.github.io/react-native-gesture-handler/docs/getting-started.html#with-wix-react-native-navigation-https-githubcom-wix-react-native-navigation
  */
-declare function TabController({ selectedIndex, asCarousel, items, onChangeIndex, carouselPageWidth, children }: PropsWithChildren<TabControllerProps>): JSX.Element;
+declare function TabController({ initialIndex, selectedIndex, asCarousel, items, onChangeIndex, carouselPageWidth, children }: PropsWithChildren<TabControllerProps>): JSX.Element;
 declare namespace TabController {
     var TabBar: React.ComponentClass<import("./TabBar").TabControllerBarProps & {
         useCustomTheme?: boolean | undefined;
