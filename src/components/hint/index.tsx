05ebf9c73cb9c26cd5b24db5206ef565e5c90d00
Ethan Sharabi <ethans@wix.com>
2021-08-08 10:34:55 +0300

    Fix/hint missing color (#1450)
    * Fix
    
    * Fix color issue in Hint

--- src/components/hint/index.tsx
+++ src/components/hint/index.tsx
@@ -423,7 +423,7 @@ class Hint extends Component<HintProps, HintState> {
   // }
 
   renderHintTip() {
-    const {position, color} = this.props;
+    const {position, color = DEFAULT_COLOR} = this.props;
     const source = this.useSideTip ? sideTip : middleTip;
     const flipVertically = position === HintPositions.TOP;
     const flipHorizontally = this.getTargetPositionOnScreen() === TARGET_POSITIONS.RIGHT;
@@ -433,7 +433,7 @@ class Hint extends Component<HintProps, HintState> {
 
     return (
       <Image
-        tintColor={color || DEFAULT_COLOR}
+        tintColor={color}
         source={source}
         style={[styles.hintTip, this.getTipPosition(), flipStyle]}
       />
@@ -447,7 +447,7 @@ class Hint extends Component<HintProps, HintState> {
       icon, 
       iconStyle, 
       borderRadius, 
-      color, 
+      color = DEFAULT_COLOR, 
       customContent, 
       removePaddings, 
       enableShadow,
@@ -464,7 +464,7 @@ class Hint extends Component<HintProps, HintState> {
           styles.hint,
           !removePaddings && styles.hintPaddings,
           visible && enableShadow && styles.containerShadow, 
-          {backgroundColor: color}, 
+          {backgroundColor: color},
           !_.isUndefined(borderRadius) && {borderRadius}
         ]}
         ref={this.setHintRef}
