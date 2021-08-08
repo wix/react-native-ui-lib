67f19ccb07a5543ce4e676a4ac294adb0becd1cd
Mendy Edri <mendye@wix.com>
2021-08-08 17:10:18 +0300

    Typescript/loader screen (#1458)
    * index.js -> index.tsx
    
    * migrate loader-screen to typescript
    
    * LoadingScreen (demo screen) to tsx
    
    * added missing exports
    
    * removed manual typings + added to generatedTypes

--- src/index.ts
+++ src/index.ts
@@ -78,6 +78,7 @@ export {default as Slider, SliderProps} from './components/slider';
 export {default as GradientSlider, GradientSliderProps} from './components/slider/GradientSlider';
 export {default as ColorSliderGroup, ColorSliderGroupProps} from './components/slider/ColorSliderGroup';
 export {default as LogService} from './services/LogService';
+export {default as LoaderScreen, LoaderScreenProps} from './components/loaderScreen';
 
 //================ Manual typings (all those exports should be removed one day) ==========
 export {
@@ -86,5 +87,5 @@ export {
   PickerProps, ProgressBar, Stepper,
   TagsInput, SharedTransition, Toast, WheelPickerDialog, Assets,
   BaseComponent, PureBaseComponent, UIComponent, forwardRef,
-  LoaderScreen, StateScreen, WheelPicker, WheelPickerProps
+  StateScreen, WheelPicker, WheelPickerProps
 } from '../typings';
