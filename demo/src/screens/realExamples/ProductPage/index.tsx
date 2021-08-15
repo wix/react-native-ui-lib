449377f53e49a456a4acf13842697db2d77774b3
maeschli <mariab@wix.com>
2021-08-15 16:50:13 +0300

    [Inspiration screens]: Changed Product page option items to pickers

--- demo/src/screens/realExamples/ProductPage/index.tsx
+++ demo/src/screens/realExamples/ProductPage/index.tsx
@@ -1,18 +1,18 @@
 import React, {Component} from 'react';
 import {ScrollView} from 'react-native';
-import {View, Text, Colors, TouchableOpacity, Image, ActionSheet, Button, Carousel} from 'react-native-ui-lib';
+import {View, Text, Colors, TouchableOpacity, Image, Button, Carousel, Picker, Dialog} from 'react-native-ui-lib';
 
-const colorOptions = {
-  red: {name: 'Red', color: Colors.red30},
-  green: {name: 'Green', color: Colors.green30},
-  blue: {name: 'Blue', color: Colors.blue30},
-};
+const colorOptions = [
+  {name: 'Red', color: Colors.red30},
+  {name: 'Green', color: Colors.green30},
+  {name: 'Blue', color: Colors.blue30},
+];
 
-const sizeOptions = {
-  s: {name: 'Small'},
-  m: {name: 'Medium'},
-  l: {name: 'Large'},
-};
+const sizeOptions = [
+  {name: 'Small'},
+  {name: 'Medium'},
+  {name: 'Large'},
+];
 
 const images = [
   'https://images.pexels.com/photos/3297502/pexels-photo-3297502.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
@@ -29,15 +29,35 @@ class Product extends Component {
   state = {
     isColor: false,
     isSize: false,
-    colorOption: colorOptions.red,
-    sizeOption: sizeOptions.s,
+    colorOption: colorOptions[0],
+    sizeOption: sizeOptions[0],
   };
   
   pickOption(opt: any) {
     this.setState({
-      colorOption: opt
+      colorOption: opt,
+      isColor: false
     });
   }
+  
+  renderDialog = (modalProps: any) => {
+    const {visible, children, toggleModal} = modalProps;
+    
+    return (
+      <Dialog
+        migrate
+        visible={visible}
+        onDismiss={() => toggleModal(false)}
+        width="90%"
+        height="40%"
+        bottom
+        useSafeArea
+        containerStyle={{backgroundColor: Colors.white, borderRadius: 12}}
+      >
+        <ScrollView>{children}</ScrollView>
+      </Dialog>
+    );
+  };
   
   render() {
     const {isColor, colorOption, isSize, sizeOption} = this.state;
@@ -73,49 +93,59 @@ class Product extends Component {
           </View>
           <Text text80 dark40 marginV-s1>SKU: 1234567890</Text>
           <Text text50L marginV-s2>$55.00</Text>
-      
-          <View marginV-s2 paddingV-s2 style={{borderColor: Colors.dark70, borderBottomWidth: 1}}>
-            <Text dark30>Color</Text>
-            <TouchableOpacity row centerV spread onPress={()=> this.setState({isColor: true})}>
-              <View row centerV>
-                <View style={{width: 14, height: 14, borderRadius: 12, marginRight: 6}} backgroundColor={colorOption.color}/>
-                <Text dark10 text70>{colorOption.name}</Text>
-              </View>
-              <Image source={require('../../../assets/icons/chevronDown.png')}/>
-            </TouchableOpacity>
-          </View>
-          <ActionSheet
-            title={'Select a Color'}
-            options={[
-              {label: colorOptions.red.name, onPress: () => this.pickOption(colorOptions.red)},
-              {label: colorOptions.green.name, onPress: () => this.pickOption(colorOptions.green)},
-              {label: colorOptions.blue.name, onPress: () => this.pickOption(colorOptions.blue)},
-            ]}
+  
+          <Picker
+            marginT-20
             visible={isColor}
-            onDismiss={()=> this.setState({isColor: false})}
-          />
-      
-          <View marginV-s2 paddingV-s2 style={{borderColor: Colors.dark70, borderBottomWidth: 1}}>
-            <Text dark30>Size</Text>
-            <TouchableOpacity row centerV spread onPress={()=> this.setState({isSize: true})}>
-              <View row centerV>
-                <Text dark10 text70>{sizeOption.name}</Text>
-              </View>
-              <Image source={require('../../../assets/icons/chevronDown.png')}/>
-            </TouchableOpacity>
-          </View>
-          <ActionSheet
-            title={'Select a Size'}
-            options={[
-              {label: sizeOptions.s.name, onPress: () => this.pickOption(sizeOptions.s)},
-              {label: sizeOptions.m.name, onPress: () => this.pickOption(sizeOptions.m)},
-              {label: sizeOptions.l.name, onPress: () => this.pickOption(sizeOptions.l)},
-            ]}
+            title="Color"
+            placeholder={colorOption.name}
+            onChange={()=>this.setState({isColor: true})}
+            rightIconSource={require('../../../assets/icons/chevronDown.png')}
+            // renderPicker={() => {
+            //   return (
+            //     <View flex marginV-s2 paddingV-s2 style={{borderColor: Colors.dark70, borderBottomWidth: 1}}>
+            //       <Text dark30>Color</Text>
+            //       <TouchableOpacity row centerV spread>
+            //         <View row centerV>
+            //           <View style={{width: 14, height: 14, borderRadius: 12, marginRight: 6}} backgroundColor={colorOption.color}/>
+            //           <Text dark10 text70>{colorOption.name}</Text>
+            //         </View>
+            //         <Image source={require('../../../assets/icons/chevronDown.png')}/>
+            //       </TouchableOpacity>
+            //     </View>
+            //   );
+            // }}
+            renderCustomModal={this.renderDialog}
+          >
+            {colorOptions.map(option => (
+              <TouchableOpacity row centerV onPress={()=> this.pickOption(option)}>
+                <View row centerV padding-20>
+                  <View style={{width: 14, height: 14, borderRadius: 12, marginRight: 6}} backgroundColor={option.color}/>
+                  <Text dark10 text70>{option.name}</Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </Picker>
+  
+          <Picker
+            marginT-20
             visible={isSize}
-            onDismiss={()=> this.setState({isSize: false})}
-          />
+            title="Size"
+            placeholder={sizeOption.name}
+            onChange={()=>this.setState({isSize: true})}
+            rightIconSource={require('../../../assets/icons/chevronDown.png')}
+            renderCustomModal={this.renderDialog}
+          >
+            {sizeOptions.map(option => (
+              <TouchableOpacity row centerV onPress={()=> this.setState({sizeOption: option})}>
+                <View row centerV padding-20>
+                  <Text dark10 text70>{option.name}</Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </Picker>
       
-          <Button style={{marginVertical: 40}} label={'Add to Cart'} />
+          <Button marginV-40 label={'Add to Cart'} />
       
           <Text text60M>Recommended for You</Text>
         </View>
