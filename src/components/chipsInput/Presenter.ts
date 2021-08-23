dec823ac23a3561db7dd34c45d2cd67db2f3b99b
Mendy <mendye@wix.com>
2021-08-23 17:03:59 +0300

    set disable color logic code

--- src/components/chipsInput/Presenter.ts
+++ src/components/chipsInput/Presenter.ts
@@ -1,5 +1,5 @@
 import {Colors} from '../../style';
-import {ChipProps} from './index';
+import {ChipProps, ChipsInputProps} from './index';
 
 export const isContainsInvalid = (chips: Array<ChipProps>) => {
   return chips.filter((chip) => chip.invalid === true)[0] !== undefined;
@@ -11,8 +11,12 @@ export const getValidationBasedColor = (chips: Array<ChipProps>, defaultChip?: C
   return isContainsInvalid(chips) ? dismissColor : Colors.primary;
 };
 
-export const getCounterTextColor = (stateChips: Array<ChipProps>, maxLength: number) => {
-  return stateChips.length >= maxLength ? Colors.red30 : Colors.grey30;
+export const getCounterTextColor = (stateChips: Array<ChipProps>, props: ChipsInputProps) => {
+  const {maxLength} = props;
+  if (isDisabled(props)) {
+    return Colors.grey50;
+  }
+  return maxLength && stateChips.length >= maxLength ? Colors.red30 : Colors.grey30;
 };
 
 export const getCounterText = (count: number, maxLength: number) => {
@@ -22,5 +26,10 @@ export const getCounterText = (count: number, maxLength: number) => {
 export const getChipDismissColor = (chip: ChipProps, isSelected: boolean, defaultChipProps?: ChipProps) => {
   const dismissColor = defaultChipProps?.dismissColor || Colors.white;
   return !chip.invalid ? dismissColor : isSelected ? Colors.red10 : Colors.red30;
+};
+
+export const isDisabled = (props: ChipsInputProps) => {
+  const {disableTagRemoval, editable} = props;
+  return disableTagRemoval || editable === false;
 };
 
