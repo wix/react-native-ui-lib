953faf0153440743cbef7912172baa36a3d44573
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-08-28 11:42:16 +0300

    Improve undefined tests to use _.get (#1504)

--- eslint-rules/lib/utils/generalUtils.js
+++ eslint-rules/lib/utils/generalUtils.js
@@ -21,11 +21,11 @@ function findValueNodeOfIdentifier(identifierName, scope) {
     }
   });
   if (valueNode === false || _.isNil(valueNode) || valueNode.value !== undefined) {
-    if (scope && scope.block && scope.block.body && scope.block.body.length > 0) {
+    if (_.get(scope, 'block.body.length', 0) > 0) {
       scope.block.body.forEach(scopeNode => {
-        if (scopeNode && scopeNode.type === 'ExpressionStatement') {
+        if (_.get(scopeNode, 'type') === 'ExpressionStatement') {
           const variableName = _.get(scopeNode, 'expression.left.name');
-          if (variableName === identifierName && scopeNode.expression && scopeNode.expression.right) {
+          if (variableName === identifierName && _.get(scopeNode, 'expression.right')) {
             valueNode = scopeNode.expression.right;
           }
         }
