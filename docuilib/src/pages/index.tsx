0311653c6227db92a9d623f04c5de98079e7bcde
Ethan Sharabi <ethans@wix.com>
2021-09-08 16:58:23 +0300

    Add MainSection

--- docuilib/src/pages/index.tsx
+++ docuilib/src/pages/index.tsx
@@ -3,8 +3,9 @@ import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 import HomepageFeatures from '../components/HomepageFeatures';
+import MainSection from '../components/MainSection';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -14,9 +15,7 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
+          <Link className="button button--secondary button--lg" to="/docs/intro">
             Docusaurus Tutorial - 5min ⏱️
           </Link>
         </div>
@@ -28,12 +27,11 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+      {/* <HomepageHeader/> */}
       <main>
-        <HomepageFeatures />
+        <MainSection/>
+        <HomepageFeatures/>
       </main>
     </Layout>
   );
