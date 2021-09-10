84b7f0a1466c5af2f435ef1b69bae15a68c352c8
Markus Moltke <MakakWasTaken@gmail.com>
2021-09-10 15:55:09 +0200

    Fixed issue, that caused Part to be missing from ListItem (ts) (#1529)
    * Fixed issue, that caused Part to be missing from ListItem (ts)
    
    * Possbile additional fix required

--- generatedTypes/src/components/listItem/index.d.ts
+++ generatedTypes/src/components/listItem/index.d.ts
@@ -1,7 +1,25 @@
 import React from 'react';
-import { ListItemProps } from './types';
-export { ListItemProps };
-declare const _default: React.ComponentClass<ListItemProps & {
+import ListItemPart from 'src/components/listItem/ListItemPart';
+import {ListItemProps} from './types';
+export {ListItemProps};
+
+declare class ListItem extends Component<ListItemProps, State> {
+  static displayName: string;
+  static Part: typeof ListItemPart;
+  constructor(props: ListItemProps);
+  onHideUnderlay(): void;
+  onShowUnderlay(): void;
+  setPressed(isPressed: boolean): void;
+  renderViewContainer: () => JSX.Element;
+  renderCustomContainer(Container: React.ComponentType): JSX.Element;
+  renderChildren(): React.DetailedReactHTMLElement<any, HTMLElement>[] | null | undefined;
+  render(): JSX.Element;
+}
+declare const _default: React.ComponentClass<
+  ListItemProps & {
     useCustomTheme?: boolean | undefined;
-}, any>;
+  },
+  any
+> &
+  typeof ListItem;
 export default _default;
