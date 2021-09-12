26570bc6b85c83050b514f320cae8e3ebaf1c4c9
Inbal Tish <inbalti@wix.com>
2021-09-12 09:22:41 +0300

    generating types

--- generatedTypes/src/components/listItem/index.d.ts
+++ generatedTypes/src/components/listItem/index.d.ts
@@ -1,25 +1,39 @@
-import React from 'react';
-import ListItemPart from 'src/components/listItem/ListItemPart';
-import {ListItemProps} from './types';
-export {ListItemProps};
-
-declare class ListItem extends Component<ListItemProps, State> {
-  static displayName: string;
-  static Part: typeof ListItemPart;
-  constructor(props: ListItemProps);
-  onHideUnderlay(): void;
-  onShowUnderlay(): void;
-  setPressed(isPressed: boolean): void;
-  renderViewContainer: () => JSX.Element;
-  renderCustomContainer(Container: React.ComponentType): JSX.Element;
-  renderChildren(): React.DetailedReactHTMLElement<any, HTMLElement>[] | null | undefined;
-  render(): JSX.Element;
+import React, { Component } from 'react';
+import ListItemPart from './ListItemPart';
+import { ListItemProps } from './types';
+declare type ListItemState = {
+    pressed: boolean;
+};
+declare class ListItem extends Component<ListItemProps, ListItemState> {
+    static displayName: string;
+    static defaultProps: {
+        height: number;
+        containerElement: React.ComponentClass<import("../../components/touchableOpacity").TouchableOpacityProps & {
+            useCustomTheme?: boolean | undefined;
+        }, any>;
+        underlayColor: string;
+    };
+    static Part: typeof ListItemPart;
+    styles: {
+        container: {
+            backgroundColor: string;
+        };
+        innerContainer: {
+            flexDirection: "row";
+            height: string | number | undefined;
+        };
+    };
+    constructor(props: ListItemProps);
+    onHideUnderlay(): void;
+    onShowUnderlay(): void;
+    setPressed(isPressed: boolean): void;
+    renderViewContainer: () => JSX.Element;
+    renderCustomContainer: (Container: React.ComponentType) => JSX.Element;
+    renderChildren: () => JSX.Element;
+    render(): JSX.Element;
 }
-declare const _default: React.ComponentClass<
-  ListItemProps & {
+export { ListItemProps };
+declare const _default: React.ComponentClass<ListItemProps & {
     useCustomTheme?: boolean | undefined;
-  },
-  any
-> &
-  typeof ListItem;
+}, any> & typeof ListItem;
 export default _default;
