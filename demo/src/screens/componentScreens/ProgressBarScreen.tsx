f04c1ced2afafea2af9c85daf4cf2326f5a82d96
Inbal Tish <inbalti@wix.com>
2021-09-13 14:17:26 +0300

    borderRadius

--- demo/src/screens/componentScreens/ProgressBarScreen.tsx
+++ demo/src/screens/componentScreens/ProgressBarScreen.tsx
@@ -32,7 +32,7 @@ export default class ProgressBarScreen extends Component {
 
   get customElement() {
     return (
-      <View style={{backgroundColor: Colors.green30, borderWidth: 0.5}}/>
+      <View style={{backgroundColor: Colors.green30, borderWidth: 0.5, borderRadius: 4}}/>
     );
   }
 
