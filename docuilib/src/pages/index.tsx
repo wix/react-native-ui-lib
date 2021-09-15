3b767ed902fedd30bd9d53ec70f626a2d0a06b93
Ethan Sharabi <ethans@wix.com>
2021-09-15 19:07:07 +0300

    Update theme colors and cleanup

--- docuilib/src/pages/index.tsx
+++ docuilib/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.scss';
+// import clsx from 'clsx';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 // import HomepageFeatures from '../components/HomepageFeatures';
 
 import MainSection from '../components/MainSection';
@@ -12,35 +10,18 @@ import FeaturesSection from '../components/FeaturesSection';
 import CodeSection from '../components/CodeSection';
 import LibrariesSection from '../components/LibrariesSection';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  // const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-      {/* <HomepageHeader/> */}
+    <Layout
+      /* title={`Hello from ${siteConfig.title}`} */ description="Description will go into a meta tag in <head />"
+    >
       <main>
         <MainSection/>
         <ComponentsSection/>
         <FeaturesSection/>
         <CodeSection/>
         <LibrariesSection/>
-        {/* <HomepageFeatures/> */}
       </main>
     </Layout>
   );
