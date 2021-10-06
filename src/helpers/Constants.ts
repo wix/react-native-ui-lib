5735a832813dab4cbb705abd9da73b809b60abde
Tatiana <43054051+tacoelho@users.noreply.github.com>
2021-10-06 04:46:30 -0300

    Update Constants.ts (#1585)
    * Update Constants.ts
    
    Fix #1582
    
    * Update package.json
    
    Update RN version.
    
    * Update src/helpers/Constants.ts
    
    Co-authored-by: Ethan Sharabi <ethans@wix.com>
    
    * Update package.json
    
    * Update package.json
    
    Co-authored-by: Ethan Sharabi <ethans@wix.com>

--- src/helpers/Constants.ts
+++ src/helpers/Constants.ts
@@ -131,7 +131,11 @@ const constants = {
   },
   /* Dimensions */
   removeDimensionsEventListener: (callback: any) => {
-    Dimensions.removeEventListener('change', callback);
+    if (Dimensions.remove) {
+      Dimensions.remove('change', callback);
+    } else {
+      Dimensions.removeEventListener('change', callback);
+    }
   },
   /* Accessibility */
   get accessibility() {
