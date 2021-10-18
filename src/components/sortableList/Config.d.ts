987167280c7729b7058771fd770b57c0dc75acb0
Avishai Nudelman <avishainu@gmail.com>
2021-10-18 18:12:54 +0300

    removed unused vars

--- src/components/sortableList/Config.d.ts
+++ src/components/sortableList/Config.d.ts
@@ -1,7 +1,2 @@
-export declare const ITEM_HEIGHT = 100;
-export declare const animationConfig: {
-    easing: (easing: (value: number) => number) => (value: number) => number;
-    duration: number;
-};
 export declare const getPosition: (order: number, itemHeight: number) => number;
 export declare const getOrder: (y: number, itemHeight: number) => number;
