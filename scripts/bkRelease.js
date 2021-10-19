1407c19ed9f06349623c024eb5161c999a5b0a57
Igor Gnatyuk <igorgn@wix.com>
2021-10-19 13:19:36 +0300

    init bk

--- scripts/bkRelease.js
+++ scripts/bkRelease.js
@@ -1,13 +1,29 @@
 const exec = require('shell-utils').exec;
+const cp = require('child_process');
 const semver = require('semver');
 const _ = require('lodash');
 const p = require('path');
 
 // Workaround JS
-const isRelease = process.env.RELEASE_BUILD === 'true';
-const branch = process.env.BRANCH;
 
-const ONLY_ON_BRANCH = `origin/${branch || 'master'}`;
+// Export buildkite variables for Release build
+// We cast toString() because function returns 'object'
+let isRelease, VERSION;
+if (process.env.BUILDKITE_MESSAGE.match(/^release$/i)) {
+  isRelease = cp.execSync(`buildkite-agent meta-data get release-build`).toString();
+  isRelease = (isRelease === 'true');
+  VERSION = cp.execSync(`buildkite-agent meta-data get version`).toString();
+  if (isRelease && Number(VERSION) === 0) {
+    throw new Error('Version can not be 0. Please specify the correct version...')
+  }
+}
+
+
+
+// const isRelease = process.env.RELEASE_BUILD === 'true';
+const branch = process.env.BUILDKITE_BRANCH;
+
+// const ONLY_ON_BRANCH = `origin/${branch || 'master'}`;
 const VERSION_TAG = isRelease ? 'latest' : 'snapshot';
 const VERSION_INC = 'patch';
 
@@ -21,19 +37,19 @@ function run() {
 }
 
 function validateEnv() {
-  if (!process.env.JENKINS_CI) {
+  if (!process.env.CI) {
     throw new Error('releasing is only available from CI');
   }
 
-  if (!process.env.JENKINS_MASTER) {
-    console.log('not publishing on a different build');
-    return false;
-  }
+  // if (!process.env.JENKINS_MASTER) {
+  //   console.log('not publishing on a different build');
+  //   return false;
+  // }
 
-  if (process.env.GIT_BRANCH !== ONLY_ON_BRANCH) {
-    console.log(`not publishing on branch ${process.env.GIT_BRANCH}`);
-    return false;
-  }
+  // if (process.env.GIT_BRANCH !== ONLY_ON_BRANCH) {
+  //   console.log(`not publishing on branch ${process.env.GIT_BRANCH}`);
+  //   return false;
+  // }
 
   return true;
 }
@@ -57,7 +73,7 @@ function versionTagAndPublish() {
   const currentPublished = findCurrentPublishedVersion();
   console.log(`current published version: ${currentPublished}`);
 
-  const version = isRelease ? process.env.VERSION : `${currentPublished}-snapshot.${process.env.BUILD_ID}`;
+  const version = isRelease ? VERSION : `${currentPublished}-snapshot.${process.env.BUILDKITE_BUILD_NUMBER}`;
   console.log(`Publishing version: ${version}`);
 
   tryPublishAndTag(version);
@@ -87,12 +103,12 @@ function tryPublishAndTag(version) {
 
 function tagAndPublish(newVersion) {
   console.log(`trying to publish ${newVersion}...`);
-  exec.execSync(`npm --no-git-tag-version version ${newVersion}`);
-  exec.execSync(`npm publish --tag ${VERSION_TAG}`);
+  // exec.execSync(`npm --no-git-tag-version version ${newVersion}`);
+  // exec.execSync(`npm publish --tag ${VERSION_TAG}`);
   if (isRelease) {
-    exec.execSync(`git tag -a ${newVersion} -m "${newVersion}"`);
+    // exec.execSync(`git tag -a ${newVersion} -m "${newVersion}"`);
   }
-  exec.execSyncSilent(`git push deploy ${newVersion} || true`);
+  // exec.execSyncSilent(`git push deploy ${newVersion} || true`);
 }
 
 run();
