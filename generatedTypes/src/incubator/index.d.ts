564084754d8fe9e89167b8dd70bc10a3fa03c094
Ethan Sharabi <ethans@wix.com>
2021-10-24 14:32:40 +0300

    Update generated types

--- generatedTypes/src/incubator/index.d.ts
+++ generatedTypes/src/incubator/index.d.ts
@@ -1,7 +1,6 @@
 export { default as ExpandableOverlay } from './expandableOverlay';
 export { default as TextField, TextFieldProps, FieldContextType } from './TextField';
 export { default as TouchableOpacity, TouchableOpacityProps } from './TouchableOpacity';
-export { default as TouchableOpacity2 } from './TouchableOpacity2';
 export { default as WheelPicker, WheelPickerProps } from './WheelPicker';
 export { default as PanView, PanViewProps, PanViewDirections, PanViewDismissThreshold } from './panView';
 export { default as TransitionView, TransitionViewProps, Direction, TransitionViewAnimationType } from './TransitionView';
