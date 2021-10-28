bd9b40085b8cad3e4f8f3a735c95022151a745fd
Ethan Sharabi <ethans@wix.com>
2021-10-28 16:16:35 +0300

    Update generated types

--- generatedTypes/src/hooks/useOrientation/index.d.ts
+++ generatedTypes/src/hooks/useOrientation/index.d.ts
@@ -1,7 +1,7 @@
 interface UseOrientationProps {
     onOrientationChange?: Function;
 }
-declare const _default: ({ onOrientationChange }: UseOrientationProps) => {
+declare const useOrientation: ({ onOrientationChange }?: UseOrientationProps) => {
     orientation: import("../../helpers/Constants").orientations;
 };
-export default _default;
+export default useOrientation;
