a76b7120b37ad115cec0e4a18beff109ef77c024
Ethan Sharabi <ethans@wix.com>
2021-10-31 09:00:45 +0200

    Feat/use orientation hook (#1625)
    * Create useOrientation hook
    
    * Update generated types

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 export {default as useToggleValue} from './useToggleValue';
 export {default as useDidUpdate} from './useDidUpdate';
+export {default as useOrientation} from './useOrientation';
 export {default as useScrollEnabler} from './useScrollEnabler';
 export {default as useScrollReached} from './useScrollReached';
 export {default as useScrollToItem} from './useScrollToItem';
