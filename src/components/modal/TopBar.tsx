1b8ab1271eae91a796073d72063d026b38e8dd51
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-11-09 09:39:42 +0200

    Prettify

--- src/components/modal/TopBar.tsx
+++ src/components/modal/TopBar.tsx
@@ -10,53 +10,53 @@ import Text from '../../components/text';
 
 export interface ModalTopBarProps {
   /**
-     * title to display in the center of the top bar
-     */
-    title?: string;
-    /**
-     * title custom style
-     */
-    titleStyle?: StyleProp<TextStyle>;
-    /**
-     * done action props (Button props)
-     */
-    doneButtonProps?: Omit<ButtonProps, 'onPress'>;
-    /**
-     * done action label
-     */
-    doneLabel?: string;
-    /**
-     * done action icon
-     */
-    doneIcon?: ImageSourcePropType;
-    /**
-     * done action callback
-     */
-    onDone?: (props?: any) => void;
-    /**
-     * cancel action props (Button props)
-     */
-    cancelButtonProps?: Omit<ButtonProps, 'onPress'>;
-    /**
-     * cancel action label
-     */
-    cancelLabel?: string;
-    /**
-     * cancel action icon
-     */
-    cancelIcon?: ImageSourcePropType;
-    /**
-     * cancel action callback
-     */
-    onCancel?: (props?: any) => void;
-    /**
-     * whether to include status bar or not (height claculations)
-     */
-    includeStatusBar?: boolean;
-    /**
-     * style for the TopBar container
-     */
-    containerStyle?: ViewProps['style'];
+   * title to display in the center of the top bar
+   */
+  title?: string;
+  /**
+   * title custom style
+   */
+  titleStyle?: StyleProp<TextStyle>;
+  /**
+   * done action props (Button props)
+   */
+  doneButtonProps?: Omit<ButtonProps, 'onPress'>;
+  /**
+   * done action label
+   */
+  doneLabel?: string;
+  /**
+   * done action icon
+   */
+  doneIcon?: ImageSourcePropType;
+  /**
+   * done action callback
+   */
+  onDone?: (props?: any) => void;
+  /**
+   * cancel action props (Button props)
+   */
+  cancelButtonProps?: Omit<ButtonProps, 'onPress'>;
+  /**
+   * cancel action label
+   */
+  cancelLabel?: string;
+  /**
+   * cancel action icon
+   */
+  cancelIcon?: ImageSourcePropType;
+  /**
+   * cancel action callback
+   */
+  onCancel?: (props?: any) => void;
+  /**
+   * whether to include status bar or not (height claculations)
+   */
+  includeStatusBar?: boolean;
+  /**
+   * style for the TopBar container
+   */
+  containerStyle?: ViewProps['style'];
   /**
    * Whether or not to handle SafeArea
    */
@@ -69,7 +69,7 @@ type topBarButtonProp = {
   icon?: ImageSourcePropType;
   accessibilityLabel?: string;
   buttonProps?: Omit<ButtonProps, 'onPress'>;
-}
+};
 
 const TOP_BAR_HEIGHT = Constants.isIOS ? 44 : 56;
 const DEFAULT_BUTTON_PROPS = {
