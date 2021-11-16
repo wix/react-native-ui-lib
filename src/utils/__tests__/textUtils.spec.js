81e0f4772f3672c9ba7f97e51f02205594e732c0
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-11-16 15:25:46 +0200

    Prettify

--- src/utils/__tests__/textUtils.spec.js
+++ src/utils/__tests__/textUtils.spec.js
@@ -2,7 +2,6 @@ import {getTextPartsByHighlight, getArrayPartsByHighlight} from '../textUtils';
 
 describe('Text', () => {
   describe('getTextPartsByHighlight', () => {
-
     it('should return the whole string as a single part when highlight string is undefined', () => {
       const result = getTextPartsByHighlight('Playground Screen', undefined);
       expect(result).toEqual([{string: 'Playground Screen', shouldHighlight: false}]);
@@ -17,32 +16,60 @@ describe('Text', () => {
     });
     it('should break text to parts according to highlight string', () => {
       const result = getTextPartsByHighlight('Playground Screen', 'Scr');
-      expect(result).toEqual([{string: 'Playground ', shouldHighlight: false}, {string: 'Scr', shouldHighlight: true}, {string: 'een', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Playground ', shouldHighlight: false},
+        {string: 'Scr', shouldHighlight: true},
+        {string: 'een', shouldHighlight: false}
+      ]);
     });
-    
+
     it('should handle case when highlight repeats more than once', () => {
       const result = getTextPartsByHighlight('Dancing in the Dark', 'Da');
-      expect(result).toEqual([{string: 'Da', shouldHighlight: true}, {string: 'ncing in the ', shouldHighlight: false}, {string: 'Da', shouldHighlight: true}, {string: 'rk', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Da', shouldHighlight: true},
+        {string: 'ncing in the ', shouldHighlight: false},
+        {string: 'Da', shouldHighlight: true},
+        {string: 'rk', shouldHighlight: false}
+      ]);
     });
-    
+
     it('should handle ignore case-sensetive', () => {
       const result = getTextPartsByHighlight('Dancing in the Dark', 'da');
-      expect(result).toEqual([{string: 'Da', shouldHighlight: true}, {string: 'ncing in the ', shouldHighlight: false}, {string: 'Da', shouldHighlight: true}, {string: 'rk', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Da', shouldHighlight: true},
+        {string: 'ncing in the ', shouldHighlight: false},
+        {string: 'Da', shouldHighlight: true},
+        {string: 'rk', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters @', () => {
       const result = getTextPartsByHighlight('@ancing in the @ark', '@a');
-      expect(result).toEqual([{string: '@a', shouldHighlight: true}, {string: 'ncing in the ', shouldHighlight: false}, {string: '@a', shouldHighlight: true}, {string: 'rk', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: '@a', shouldHighlight: true},
+        {string: 'ncing in the ', shouldHighlight: false},
+        {string: '@a', shouldHighlight: true},
+        {string: 'rk', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters !', () => {
       const result = getTextPartsByHighlight('!ancing in the !ark', '!a');
-      expect(result).toEqual([{string: '!a', shouldHighlight: true}, {string: 'ncing in the ', shouldHighlight: false}, {string: '!a', shouldHighlight: true}, {string: 'rk', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: '!a', shouldHighlight: true},
+        {string: 'ncing in the ', shouldHighlight: false},
+        {string: '!a', shouldHighlight: true},
+        {string: 'rk', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters starts with @', () => {
       const result = getTextPartsByHighlight('uilib@wix.com', '@wix');
-      expect(result).toEqual([{string: 'uilib', shouldHighlight: false}, {string: '@wix', shouldHighlight: true}, {string: '.com', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'uilib', shouldHighlight: false},
+        {string: '@wix', shouldHighlight: true},
+        {string: '.com', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle empty string.', () => {
@@ -57,7 +84,6 @@ describe('Text', () => {
   });
 
   describe('getArrayPartsByHighlight', () => {
-
     it('should return the whole string as a single part when highlight array is empty', () => {
       const result = getArrayPartsByHighlight('Playground Screen', []);
       expect(result).toEqual([{string: 'Playground Screen', shouldHighlight: false}]);
@@ -72,32 +98,52 @@ describe('Text', () => {
     });
     it('should break text to parts according to highlight string', () => {
       const result = getArrayPartsByHighlight('Playground Screen', ['Scr']);
-      expect(result).toEqual([{string: 'Playground ', shouldHighlight: false}, {string: 'Scr', shouldHighlight: true}, {string: 'een', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Playground ', shouldHighlight: false},
+        {string: 'Scr', shouldHighlight: true},
+        {string: 'een', shouldHighlight: false}
+      ]);
     });
-    
+
     it('highlight repeats more than once should color the first match', () => {
       const result = getArrayPartsByHighlight('Dancing in the Dark', ['Da']);
-      expect(result).toEqual([{string: 'Da', shouldHighlight: true}, {string: 'ncing in the Dark', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Da', shouldHighlight: true},
+        {string: 'ncing in the Dark', shouldHighlight: false}
+      ]);
     });
-    
+
     it('should handle ignore case-sensetive', () => {
       const result = getArrayPartsByHighlight('Dancing in the Dark', ['da']);
-      expect(result).toEqual([{string: 'Da', shouldHighlight: true}, {string: 'ncing in the Dark', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'Da', shouldHighlight: true},
+        {string: 'ncing in the Dark', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters @', () => {
       const result = getArrayPartsByHighlight('@ancing in the @ark', ['@a']);
-      expect(result).toEqual([{string: '@a', shouldHighlight: true}, {string: 'ncing in the @ark', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: '@a', shouldHighlight: true},
+        {string: 'ncing in the @ark', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters !', () => {
       const result = getArrayPartsByHighlight('!ancing in the !ark', ['!a']);
-      expect(result).toEqual([{string: '!a', shouldHighlight: true}, {string: 'ncing in the !ark', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: '!a', shouldHighlight: true},
+        {string: 'ncing in the !ark', shouldHighlight: false}
+      ]);
     });
 
     it('Should handle special characters starts with @', () => {
       const result = getArrayPartsByHighlight('uilib@wix.com', ['@wix']);
-      expect(result).toEqual([{string: 'uilib', shouldHighlight: false}, {string: '@wix', shouldHighlight: true}, {string: '.com', shouldHighlight: false}]);
+      expect(result).toEqual([
+        {string: 'uilib', shouldHighlight: false},
+        {string: '@wix', shouldHighlight: true},
+        {string: '.com', shouldHighlight: false}
+      ]);
     });
   });
 });
