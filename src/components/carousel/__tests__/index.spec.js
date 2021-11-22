4dea65a2108871d9037b8aaa6bc9cb9eb90c63fe
Inbal Tish <inbalti@wix.com>
2021-11-22 09:54:10 +0200

    Carousel - render tests - first tests

---
+++ src/components/carousel/__tests__/index.spec.js
@@ -0,0 +1,89 @@
+import {map} from 'lodash';
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
+import {Text, View} from 'react-native';
+import {Constants} from '../../../helpers';
+import Carousel from '../index';
+
+
+const numberOfPagesShown = 5;
+const eventData = {
+  nativeEvent: {
+    contentOffset: {
+      x: Constants.screenWidth
+    }
+  }
+};
+const onChangePageMock = jest.fn();
+const onScrollMock = jest.fn();
+const props = {
+  testID: 'carousel',
+  initialPage: 0,
+  pagingEnabled: true,
+  autoplay: false,
+  autoplayInterval: 4000,
+  horizontal: true,
+  onChangePage: onChangePageMock,
+  onScroll: onScrollMock
+  //animatedScrollOffset: // set to check Animated
+};
+
+const Page = ({children, ...others}) => {
+  return (
+    <View {...others} style={{flex: 1}}>
+      {children}
+    </View>
+  );
+};
+
+describe('Carousel tests', () => {
+  describe('default setup', () => {
+    it('should be set to default', () => {
+      const component = render(<Carousel {...props}>
+        {map([...Array(numberOfPagesShown)], (_, index) => (
+          <Page key={index}>
+            <Text testID={`page-${index}`}>Page #{index}</Text>
+          </Page>
+        ))}
+      </Carousel>);
+      component.getByText('Page #0'); // Validates that the text is there
+    });
+
+    it('should trigger onScroll from the second scroll', () => {
+      const component = render(<Carousel {...props}>
+        {map([...Array(numberOfPagesShown)], (_, index) => (
+          <Page key={index}>
+            <Text>Page #{index}</Text>
+          </Page>
+        ))}
+      </Carousel>);
+      const scrollView = component.getByTestId('carousel.scrollView');
+
+      fireEvent.scroll(scrollView, eventData); //NOTE: first scroll will no fire onScroll
+      expect(onScrollMock).not.toHaveBeenCalled();
+
+      fireEvent.scroll(scrollView, eventData);
+      expect(onScrollMock).toHaveBeenCalled();
+    });
+
+    it('should trigger onChangePage with current page', async () => {
+      const component = render(<Carousel {...props}>
+        {map([...Array(numberOfPagesShown)], (_, index) => (
+          <Page key={index}>
+            <Text>Page #{index}</Text>
+          </Page>
+        ))}
+      </Carousel>);
+      const scrollView = component.getByTestId('carousel.scrollView');
+
+      fireEvent.scroll(scrollView, eventData); //NOTE: first scroll will no fire onScroll
+      fireEvent.scroll(scrollView, eventData);
+      expect(onChangePageMock).not.toHaveBeenCalled();
+      
+      await new Promise((r) => setTimeout(r, 2000));
+      fireEvent(scrollView, 'onMomentumScrollEnd', eventData);
+      expect(onChangePageMock).toHaveBeenCalled();
+      expect(onChangePageMock).toHaveBeenCalledWith(1, 0, {isAutoScrolled: false});
+    });
+  });
+});
