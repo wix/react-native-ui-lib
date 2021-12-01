58f878de02da093ce5230945c5f3a34bb75ff4c7
Lidor Dafna <66782556+lidord-wix@users.noreply.github.com>
2021-12-01 10:20:57 +0200

    WheelPicker - render tests (#1688)
    * add render tests
    
    * pr fixes
    
    * scrollViewHelper
    
    * PR fixes

--- src/uilib-test-renderer/index.ts
+++ src/uilib-test-renderer/index.ts
@@ -1,8 +1,10 @@
 export * from '@testing-library/react-native';
 import {findStyle} from './helper';
+import {fireOnMomentumScrollEnd} from './scrollViewHelper';
 
 export {
-  findStyle
+  findStyle,
+  fireOnMomentumScrollEnd
 };
 
 /* Docs: https://callstack.github.io/react-native-testing-library/docs/api/  */
