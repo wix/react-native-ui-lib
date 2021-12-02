b7f51bd79d9d80287a08f7c390afdc8218c5574b
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-12-02 16:01:19 +0200

    Fix readability

--- src/components/checkbox/__tests__/index.spec.js
+++ src/components/checkbox/__tests__/index.spec.js
@@ -25,79 +25,60 @@ describe('Checkbox renderer test', () => {
 
   it('Default value is false', async () => {
     const props = {onValueChange};
-    const element = <TestCase {...props}/>;
 
-    const {getByTestId} = render(element);
+    const {getByTestId} = render(<TestCase {...props}/>);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
     expect(onValueChange).toHaveBeenCalledTimes(1);
     expect(onValueChange).toHaveBeenCalledWith(true);
   });
 
   it('Send value (false)', async () => {
     const props = {onValueChange, value: false};
-    const element = <TestCase {...props}/>;
 
-    const {getByTestId} = render(element);
+    const {getByTestId} = render(<TestCase {...props}/>);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
     expect(onValueChange).toHaveBeenCalledTimes(1);
     expect(onValueChange).toHaveBeenCalledWith(true);
   });
 
   it('Send value (true)', async () => {
     const props = {onValueChange, value: true};
-    const element = <TestCase {...props}/>;
 
-    const {getByTestId} = render(element);
+    const {getByTestId} = render(<TestCase {...props}/>);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
     expect(onValueChange).toHaveBeenCalledTimes(1);
     expect(onValueChange).toHaveBeenCalledWith(false);
   });
 
   it('Multiple clicks', async () => {
     const props = {onValueChange};
-    const element = <TestCase {...props}/>;
 
-    const {getByTestId} = render(element);
+    const {getByTestId} = render(<TestCase {...props}/>);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
     expect(onValueChange).toHaveBeenCalledTimes(1);
     expect(onValueChange).toHaveBeenCalledWith(true);
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
-    expect(onValueChange).toHaveBeenCalledTimes(2);
     expect(onValueChange.mock.calls).toEqual([[true], [false]]);
     fireEvent.press(checkbox);
-    expect(onValueChange).toHaveBeenCalled();
-    expect(onValueChange).toHaveBeenCalledTimes(3);
     expect(onValueChange.mock.calls).toEqual([[true], [false], [true]]);
   });
 
   it('Disabled (value = false)', async () => {
     const props = {onValueChange, value: false, disabled: true};
-    const element = <TestCase {...props}/>;
 
-    const {getByTestId} = render(element);
+    const {getByTestId} = render(<TestCase {...props}/>);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
     expect(onValueChange).not.toHaveBeenCalled();
-    expect(onValueChange).toHaveBeenCalledTimes(0);
   });
 
   it('Disabled (value = true)', async () => {
@@ -107,9 +88,7 @@ describe('Checkbox renderer test', () => {
     const {getByTestId} = render(element);
 
     const checkbox = getByTestId(testID);
-    expect(checkbox).toBeTruthy();
     fireEvent.press(checkbox);
     expect(onValueChange).not.toHaveBeenCalled();
-    expect(onValueChange).toHaveBeenCalledTimes(0);
   });
 });
