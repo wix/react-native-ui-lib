520798915ae52a7882f65d1d5ec220ac19b737c0
Ethan Sharabi <ethans@wix.com>
2021-12-06 08:57:04 +0200

    Update toast generated types

--- generatedTypes/src/incubator/toast/helpers/useToastAnimation.d.ts
+++ generatedTypes/src/incubator/toast/helpers/useToastAnimation.d.ts
@@ -1,16 +1,22 @@
 import { Animated } from 'react-native';
 import { ToastProps } from '../types';
-interface UseToastAnimationProps extends Pick<ToastProps, 'visible' | 'preset' | 'position' | 'onAnimationEnd'> {
+declare type UseToastAnimationProps = Pick<ToastProps, 'visible' | 'position' | 'onAnimationEnd' | 'enableHapticFeedback'> & {
     toastHeight: number;
     playAccessibilityFeatures: () => void;
-    setDismissTimer: () => void;
-}
-declare const _default: ({ visible, preset, position, toastHeight, onAnimationEnd, setDismissTimer, playAccessibilityFeatures }: UseToastAnimationProps) => {
+    setTimer: () => void;
+};
+declare const _default: ({ visible, position, toastHeight, onAnimationEnd, enableHapticFeedback, setTimer, playAccessibilityFeatures }: UseToastAnimationProps) => {
     isAnimating: boolean | undefined;
     toggleToast: (show?: boolean, { delay }?: {
         delay?: number | undefined;
     }) => void;
-    toastOpacity: Animated.AnimatedInterpolation;
-    toastTranslateY: Animated.AnimatedInterpolation;
+    opacityStyle: {
+        opacity: Animated.AnimatedInterpolation;
+    };
+    translateStyle: {
+        transform: {
+            translateY: Animated.AnimatedInterpolation;
+        }[];
+    };
 };
 export default _default;
--- generatedTypes/src/incubator/toast/helpers/useToastTimer.d.ts
+++ generatedTypes/src/incubator/toast/helpers/useToastTimer.d.ts
@@ -1,6 +1,6 @@
 import { ToastProps } from '../types';
 declare const _default: ({ autoDismiss, onDismiss }: Pick<ToastProps, 'autoDismiss' | 'onDismiss'>) => {
     clearTimer: () => void;
-    setDismissTimer: () => void;
+    setTimer: () => void;
 };
 export default _default;
