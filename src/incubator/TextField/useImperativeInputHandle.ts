8462de96dbf48df56bbaf50e92d3ace356b52bf6
Ethan Sharabi <ethans@wix.com>
2021-12-07 08:48:44 +0200

    Fix issue with RN TextInput clear doesn't trigger onChangeText (#1711)

--- src/incubator/TextField/useImperativeInputHandle.ts
+++ src/incubator/TextField/useImperativeInputHandle.ts
@@ -1,15 +1,19 @@
 import React, {useContext, useImperativeHandle, useRef} from 'react';
-import {TextInput} from 'react-native';
+import {TextInput, TextInputProps} from 'react-native';
 import FieldContext from './FieldContext';
 
-const useImperativeInputHandle = (ref: React.Ref<any>) => {
+const useImperativeInputHandle = (ref: React.Ref<any>, props: Pick<TextInputProps, 'onChangeText'>) => {
   const inputRef = useRef<TextInput>();
   const context = useContext(FieldContext);
   useImperativeHandle(ref, () => {
     return {
       focus: () => inputRef.current?.focus(),
       blur: () => inputRef.current?.blur(),
-      clear: () => inputRef.current?.clear(),
+      clear: () => {
+        inputRef.current?.clear();
+        // NOTE: This fixes an RN issue - when triggering imperative clear method, it doesn't call onChangeText
+        props.onChangeText?.('');
+      },
       validate: () => {
         context.validateField();
       }
