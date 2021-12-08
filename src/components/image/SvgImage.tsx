6c1cf5842f2d9a6b5affce280531d5fbdfb54dae
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-12-08 11:29:42 +0200

    Image - support SVG from uri (#1712)

--- src/components/image/SvgImage.tsx
+++ src/components/image/SvgImage.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import {ImageProps} from 'react-native';
 import {SvgPackage} from '../../optionalDependencies';
 const SvgXml = SvgPackage?.SvgXml;
+const SvgCssUri = SvgPackage?.SvgCssUri;
 // const SvgProps = SvgPackage?.SvgProps; TODO: not sure how (or if) we can use their props
+
+function isSvgUri(source: ImageProps['source']) {
+  // @ts-expect-error
+  return typeof source === 'object' && source?.uri?.endsWith('.svg');
+}
+
+export function isSvg(source: ImageProps['source']) {
+  return typeof source === 'string' || typeof source === 'function' || isSvgUri(source);
+}
 
 export interface SvgImageProps {
   data: any; // TODO: I thought this should be string | React.ReactNode but it doesn't work properly
@@ -16,7 +27,9 @@ function SvgImage(props: SvgImageProps) {
     return null;
   }
 
-  if (typeof data === 'string') {
+  if (isSvgUri(data)) {
+    return <SvgCssUri {...others} uri={data.uri}/>;
+  } else if (typeof data === 'string') {
     return <SvgXml xml={data} {...others}/>;
   } else if (data) {
     const File = data; // Must be with capital letter
@@ -27,5 +40,6 @@ function SvgImage(props: SvgImageProps) {
 }
 
 SvgImage.displayName = 'IGNORE';
+SvgImage.isSvg = isSvg;
 
 export default SvgImage;
