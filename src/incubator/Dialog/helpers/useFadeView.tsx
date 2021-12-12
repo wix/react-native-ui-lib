1ba99ad592b2b9bb5298b4e0e5f6d0df1c0b4d61
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2021-12-12 10:48:11 +0200

    Feat/incubator dialog fixes and improvements (#1704)
    * Add ImperativeDialog - fix bug while keeping simple
    
    * Add alignment support
    
    * Refactor (add useFadeView) and fix pointerEvents
    
    * Fix onRequestClose
    
    * Support useSafeArea
    
    * Add supportedOrientations and accessibilityLabel
    
    * Revert "Add supportedOrientations and accessibilityLabel"
    
    This reverts commit 9d8b5882a1bbc5f3a24cf8b0dadc03edcaaa25f6.
    
    * Add modalProps.supportedOrientations to the screen
    
    * Bring back containerStyle
    
    * Workaround useSafeArea performance issue
    
    * Fix types
    
    * Remove containerStyle again
    
    * Add api

---
+++ src/incubator/Dialog/helpers/useFadeView.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useCallback} from 'react';
+import {useSharedValue, withTiming, useAnimatedStyle} from 'react-native-reanimated';
+import {Colors} from 'style';
+import View from '../../../components/view';
+import {ModalProps} from '../../../components/modal';
+import {TransitionViewAnimationType} from '../../TransitionView';
+import {ImperativeDialogProps} from '../types';
+
+const DEFAULT_OVERLAY_BACKGROUND_COLORS = Colors.rgba(Colors.black, 0.2);
+
+export type AnimationType = TransitionViewAnimationType;
+
+export type FadeViewProps = Pick<ImperativeDialogProps, 'initialVisibility' | 'testID'> &
+  Pick<ModalProps, 'overlayBackgroundColor'>;
+
+export interface FadeViewMethods {
+  hideNow: () => void;
+}
+
+const useFadeView = (props: FadeViewProps) => {
+  const {initialVisibility, overlayBackgroundColor = DEFAULT_OVERLAY_BACKGROUND_COLORS, testID} = props;
+  const fadeOpacity = useSharedValue<number>(Number(initialVisibility));
+
+  const hideNow = useCallback(() => {
+    fadeOpacity.value = 0;
+  }, []);
+
+  const fade = useCallback((type: AnimationType) => {
+    fadeOpacity.value = withTiming(Number(type === 'enter'), {duration: 300});
+  }, []);
+
+  const fadeStyle = useAnimatedStyle(() => {
+    return {
+      opacity: fadeOpacity.value,
+      backgroundColor: overlayBackgroundColor
+    };
+  }, [overlayBackgroundColor]);
+
+  const FadeView = <View testID={testID} absF reanimated style={fadeStyle} pointerEvents="none"/>;
+
+  return {FadeView, hideNow, fade};
+};
+
+export default useFadeView;
