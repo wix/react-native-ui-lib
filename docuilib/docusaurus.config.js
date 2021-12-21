31b0db15ec3fb5413336e10b8c71d0eb1ef8826b
Ethan Sharabi <ethans@wix.com>
2021-12-21 18:11:40 +0200

    Config algolia doc search

--- docuilib/docusaurus.config.js
+++ docuilib/docusaurus.config.js
@@ -50,6 +50,20 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
     themeConfig:
       /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
+        algolia: {
+          // If Algolia did not provide you any appId, use 'BH4D9OD16A'
+          appId: 'BWQFOHCCF3',
+          // Public API key: it is safe to commit it
+          apiKey: 'e295e6c247076baa129b10202fc0c6c9',
+          indexName: 'react-native-ui-lib',
+          // Optional: see doc section below
+          contextualSearch: true
+          // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+          // externalUrlRegex: 'external\\.com|domain\\.com',
+          // Optional: Algolia search parameters
+          // searchParameters: {}
+          //... other Algolia params
+        },
         navbar: {
           title: 'RNUILib',
           hideOnScroll: true,
