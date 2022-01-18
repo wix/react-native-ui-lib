fecdfec2fb7608173ec6cf5834a482b2efa96d38
dankov <dankov@wix.com>
2022-01-18 14:44:51 +0200

    add tests for Text component using TextTestKit

--- src/components/text/Text.driver.ts
+++ src/components/text/Text.driver.ts
@@ -1,12 +1,11 @@
-import {RenderAPI} from '@testing-library/react-native';
+import {fireEvent, RenderAPI} from '@testing-library/react-native';
 import {ReactTestInstance} from 'react-test-renderer';
-
+import _ from 'lodash';
 
 const TextDriverFactory = async ({wrapperComponent, testID}: {wrapperComponent: RenderAPI, testID: string}) => {
 
   const text: ReactTestInstance | null = await wrapperComponent.queryByTestId(testID);
   return {
-    //todo more research on this component
     exists: () => !!text,
     getRootElement: () => text,
     getTextContent: () => {
@@ -16,9 +15,15 @@ const TextDriverFactory = async ({wrapperComponent, testID}: {wrapperComponent: 
         console.warn(`Text component with testId:${testID}, is not found. So you can't get the content`);
         return null;
       }
+    },
+    isClickable: () => typeof _.get(text, 'props.onPress') === 'function',
+    click: () => {
+      if (text) {
+        fireEvent.press(text);
+      } else {
+        console.warn(`TextDriverFactory: cannot click because testID:${testID} were not found`);
+      }
     }
-    // isClickable: () => te
-    // click: () => fireEvent.press(text),
 
   };
 };
