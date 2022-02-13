354adbf70b4cd31f3368d415b7c9fa414d398b77
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-02-13 10:04:59 +0200

    adding example

--- demo/src/screens/componentScreens/MarqueeScreen.tsx
+++ demo/src/screens/componentScreens/MarqueeScreen.tsx
@@ -2,22 +2,31 @@ import _ from 'lodash';
 import React, {Component} from 'react';
 import {View, Text, Marquee} from 'react-native-ui-lib';
 
-export default class MarqueeScreen extends Component {  
+export default class MarqueeScreen extends Component { 
+  renderRow() {
+    return (
+      <View row>
+        {_.times(8, i => {
+          return <View style={{backgroundColor: '#FF10F0', width: 20, height: 6, marginRight: 10}}/>;
+        })}
+      </View>
+    );
+  }
+  
   render() {
     return (
       <View flex padding-20>
         <Text text50 grey10 marginB-20>Marquee</Text>
+        <Marquee duration={2000}>
+          {this.renderRow()}
+        </Marquee>
         <Marquee>
           <View padding-20 center style={{backgroundColor: '#cfff04'}}>
             <Text text40>Hello World</Text>
           </View>
         </Marquee>
         <Marquee duration={2000} direction={'right'}>
-          <View row>
-            {_.times(8, i => {
-              return <View style={{backgroundColor: '#FF10F0', width: 40, height: 8, marginRight: 10}}/>;
-            })}
-          </View>
+          {this.renderRow()}
         </Marquee>
       </View>
     );
