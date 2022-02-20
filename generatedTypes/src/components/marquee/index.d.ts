923c47af66128cb83b466004e439b05b98829c3d
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-02-20 13:15:52 +0200

    generating types

--- generatedTypes/src/components/marquee/index.d.ts
+++ generatedTypes/src/components/marquee/index.d.ts
@@ -5,11 +5,15 @@ export declare type MarqueeProps = ViewProps & {
     duration?: number;
     direction?: Directions;
     width?: number;
+    start?: number;
+    end?: number;
 };
 declare const _default: React.ComponentClass<ViewProps & {
     duration?: number | undefined;
     direction?: Directions | undefined;
     width?: number | undefined;
+    start?: number | undefined;
+    end?: number | undefined;
 } & {
     useCustomTheme?: boolean | undefined;
 }, any>;
