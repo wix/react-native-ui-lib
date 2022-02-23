2efd826c8e2b6951634cb68b7ce04156403f9ce8
Lidor Dafna <lidord@wix.com>
2022-02-23 15:43:31 +0200

    move Incubator.Toast to design tokens

--- generatedTypes/src/incubator/toast/helpers/useToastPresets.d.ts
+++ generatedTypes/src/incubator/toast/helpers/useToastPresets.d.ts
@@ -2,7 +2,7 @@ import 'react';
 import { ToastProps } from '../types';
 declare const _default: ({ preset, icon, iconColor, message }: Pick<ToastProps, 'preset' | 'icon' | 'message' | 'iconColor'>) => {
     icon: any;
-    iconColor: any;
+    iconColor: string | undefined;
     accessibilityMessage: string;
 };
 export default _default;
