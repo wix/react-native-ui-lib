af2fbf792ae09d9a194d3a5c9073524dfecfd638
Ethan Sharabi <ethans@wix.com>
2022-03-14 10:28:52 +0200

    Revert changes in globalTypes file

--- typings/globalTypes.d.ts
+++ typings/globalTypes.d.ts
@@ -1,8 +1,5 @@
-type Constructor<T, TA = any> = new (...args: TA[]) => T;
-type ExtendTypeWith<T extends Constructor<any>, OtherObject extends object> = Constructor<
-  InstanceType<T> & OtherObject,
-  ConstructorParameters<T>
->;
+type Constructor<T, TA = any> = new(...args: TA[]) => T;
+type ExtendTypeWith<T extends Constructor<any>, OtherObject extends object> = Constructor<InstanceType<T> & OtherObject, ConstructorParameters<T>>;
 type Dictionary<TYPE> = {[key: string]: TYPE};
 
 declare module 'react-native-color' {
