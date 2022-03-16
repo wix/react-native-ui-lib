f61c04d3e902ca45ff3909f5b81dbb7fad165e66
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2022-03-16 15:23:45 +0200

    SvgImage - remove tintColor to fix Android crash

--- src/components/svgImage/index.tsx
+++ src/components/svgImage/index.tsx
@@ -6,11 +6,16 @@ const SvgCssUri = SvgPackage?.SvgCssUri;
 // const SvgProps = SvgPackage?.SvgProps; TODO: not sure how (or if) we can use their props
 
 export interface SvgImageProps {
+  /**
+   * the asset tint
+   */
+  tintColor?: string | null;
   data: any; // TODO: I thought this should be string | React.ReactNode but it doesn't work properly
 }
 
 function SvgImage(props: SvgImageProps) {
-  const {data, ...others} = props;
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const {data, tintColor: _tintColor, ...others} = props;
 
   if (!SvgXml) {
     // eslint-disable-next-line max-len
