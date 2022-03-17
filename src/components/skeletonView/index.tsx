463b7b34ff8bbe4b21b9b6cf757ab07d461a5b76
Lidor Dafna <66782556+lidord-wix@users.noreply.github.com>
2022-03-17 13:09:09 +0200

    Feat/ support passing colors to skeletonView (#1905)
    * support passing colors to skeletonView
    
    * fix description
    
    * add prop to api file
    
    * update description
    
    * description fix

--- src/components/skeletonView/index.tsx
+++ src/components/skeletonView/index.tsx
@@ -129,6 +129,11 @@ export interface SkeletonViewProps extends AccessibilityProps, MarginModifiers {
    */
   width?: number;
   /**
+   * The colors of the skeleton view, the array length has to be >=2
+   * default: [Colors.grey70, Colors.grey60, Colors.grey70]
+   */
+  colors?: string[]
+  /**
    * The border radius of the skeleton view
    */
   borderRadius?: number;
@@ -229,7 +234,7 @@ class SkeletonView extends Component<InternalSkeletonViewProps, SkeletonState> {
 
   getDefaultSkeletonProps = (input?: {circleOverride: boolean; style: StyleProp<ViewStyle>}) => {
     const {circleOverride, style} = input || {};
-    const {circle, width = 0, height = 0} = this.props;
+    const {circle, colors, width = 0, height = 0} = this.props;
     let {borderRadius} = this.props;
     let size;
     if (circle || circleOverride) {
@@ -238,7 +243,7 @@ class SkeletonView extends Component<InternalSkeletonViewProps, SkeletonState> {
     }
 
     return {
-      shimmerColors: [Colors.grey70, Colors.grey60, Colors.grey70],
+      shimmerColors: colors || [Colors.grey70, Colors.grey60, Colors.grey70],
       isReversed: Constants.isRTL,
       style: [{borderRadius}, style],
       width: size || width,
@@ -425,6 +430,7 @@ class SkeletonView extends Component<InternalSkeletonViewProps, SkeletonState> {
       showLastSeparator,
       height,
       width,
+      colors,
       borderRadius,
       circle,
       style,
@@ -445,6 +451,7 @@ class SkeletonView extends Component<InternalSkeletonViewProps, SkeletonState> {
       showLastSeparator,
       height,
       width,
+      colors,
       borderRadius,
       circle,
       style,
