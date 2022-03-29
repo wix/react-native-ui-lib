7eae64ab74dbe1ec13de5cb95df39f765bd52a98
Marik Shnitman <mariks@wix.com>
2022-03-29 14:42:05 +0300

    Removed .js file

--- src/testkit/index.js
+++
@@ -1,3 +0,0 @@
-import { default as ButtonDriverFactory } from "../components/button/Button.driver";
-import { default as TextDriverFactory } from "../components/text/Text.driver";
-export { ButtonDriverFactory, TextDriverFactory };
