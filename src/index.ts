200dbce9636a2df63077dda0141c8e2d9ccccf79
Ethan Sharabi <ethans@wix.com>
2022-03-30 17:43:39 +0300

    Fix main index file export and remove old index files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-/**
- * This is a fake index.ts file, for auto-generating the types of all the inline required components.
- * The real index file that will be bundled is "src/index.js".
- * Please use this file for declaring all the exports, so they could be picked up by typescript's complier
- */
-
 /* ===== General Stuff ===== */
 export {default as Assets} from './assets';
 export * from './style';
@@ -21,11 +15,14 @@ export {
   ColorsModifiers,
   BackgroundColorModifier
 } from './commons/new';
+// @ts-expect-error
 export {BaseComponent, PureBaseComponent} from './commons';
-export * as Incubator from './incubator';
-export * as Hooks from './hooks';
-export * as Modifiers from './commons/modifiers';
+
+import * as Incubator from './incubator';
+import * as Hooks from './hooks';
+import * as Modifiers from './commons/modifiers';
 export {default as LogService} from './services/LogService';
+export {Incubator, Hooks, Modifiers};
 
 /* ===== Components ===== */
 export {default as ActionBar, ActionBarProps} from './components/actionBar';
@@ -38,12 +35,12 @@ export {default as Button, ButtonProps, ButtonSize, ButtonAnimationDirection} fr
 export {default as Card, CardProps, CardSectionProps, CardSelectionOptions} from './components/card';
 export {default as Carousel, CarouselProps, PageControlPosition} from './components/carousel';
 export {default as Checkbox, CheckboxProps} from './components/checkbox';
-export {default as ChipsInput, ChipsInputProps, ChipsInputChipProps} from './components/ChipsInput';
+export {default as ChipsInput, ChipsInputProps, ChipsInputChipProps} from './components/chipsInput';
 export {default as Chip, ChipProps} from './components/chip';
 export {default as ColorPicker, ColorPickerProps} from './components/colorPicker';
-export {default as ColorPalette, ColorPaletteProps} from './components/ColorPalette';
+export {default as ColorPalette, ColorPaletteProps} from './components/colorPalette';
 export {default as ColorSliderGroup, ColorSliderGroupProps} from './components/slider/ColorSliderGroup';
-export {default as ColorSwatch, ColorSwatchProps} from './components/ColorSwatch';
+export {default as ColorSwatch, ColorSwatchProps} from './components/colorSwatch';
 export {default as ConnectionStatusBar, ConnectionStatusBarProps} from './components/connectionStatusBar';
 export {default as Constants} from './commons/Constants';
 export {default as DateTimePicker, DateTimePickerProps} from './components/dateTimePicker';
@@ -61,6 +58,7 @@ export {default as Icon, IconProps} from './components/icon';
 export {default as Image, ImageProps} from './components/image';
 export {default as ListItem, ListItemProps} from './components/listItem';
 export {default as LoaderScreen, LoaderScreenProps} from './components/loaderScreen';
+// @ts-expect-error
 export {default as MaskedInput} from './components/maskedInput';
 export {default as Modal, ModalProps, ModalTopBarProps} from './components/modal';
 export {default as Overlay, OverlayTypes} from './components/overlay';
@@ -86,13 +84,14 @@ export {default as Picker, PickerProps, PickerValue, PickerModes, PickerSearchSt
 export {default as ProgressBar, ProgressBarProps} from './components/progressBar';
 export {default as RadioButton, RadioButtonProps} from './components/radioButton';
 export {default as RadioGroup, RadioGroupProps} from './components/radioGroup';
-export {default as ScrollBar, ScrollBarProps} from './components/ScrollBar';
+export {default as ScrollBar, ScrollBarProps} from './components/scrollBar';
 export {default as SectionsWheelPicker, SectionsWheelPickerProps} from './components/sectionsWheelPicker';
 export {
   default as SegmentedControl,
   SegmentedControlProps,
   SegmentedControlItemProps
 } from './components/segmentedControl';
+// @ts-expect-error
 export {default as SharedTransition} from './components/sharedTransition';
 export {default as SkeletonView, SkeletonViewProps} from './components/skeletonView';
 export {default as Slider, SliderProps} from './components/slider';
@@ -101,10 +100,12 @@ export {default as StateScreen, StateScreenProps} from './components/stateScreen
 export {default as Stepper, StepperProps} from './components/stepper';
 export {default as Switch, SwitchProps} from './components/switch';
 export {default as TabController, TabControllerProps, TabControllerItemProps} from './components/tabController';
-export {default as TabBar, TabBarProps} from './components/TabBar'; //TODO: remove on V7
+export {default as TabBar, TabBarProps} from './components/tabBar'; //TODO: remove on V7
 export {default as Text, TextProps} from './components/text';
+// @ts-expect-error
 export {default as TextArea} from './components/textArea';
 export {default as TextField} from './components/textField/TextFieldMigrator';
+// @ts-expect-error
 export {default as Toast} from './components/toast';
 export {default as TouchableOpacity, TouchableOpacityProps} from './components/touchableOpacity';
 export {default as View, ViewProps} from './components/view';
@@ -116,6 +117,7 @@ export {
   WizardStepConfig,
   WizardStepsConfig
 } from './components/wizard';
+// @ts-expect-error
 export {default as WheelPickerDialog} from './components/wheelPickerDialog';
 
 /* ===== TestKit ===== */
