32843230283b3578826a3a99eddb5c99ff11273b
Marik Shnitman <mariks@wix.com>
2022-03-31 11:14:02 +0300

    Added drivers to export

--- src/testkit/index.ts
+++ src/testkit/index.ts
@@ -1,1 +1,3 @@
 export {default as ButtonDriverFactory} from '../components/button/Button.driver';
+export {default as TextDriverFactory} from '../components/Text/Text.driver';
+export {default as ImageDriver} from '../components/image/Image.driver';
