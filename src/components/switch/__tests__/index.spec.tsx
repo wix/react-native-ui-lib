7034da2866a78819f3713e892ae66564e2485710
Ethan Sharabi <ethans@wix.com>
2022-04-06 11:44:13 +0300

    Move Switch spec into __tests__ folder

--- src/components/switch/__tests__/index.spec.tsx
+++ src/components/switch/__tests__/index.spec.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
-import Switch, {SwitchProps} from './index';
-import {SwitchDriverFactory} from './switch.driver';
-import View from '../view';
+import Switch, {SwitchProps} from '../index';
+import {SwitchDriverFactory} from '../switch.driver';
+import View from '../../view';
 
 describe('Switch', () => {
 
