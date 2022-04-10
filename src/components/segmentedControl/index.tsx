4a5a224859c28d30c3be06af8954e4148301f641
Lidor Dafna <lidord@wix.com>
2022-04-10 17:20:13 +0300

    update outline token

--- src/components/segmentedControl/index.tsx
+++ src/components/segmentedControl/index.tsx
@@ -202,7 +202,7 @@ const SegmentedControl = (props: SegmentedControlProps) => {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.$backgroundNeutralMedium,
-    borderColor: Colors.$outlineNeutral,
+    borderColor: Colors.$outlineDefault,
     borderWidth: BORDER_WIDTH
   },
   selectedSegment: {
