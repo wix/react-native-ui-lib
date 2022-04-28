69148c771976eb9a9215d81d55608c55bf6a7807
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2022-04-28 10:21:59 +0300

    Apply only to multi

--- src/components/picker/helpers/usePickerSelection.tsx
+++ src/components/picker/helpers/usePickerSelection.tsx
@@ -1,20 +1,20 @@
 import {RefObject, useCallback, useState, useEffect} from 'react';
 import _ from 'lodash';
-import {PickerProps, PickerValue, PickerSingleValue, PickerMultiValue} from '../types';
+import {PickerProps, PickerValue, PickerSingleValue, PickerMultiValue, PickerModes} from '../types';
 
 interface UsePickerSelectionProps
-  extends Pick<PickerProps, 'migrate' | 'value' | 'onChange' | 'getItemValue' | 'topBarProps'> {
+  extends Pick<PickerProps, 'migrate' | 'value' | 'onChange' | 'getItemValue' | 'topBarProps' | 'mode'> {
   pickerExpandableRef: RefObject<any>;
   setSearchValue: (searchValue: string) => void;
 }
 
 const usePickerSelection = (props: UsePickerSelectionProps) => {
-  const {migrate, value, onChange, topBarProps, pickerExpandableRef, getItemValue, setSearchValue} = props;
+  const {migrate, value, onChange, topBarProps, pickerExpandableRef, getItemValue, setSearchValue, mode} = props;
   const [multiDraftValue, setMultiDraftValue] = useState(value as PickerMultiValue);
   const [multiFinalValue, setMultiFinalValue] = useState(value as PickerMultiValue);
 
   useEffect(() => {
-    if (multiFinalValue !== value) {
+    if (mode === PickerModes.MULTI && multiFinalValue !== value) {
       setMultiDraftValue(value as PickerMultiValue);
       setMultiFinalValue(value as PickerMultiValue);
     }
