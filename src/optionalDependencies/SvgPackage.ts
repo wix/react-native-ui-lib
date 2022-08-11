87d8796aebd29688ca51555dc67476fa4b2b8aa8
OrenZak <orenz@wix.com>
2022-08-11 13:53:44 +0300

    logs svg require

--- src/optionalDependencies/SvgPackage.ts
+++ src/optionalDependencies/SvgPackage.ts
@@ -1,10 +1,14 @@
 let SvgPackage: any;
 try {
+  console.log('before svg require:', SvgPackage);
   SvgPackage = require('react-native-svg');
   // Sadly we cannot verify with require('react-native-svg-transformer');
+  console.log('SvgPackage:', SvgPackage);
+  
 } catch (error) {
   console.error(error, SvgPackage);
   
 }
-
+console.log('default exported');
 export default SvgPackage;
+
