85a81f539188ef88808da36547108738d832a8d5
OrenZak <orenz@wix.com>
2022-08-11 09:52:54 +0300

    remove use of requireNativeComp

--- src/optionalDependencies/SvgPackage.ts
+++ src/optionalDependencies/SvgPackage.ts
@@ -2,6 +2,9 @@ let SvgPackage: any;
 try {
   SvgPackage = require('react-native-svg');
   // Sadly we cannot verify with require('react-native-svg-transformer');
-} catch (error) {}
+} catch (error) {
+  console.error(error, SvgPackage);
+  
+}
 
 export default SvgPackage;
