4850a7190ba737006a01e868f2a64c7c50673697
Ethan Sharabi <ethans@wix.com>
2022-08-11 17:51:47 +0300

    Set Intl Polyfills to support Intl with hermes engine

--- demo/src/demoApp.js
+++ demo/src/demoApp.js
@@ -128,3 +128,30 @@ Navigation.events().registerAppLaunchedListener(() => {
   registerScreens(Navigation.registerComponent.bind(Navigation));
   getDefaultScreenAndStartApp();
 });
+
+
+/* Setting Intl Polyfills
+This is due to lack of Intl support in Hermes engine
+ */
+
+if (global.HermesInternal) {
+  if (Constants.isIOS) {  
+    
+    // Polyfills required to use Intl with Hermes engine
+    require('@formatjs/intl-getcanonicallocales/polyfill').default;
+    require('@formatjs/intl-locale/polyfill').default;
+    require('@formatjs/intl-pluralrules/polyfill').default;
+    require('@formatjs/intl-pluralrules/locale-data/en').default;
+    require('@formatjs/intl-numberformat/polyfill').default;
+    require('@formatjs/intl-numberformat/locale-data/en').default;
+    require('@formatjs/intl-datetimeformat/polyfill').default;
+    require('@formatjs/intl-datetimeformat/locale-data/en').default;
+    require('@formatjs/intl-datetimeformat/add-all-tz').default;
+  } else {
+    require('@formatjs/intl-getcanonicallocales/polyfill');
+    require('@formatjs/intl-locale/polyfill');
+    require('@formatjs/intl-datetimeformat/polyfill');
+    require('@formatjs/intl-datetimeformat/locale-data/en');
+    require('@formatjs/intl-datetimeformat/add-all-tz');
+  }
+}
