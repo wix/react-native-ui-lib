0a93ea2b3679c3fb1e936cd83967ecd2944044b0
Adi <adids1221@gmail.com>
2022-09-01 10:09:22 +0300

    Fixed exmaple screen

--- demo/src/screens/componentScreens/MarqueeScreen.tsx
+++ demo/src/screens/componentScreens/MarqueeScreen.tsx
@@ -8,31 +8,31 @@ function MarqueeScreen() {
     <View>
       <ScrollView>
         <View flex center padding-20>
-          <Text h1 marginB-s4 $textDefault>
+          <Text h1 marginB-s3 $textDefault>
             Marquee
           </Text>
-          <Text h3 marginV-s4 $textDefault>
-            Marquee default: ltr
+          <Text h3 marginV-s3 $textDefault>
+            Marquee horizontal {'(default)'}: Left To Right
           </Text>
-          <View marginT-10 style={{borderWidth: 1, borderColor: 'black'}} width={200}>
+          <View marginV-s4 style={{borderWidth: 1, borderColor: 'black'}} width={200}>
             <Marquee direction={MarqueeDirections.RIGHT}>{HorizontalExample()}</Marquee>
           </View>
-          <Text h3 marginV-s4 $textDefault>
-            Marquee default: rtl
+          <Text h3 marginV-s3 $textDefault>
+            Marquee horizontal: Right To Left
           </Text>
-          <View marginT-10 style={{borderWidth: 1, borderColor: 'black'}} width={200}>
+          <View marginV-s4 style={{borderWidth: 1, borderColor: 'black'}} width={200}>
             <Marquee direction={MarqueeDirections.LEFT}>{HorizontalExample()}</Marquee>
           </View>
-          <Text h3 marginV-s4 $textDefault>
-            Marquee default: ttb
+          <Text h3 marginV-s3 $textDefault>
+            Marquee vertical: Top To Bottom
           </Text>
-          <View marginT-10 style={{borderWidth: 1, borderColor: 'black'}} width={200} height={100}>
+          <View marginV-s4 style={{borderWidth: 1, borderColor: 'black'}} width={200} height={100}>
             <Marquee direction={MarqueeDirections.UP}>{VerticalExample()}</Marquee>
           </View>
-          <Text h3 marginV-s4 $textDefault>
-            Marquee default: btt
+          <Text h3 marginV-s3 $textDefault>
+            Marquee vertical: Bottom To Top
           </Text>
-          <View marginT-10 style={{borderWidth: 1, borderColor: 'black'}} width={200} height={100}>
+          <View marginV-s4 style={{borderWidth: 1, borderColor: 'black'}} width={200} height={100}>
             <Marquee direction={MarqueeDirections.DOWN}>{VerticalExample()}</Marquee>
           </View>
         </View>
