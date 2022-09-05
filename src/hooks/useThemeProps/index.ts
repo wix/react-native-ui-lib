9caf7759d5ff878dbeb4f150f1e2e7b5fe643247
Batyr <kanzitdinov@gmail.com>
2022-09-05 07:04:14 +0200

    `useColorScheme()` added (#2234)

--- src/hooks/useThemeProps/index.ts
+++ src/hooks/useThemeProps/index.ts
@@ -1,10 +1,12 @@
 import {useContext, createContext} from 'react';
+import {useColorScheme} from 'react-native';
 import * as Modifiers from '../../commons/modifiers';
 import {ThemeManager} from 'style';
 
 const EmptyContext = createContext({});
 
 const useThemeProps = (props: any, componentName: string) => {
+  useColorScheme();
   const themeContext = ThemeManager.getThemeContext();
   const context = useContext(themeContext ?? EmptyContext);
   return Modifiers.getThemeProps(props, context, componentName);
