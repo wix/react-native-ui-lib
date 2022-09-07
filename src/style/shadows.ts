cd240ac8cec3c098bbfb090962a34f79dca190ae
Adi <adids1221@gmail.com>
2022-09-07 16:09:47 +0300

    Fixed review notes

--- src/style/shadows.ts
+++ src/style/shadows.ts
@@ -57,133 +57,53 @@ const Shadows = {
   },
   /* Old Presets */
   white10: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.04, shadowRadius: 13.5},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.09,
-      shadowRadius: 2,
-      shadowOffset: {height: 2, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.04, shadowRadius: 13.5},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.09, shadowRadius: 2, shadowOffset: {height: 2, width: 0}}
   },
   white20: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.06, shadowRadius: 15},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.04,
-      shadowRadius: 3,
-      shadowOffset: {height: 3, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.06, shadowRadius: 15},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.04, shadowRadius: 3, shadowOffset: {height: 3, width: 0}}
   },
   white30: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.05, shadowRadius: 12},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.06,
-      shadowRadius: 4.5,
-      shadowOffset: {height: 4, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.05, shadowRadius: 12},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.06, shadowRadius: 4.5, shadowOffset: {height: 4, width: 0}}
   },
   white40: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.06, shadowRadius: 18.5},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.07,
-      shadowRadius: 8.5,
-      shadowOffset: {height: 5, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.06, shadowRadius: 18.5},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.07, shadowRadius: 8.5, shadowOffset: {height: 5, width: 0}}
   },
   grey10: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.02, shadowRadius: 13.5},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.03,
-      shadowRadius: 2,
-      shadowOffset: {height: 2, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.02, shadowRadius: 13.5},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.03, shadowRadius: 2, shadowOffset: {height: 2, width: 0}}
   },
   grey20: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.03, shadowRadius: 15},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.02,
-      shadowRadius: 3,
-      shadowOffset: {height: 2.5, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.03, shadowRadius: 15},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.02, shadowRadius: 3, shadowOffset: {height: 2.5, width: 0}}
   },
   grey30: {
-    top: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.04,
-      shadowRadius: 3.5,
-      shadowOffset: {height: 3, width: 0}
-    },
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey20,
-      shadowOpacity: 0.04,
-      shadowRadius: 8,
-      shadowOffset: {height: 7, width: 0}
-    }
+    top: {shadowColor: Colors.grey10, shadowOpacity: 0.04, shadowRadius: 3.5, shadowOffset: {height: 3, width: 0}},
+    bottom: {shadowColor: Colors.grey20, shadowOpacity: 0.04, shadowRadius: 8, shadowOffset: {height: 7, width: 0}}
   },
   grey40: {
-    top: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.04,
-      shadowRadius: 4.5,
-      shadowOffset: {height: 5, width: 0}
-    },
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey20,
-      shadowOpacity: 0.04,
-      shadowRadius: 9,
-      shadowOffset: {height: 10, width: 0}
-    }
+    top: {shadowColor: Colors.grey10, shadowOpacity: 0.04, shadowRadius: 4.5, shadowOffset: {height: 5, width: 0}},
+    bottom: {shadowColor: Colors.grey20, shadowOpacity: 0.04, shadowRadius: 9, shadowOffset: {height: 10, width: 0}}
   },
   // TODO: We should remove dark presets eventually in favor of grey presets (after completing migration properly)
   dark10: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.02, shadowRadius: 13.5},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.03,
-      shadowRadius: 2,
-      shadowOffset: {height: 2, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.02, shadowRadius: 13.5},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.03, shadowRadius: 2, shadowOffset: {height: 2, width: 0}}
   },
   dark20: {
-    top: {shadowColor: isDark ? 'transparent' : Colors.grey20, shadowOpacity: 0.03, shadowRadius: 15},
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.02,
-      shadowRadius: 3,
-      shadowOffset: {height: 2.5, width: 0}
-    }
+    top: {shadowColor: Colors.grey20, shadowOpacity: 0.03, shadowRadius: 15},
+    bottom: {shadowColor: Colors.grey10, shadowOpacity: 0.02, shadowRadius: 3, shadowOffset: {height: 2.5, width: 0}}
   },
   dark30: {
-    top: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.04,
-      shadowRadius: 3.5,
-      shadowOffset: {height: 3, width: 0}
-    },
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey20,
-      shadowOpacity: 0.04,
-      shadowRadius: 8,
-      shadowOffset: {height: 7, width: 0}
-    }
+    top: {shadowColor: Colors.grey10, shadowOpacity: 0.04, shadowRadius: 3.5, shadowOffset: {height: 3, width: 0}},
+    bottom: {shadowColor: Colors.grey20, shadowOpacity: 0.04, shadowRadius: 8, shadowOffset: {height: 7, width: 0}}
   },
   dark40: {
-    top: {
-      shadowColor: isDark ? 'transparent' : Colors.grey10,
-      shadowOpacity: 0.04,
-      shadowRadius: 4.5,
-      shadowOffset: {height: 5, width: 0}
-    },
-    bottom: {
-      shadowColor: isDark ? 'transparent' : Colors.grey20,
-      shadowOpacity: 0.04,
-      shadowRadius: 9,
-      shadowOffset: {height: 10, width: 0}
-    }
+    top: {shadowColor: Colors.grey10, shadowOpacity: 0.04, shadowRadius: 4.5, shadowOffset: {height: 5, width: 0}},
+    bottom: {shadowColor: Colors.grey20, shadowOpacity: 0.04, shadowRadius: 9, shadowOffset: {height: 10, width: 0}}
   },
 
   /**
