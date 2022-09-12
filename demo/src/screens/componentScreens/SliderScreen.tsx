990052a8b35ac88dd751553cee573e5eba9d6a81
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2022-09-12 15:36:05 +0300

    Slider - new features (#2248)
    * Slider - add initial range values
    
    * Slider - adding 'reset' option
    
    * Adding new props to api.json
    
    * Slider - add 'useGap' prop to have the option for the two thumbs to overlap
    
    * Thumb - revert type change
    
    * GadientSlider - reset
    
    * fix initial call for onRangeChange
    
    * fix - reset min only for useRange
    
    * Update demo/src/screens/componentScreens/SliderScreen.tsx
    
    Co-authored-by: Lidor Dafna <66782556+lidord-wix@users.noreply.github.com>
    
    Co-authored-by: Lidor Dafna <66782556+lidord-wix@users.noreply.github.com>

--- demo/src/screens/componentScreens/SliderScreen.tsx
+++ demo/src/screens/componentScreens/SliderScreen.tsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {StyleSheet, ScrollView} from 'react-native';
-import {Colors, View, Text, Icon, Slider, GradientSlider, ColorSliderGroup, Constants} from 'react-native-ui-lib';
+import {Constants, Colors, View, Text, Button, Icon, Slider, GradientSlider, ColorSliderGroup} from 'react-native-ui-lib';
 import {renderBooleanOption} from '../ExampleScreenPresenter';
 
 interface SliderScreenProps {
@@ -13,10 +13,15 @@ interface SliderScreenState {
   sliderValue: number;
   sliderMinValue: number;
   sliderMaxValue: number;
+  sliderMinValue2: number;
+  sliderMaxValue2: number;
   forceLTR: boolean;
 }
 
-const INITIAL_VALUE = 0;
+const INITIAL_VALUE = 20;
+const RANGE_INITIAL_MIN = 0;
+const RANGE_INITIAL_MAX = 100;
+
 const COLOR = Colors.blue30;
 
 export default class SliderScreen extends Component<SliderScreenProps, SliderScreenState> {
@@ -24,19 +29,44 @@ export default class SliderScreen extends Component<SliderScreenProps, SliderScr
     alpha: 1,
     color: COLOR,
     sliderValue: INITIAL_VALUE,
-    sliderMinValue: 0,
-    sliderMaxValue: 100,
+    sliderMinValue: RANGE_INITIAL_MIN,
+    sliderMaxValue: RANGE_INITIAL_MAX,
+    sliderMinValue2: 25,
+    sliderMaxValue2: 80,
     forceLTR: false
   };
+
+  slider = React.createRef();
+  rangeSlider = React.createRef();
+  gradientSlider = React.createRef();
+
+  resetSlider = () => {
+    this.slider.current?.reset();
+    this.rangeSlider.current?.reset();
+    this.gradientSlider.current?.reset();
+  }
 
   onSliderRangeChange = (values: {min: number, max: number}) => {
     const {min, max} = values;
     this.setState({sliderMinValue: min, sliderMaxValue: max});
   };
 
+  onSliderRangeChange2 = (values: {min: number, max: number}) => {
+    const {min, max} = values;
+    this.setState({sliderMinValue2: min, sliderMaxValue2: max});
+  };
+
   onSliderValueChange = (value: number) => {
     this.setState({sliderValue: value});
   };
+
+  onSliderReset = () => {
+    this.setState({sliderValue: INITIAL_VALUE});
+  }
+
+  onRangeSliderReset = () => {
+    this.setState({sliderMinValue: RANGE_INITIAL_MIN, sliderMaxValue: RANGE_INITIAL_MAX});
+  }
 
   onGradientValueChange = (value: string, alpha: number) => {
     this.setState({color: value, alpha});
@@ -50,141 +80,231 @@ export default class SliderScreen extends Component<SliderScreenProps, SliderScr
     return Constants.isRTL && this.state.forceLTR && styles.ltr;
   }
 
-  render() {
-    const {color, alpha, sliderValue, sliderMinValue, sliderMaxValue, forceLTR} = this.state;
+  renderDefaultSliderExample() {
+    const {sliderValue, forceLTR} = this.state;
 
     return (
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View flex padding-20>
-          <Text text40 $textDefault marginB-20>
-            Sliders
-          </Text>
-
-          {Constants.isRTL && renderBooleanOption.call(this, 'Force LTR', 'forceLTR')}
-
-          <Text $textDefault text70BO marginB-10>
-            Default slider
-          </Text>
-          <View row centerV style={this.getReverseStyle()}>
-            <Icon assetName={'search'} style={styles.image}/>
-            <Slider
-              onValueChange={this.onSliderValueChange}
-              value={INITIAL_VALUE}
-              minimumValue={0}
-              maximumValue={100}
-              step={1}
-              containerStyle={styles.sliderContainer}
-              disableRTL={forceLTR}
-            />
-            <Text bodySmall $textNeutral style={styles.text} numberOfLines={1}>
-              ${sliderValue}
-            </Text>
-          </View>
-
-          <Text $textDefault text70BO marginT-30>
-            Negatives
-          </Text>
+      <Fragment>
+        <Text $textDefault text70BO>
+          Default slider
+        </Text>
+        
+        <View row centerV style={this.getReverseStyle()}>
+          <Icon assetName={'search'} style={styles.image}/>
           <Slider
-            minimumValue={-100}
-            maximumValue={100}
-            value={-30}
-            minimumTrackTintColor={Colors.red30}
-            thumbTintColor={Colors.red50}
-            containerStyle={styles.slider}
-          />
-          <Slider
-            minimumValue={-300}
-            maximumValue={-100}
-            value={-130}
-            minimumTrackTintColor={Colors.red30}
-            thumbTintColor={Colors.red50}
-            containerStyle={styles.slider}
-          />
-
-          <Text $textDefault text70BO marginT-20>
-            Disabled
-          </Text>
-          <Slider minimumValue={100} maximumValue={200} value={120} containerStyle={styles.slider} disabled/>
-
-          <Text $textDefault text70BO marginT-15>
-            Custom with Steps
-          </Text>
-          <Slider
-            value={50}
-            minimumValue={0}
-            maximumValue={100}
-            step={25}
-            containerStyle={styles.slider}
-            trackStyle={styles.track}
-            thumbStyle={styles.thumb}
-            activeThumbStyle={styles.activeThumb}
-            thumbTintColor={Colors.violet70}
-            minimumTrackTintColor={Colors.violet40}
-            maximumTrackTintColor={Colors.violet70}
-          />
-
-          <Text $textDefault text70BO marginV-15>
-            Range slider
-          </Text>
-          <View row spread style={this.getReverseStyle()}>
-            <Text bodySmall $textNeutral>
-              min. {sliderMinValue}%
-            </Text>
-            <Text bodySmall $textNeutral>
-              max. {sliderMaxValue}%
-            </Text>
-          </View>
-          <Slider
-            useRange
-            onRangeChange={this.onSliderRangeChange}
+            onValueChange={this.onSliderValueChange}
             value={INITIAL_VALUE}
             minimumValue={0}
             maximumValue={100}
             step={1}
+            containerStyle={styles.sliderContainer}
             disableRTL={forceLTR}
+            ref={this.slider}
+            onReset={this.onSliderReset}
           />
-
-          <Text $textDefault text70BO marginT-15>
-            Gradient Sliders
+          <Text bodySmall $textNeutral style={styles.text} numberOfLines={1}>
+            ${sliderValue}
           </Text>
-          <View row centerV>
-            <Text text90 $textNeutral>
-              DEFAULT
-            </Text>
-            <GradientSlider
-              color={color}
-              containerStyle={styles.gradientSliderContainer}
-              onValueChange={this.onGradientValueChange}
-            />
-            <View style={styles.box}>
-              <View style={{flex: 1, backgroundColor: color, opacity: alpha}}/>
-            </View>
-          </View>
-          <View row centerV>
-            <Text text90 $textNeutral>
-              HUE
-            </Text>
-            <GradientSlider
-              type={GradientSlider.types.HUE}
-              color={COLOR}
-              containerStyle={styles.gradientSliderContainer}
-              onValueChange={this.onGradientValueChange}
-            />
-            <View style={styles.box}>
-              <View style={{flex: 1, backgroundColor: color}}/>
-            </View>
-          </View>
+        </View>
+      </Fragment>
+    );
+  }
 
-          <Text $textDefault text70BO marginV-15>
-            Color Slider Group
+  renderNegativeSliderExample() {
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginT-30>
+          Negatives
+        </Text>
+        <Slider
+          minimumValue={-100}
+          maximumValue={100}
+          value={-30}
+          minimumTrackTintColor={Colors.red30}
+          thumbTintColor={Colors.red50}
+          containerStyle={styles.slider}
+        />
+        <Slider
+          minimumValue={-300}
+          maximumValue={-100}
+          value={-130}
+          minimumTrackTintColor={Colors.red30}
+          thumbTintColor={Colors.red50}
+          containerStyle={styles.slider}
+        />
+      </Fragment>
+    );
+  }
+
+  renderDisabledSliderExample() {
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginT-20>
+          Disabled
+        </Text>
+        <Slider minimumValue={100} maximumValue={200} value={120} containerStyle={styles.slider} disabled/>
+
+        <Text $textDefault text70BO marginT-15>
+          Custom with Steps
+        </Text>
+        <Slider
+          value={50}
+          minimumValue={0}
+          maximumValue={100}
+          step={25}
+          containerStyle={styles.slider}
+          trackStyle={styles.track}
+          thumbStyle={styles.thumb}
+          activeThumbStyle={styles.activeThumb}
+          thumbTintColor={Colors.violet70}
+          minimumTrackTintColor={Colors.violet40}
+          maximumTrackTintColor={Colors.violet70}
+        />
+      </Fragment>
+    );
+  }
+
+  renderRangeSliderExample() {
+    const {sliderMinValue, sliderMaxValue, forceLTR} = this.state;
+
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginV-15>
+          Range Slider
+        </Text>
+        <View row spread style={this.getReverseStyle()}>
+          <Text bodySmall $textNeutral>
+            min. {sliderMinValue}%
           </Text>
-          <ColorSliderGroup
-            initialColor={color}
-            sliderContainerStyle={styles.slider}
-            containerStyle={styles.group}
-            showLabels
-            // onValueChange={this.onGroupValueChange}
+          <Text bodySmall $textNeutral>
+            max. {sliderMaxValue}%
+          </Text>
+        </View>
+        <Slider
+          useRange
+          onRangeChange={this.onSliderRangeChange}
+          value={INITIAL_VALUE}
+          minimumValue={0}
+          maximumValue={100}
+          step={1}
+          disableRTL={forceLTR}
+          ref={this.rangeSlider}
+          onReset={this.onRangeSliderReset}
+        />
+      </Fragment>
+    );
+  }
+
+  renderRangeSliderWithValuesExample() {
+    const {sliderMinValue2, sliderMaxValue2, forceLTR} = this.state;
+
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginV-15>
+          Range w/ initial values and no default gap
+        </Text>
+        <View row spread style={this.getReverseStyle()}>
+          <Text bodySmall $textNeutral>
+            min. {sliderMinValue2}%
+          </Text>
+          <Text bodySmall $textNeutral>
+            max. {sliderMaxValue2}%
+          </Text>
+        </View>
+        <Slider
+          useRange
+          useGap={false}
+          onRangeChange={this.onSliderRangeChange2}
+          value={INITIAL_VALUE}
+          minimumValue={0}
+          maximumValue={100}
+          step={1}
+          disableRTL={forceLTR}
+          initialMinimumValue={25}
+          initialMaximumValue={80}
+        />
+      </Fragment>
+    );
+  }
+
+  renderGradientSlidersExample() {
+    const {color, alpha} = this.state;
+
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginT-15>
+          Gradient Sliders
+        </Text>
+        <View row centerV>
+          <Text text90 $textNeutral>
+            DEFAULT
+          </Text>
+          <GradientSlider
+            color={color}
+            containerStyle={styles.gradientSliderContainer}
+            onValueChange={this.onGradientValueChange}
+            ref={this.gradientSlider}
           />
+          <View style={styles.box}>
+            <View style={{flex: 1, backgroundColor: color, opacity: alpha}}/>
+          </View>
+        </View>
+        <View row centerV>
+          <Text text90 $textNeutral>
+            HUE
+          </Text>
+          <GradientSlider
+            type={GradientSlider.types.HUE}
+            color={COLOR}
+            containerStyle={styles.gradientSliderContainer}
+            onValueChange={this.onGradientValueChange}
+          />
+          <View style={styles.box}>
+            <View style={{flex: 1, backgroundColor: color}}/>
+          </View>
+        </View>
+      </Fragment>
+    );
+  }
+
+  renderColorSliderGroupExample() {
+    const {color} = this.state;
+
+    return (
+      <Fragment>
+        <Text $textDefault text70BO marginV-15>
+          Color Slider Group
+        </Text>
+        <ColorSliderGroup
+          initialColor={color}
+          sliderContainerStyle={styles.slider}
+          containerStyle={styles.group}
+          showLabels
+          // onValueChange={this.onGroupValueChange}
+        />
+      </Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View flex padding-20>
+          <View row spread centerV marginB-10>
+            <Text text40 $textDefault marginB-20>
+              Sliders
+            </Text>
+            <Button link label="Reset" onPress={this.resetSlider}/>
+          </View>
+          {Constants.isRTL && renderBooleanOption.call(this, 'Force LTR', 'forceLTR')}
+
+          {this.renderDefaultSliderExample()}
+          {this.renderNegativeSliderExample()}
+          {this.renderDisabledSliderExample()}
+          {this.renderRangeSliderExample()}
+          {this.renderRangeSliderWithValuesExample()}
+          {this.renderGradientSlidersExample()}
+          {this.renderColorSliderGroupExample()}
         </View>
       </ScrollView>
     );
