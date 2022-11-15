597eeebed910945aa92e7baf8412f8b2489682a3
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2022-11-15 14:59:34 +0200

    Start deprecation of Picker useNativePicker prop

--- src/components/picker/helpers/usePickerMigrationWarnings.tsx
+++ src/components/picker/helpers/usePickerMigrationWarnings.tsx
@@ -3,10 +3,10 @@ import _ from 'lodash';
 import {LogService} from '../../../services';
 import {PickerProps, PickerModes} from '../types';
 
-type UsePickerMigrationWarnings = Pick<PickerProps, 'value' | 'mode'>;
+type UsePickerMigrationWarnings = Pick<PickerProps, 'value' | 'mode' | 'useNativePicker'>;
 
 const usePickerMigrationWarnings = (props: UsePickerMigrationWarnings) => {
-  const {value, mode} = props;
+  const {value, mode, useNativePicker} = props;
   useEffect(() => {
     if (mode === PickerModes.SINGLE && Array.isArray(value)) {
       LogService.warn('Picker in SINGLE mode cannot accept an array for value');
@@ -17,6 +17,10 @@ const usePickerMigrationWarnings = (props: UsePickerMigrationWarnings) => {
 
     if (_.isPlainObject(value)) {
       LogService.warn('UILib Picker will stop supporting passing object as value in the next major version. Please use either string or a number as value');
+    }
+
+    if (useNativePicker) {
+      LogService.warn(`UILib Picker will stop supporting the 'useNativePicker' prop soon, please pass instead the 'useWheelPicker' prop and handle relevant TextField migration if required to`);
     }
   }, []);
 };
