9c53baf5c4bf5ee18b409f82f3a51ca8ddce6a0d
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2022-12-26 13:56:53 +0200

    Add DateUtils with stubs

---
+++ src/incubator/Calendar/helpers/DateUtils.ts
@@ -0,0 +1,5 @@
+export function getMonthWeekNumbers(year: number, month: number) {}
+
+export function getDaysOfWeekNumber(year: number, weekNumber: number) {}
+
+export function getDayOfDate(date: number) {}
