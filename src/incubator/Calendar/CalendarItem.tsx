1b7dd9da0d822df11b7f28cb2dccf6657d96d831
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2022-12-26 14:52:48 +0200

    Fix hardcoded month value we pass to Month component

--- src/incubator/Calendar/CalendarItem.tsx
+++ src/incubator/Calendar/CalendarItem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Month from './Month';
 
 function CalendarItem() {
-  return <Month month={12} year={2022}/>;
+  return <Month month={11} year={2022}/>;
 }
 
 export default CalendarItem;
