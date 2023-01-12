ab4d008288379bafaeea53f58ae37f8fb3e7ab73
OrenZak <orenz@wix.com>
2023-01-12 14:12:11 +0200

    jsCss optionaldep

--- src/components/svgImage/index.web.tsx
+++ src/components/svgImage/index.web.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {isSvg, isSvgUri, isBase64ImageContent} from '../../utils/imageUtils';
 
 export interface SvgImageProps {
