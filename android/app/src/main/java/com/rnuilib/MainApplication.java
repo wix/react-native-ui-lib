c0591b7f7b8ac2944c2b268a95b6bec43b87d7fb
Inbal Tish <inbalti@wix.com>
2023-02-13 11:19:18 +0200

    Android changes

--- android/app/src/main/java/com/rnuilib/MainApplication.java
+++ android/app/src/main/java/com/rnuilib/MainApplication.java
@@ -1,24 +1,23 @@
 package com.rnuilib;
 
-import android.content.Context;
-
 import com.facebook.react.PackageList;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
+import com.facebook.react.bridge.JSIModulePackage;
+
 import com.reactnativenavigation.NavigationApplication;
 import com.reactnativenavigation.react.NavigationReactNativeHost;
 import com.wix.reactnativeuilib.UiLibPackageList;
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
+import java.util.List;
 
 
 public class MainApplication extends NavigationApplication {
 
-    private final ReactNativeHost mReactNativeHost = new NavigationReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new NavigationReactNativeHost(this) { //DefaultReactNativeHost
         @Override
         protected String getJSMainModuleName() {
             return "demo";
@@ -40,55 +39,31 @@ public class MainApplication extends NavigationApplication {
             packages.addAll(new UiLibPackageList(MainApplication.this).getPackageList());
             return packages;
         }
+    
+        // private final ReactNativeHost mNewArchitectureNativeHost = new MainApplicationReactNativeHost(this);
+        @Override
+        protected boolean isNewArchEnabled() {
+        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+        @Override
+        protected Boolean isHermesEnabled() {
+        return BuildConfig.IS_HERMES_ENABLED;
+        }
     };
-
-    private final ReactNativeHost mNewArchitectureNativeHost = new MainApplicationReactNativeHost(this);
 
     @Override
     public ReactNativeHost getReactNativeHost() {
-        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-            return mNewArchitectureNativeHost;
-        } else {
-            return mReactNativeHost;
-        }
+        return mReactNativeHost;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        // If you opted-in for the New Architecture, we enable the TurboModule system
-        // ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    }
 
-    /**
-     * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-     * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-     *
-     * @param context
-     * @param reactInstanceManager
-     */
-    private static void initializeFlipper(
-            Context context, ReactInstanceManager reactInstanceManager) {
-        if (BuildConfig.DEBUG) {
-            try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-                Class<?> aClass = Class.forName("com.rnuilib.ReactNativeFlipper");
-                aClass
-                        .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-                        .invoke(null, context, reactInstanceManager);
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            DefaultNewArchitectureEntryPoint.load();
+          }
+          ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     }
 }
