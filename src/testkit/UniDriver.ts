9a3ffa74ac0ae29cce8b9549c9afc1e497ade036
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2023-02-15 12:16:39 +0200

    Press is actually sync (#2459)

--- src/testkit/UniDriver.ts
+++ src/testkit/UniDriver.ts
@@ -6,7 +6,7 @@ export interface UniDriver {
   at(index: number): Promise<UniDriver>;
   instance(): Promise<any>;
   getInstanceProps(): Promise<any>;
-  press(): Promise<any>;
+  press(): void;
   typeText(text: string): Promise<void>;
   scrollX(deltaX: number): Promise<void>;
   scrollY(deltaY: number): Promise<void>;
