5c37606791e523c641dca72507c44ca148ed984b
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2023-02-15 16:53:15 +0200

    V7_DateTimePicker

--- src/components/dateTimePicker/index.tsx
+++ src/components/dateTimePicker/index.tsx
@@ -137,7 +137,7 @@ function DateTimePicker(props: DateTimePickerPropsInternal) {
     // @ts-expect-error
     useCustomTheme,
     testID,
-    migrateTextField,
+    migrateTextField = true,
     ...others
   } = props;
 
