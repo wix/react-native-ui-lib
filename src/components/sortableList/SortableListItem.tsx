d5c0b04ad7f73198a8b3f79276bf8bb29f32499b
Lidor Dafna <lidord@wix.com>
2023-04-19 12:05:18 +0300

    move height calculation

--- src/components/sortableList/SortableListItem.tsx
+++ src/components/sortableList/SortableListItem.tsx
@@ -49,7 +49,6 @@ const SortableListItem = (props: Props) => {
   const initialIndex = useSharedValue<number>(map(data, 'id').indexOf(id));
   const currIndex = useSharedValue(initialIndex.value);
   const translateY = useSharedValue<number>(0);
-  const itemContainerHeight = itemHeight.value + (itemMargins?.marginTop ?? 0) + (itemMargins?.marginBottom ?? 0);
 
   const isDragging = useSharedValue(false);
 
@@ -83,7 +82,7 @@ const SortableListItem = (props: Props) => {
 
       currIndex.value = newIndex;
       if (!isDragging.value) {
-        const translation = getTranslationByIndexChange(currIndex.value, initialIndex.value, itemContainerHeight);
+        const translation = getTranslationByIndexChange(currIndex.value, initialIndex.value, itemHeight.value);
 
         translateY.value = withTiming(translation, animationConfig);
       }
@@ -95,7 +94,7 @@ const SortableListItem = (props: Props) => {
     .enabled(!locked)
     .onStart(() => {
       isDragging.value = true;
-      translateY.value = getTranslationByIndexChange(currIndex.value, initialIndex.value, itemContainerHeight);
+      translateY.value = getTranslationByIndexChange(currIndex.value, initialIndex.value, itemHeight.value);
       tempTranslateY.value = translateY.value;
       tempItemsOrder.value = itemsOrder.value;
     })
@@ -108,7 +107,7 @@ const SortableListItem = (props: Props) => {
       translateY.value = tempTranslateY.value + event.translationY;
 
       // Swapping items
-      let newIndex = getIndexByPosition(translateY.value, itemContainerHeight) + initialIndex.value;
+      let newIndex = getIndexByPosition(translateY.value, itemHeight.value) + initialIndex.value;
       const oldIndex = getItemIndexById(itemsOrder.value, id);
 
       if (newIndex !== oldIndex) {
@@ -138,7 +137,7 @@ const SortableListItem = (props: Props) => {
     .onEnd(() => {
       const translation = getTranslationByIndexChange(getItemIndexById(itemsOrder.value, id),
         getItemIndexById(tempItemsOrder.value, id),
-        itemContainerHeight);
+        itemHeight.value);
 
       translateY.value = withTiming(tempTranslateY.value + translation, animationConfig, () => {
         if (tempItemsOrder.value.toString() !== itemsOrder.value.toString()) {
--- src/components/sortableList/index.tsx
+++ src/components/sortableList/index.tsx
@@ -47,7 +47,7 @@ const SortableList = <ItemT extends SortableListItemProps>(props: SortableListPr
   const onItemLayout = useCallback((event: LayoutChangeEvent) => {
     // Round height for Android
     const newHeight = Math.round(event.nativeEvent.layout.height);
-    itemHeight.value = newHeight;
+    itemHeight.value = newHeight + (itemMargins?.marginTop ?? 0) + (itemMargins?.marginBottom ?? 0);
   }, []);
 
   const context = useMemo(() => {
