8f63ecfd94400b46baf43ffc63b845eb88923ab2
Adi <adids1221@gmail.com>
2023-05-18 12:40:27 +0300

    added issue documtion to the hook file

--- src/incubator/TextField/useSafeMultiline.tsx
+++ src/incubator/TextField/useSafeMultiline.tsx
@@ -4,6 +4,8 @@ import {Constants} from '../../commons/new';
 
 type SafeMultilineProps = Pick<InternalTextFieldProps, 'multiline' | 'maxLength' | 'value' | 'onChangeText'>;
 
+//TODO: Remove useSafeMultiline file when RN 0.71.8 is released.
+//GitHub issue link: https://github.com/facebook/react-native/issues/36494
 export default function useSafeMultiline({multiline, maxLength, value, onChangeText}: SafeMultilineProps) {
   const isSafeMultiline = multiline && maxLength && Constants.isIOS;
   const lockedText = useRef(value);
