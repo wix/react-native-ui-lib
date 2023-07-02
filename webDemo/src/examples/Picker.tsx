be50f40407d840b3e951de35395285748aee0030
Adi <adids1221@gmail.com>
2023-07-02 17:04:21 +0300

    new picker dialog example

--- webDemo/src/examples/Picker.tsx
+++ webDemo/src/examples/Picker.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {Picker, Colors, View, Text} from 'react-native-ui-lib';
+import {ScrollView} from 'react-native-gesture-handler';
+import {Picker, Colors, View, Text, Incubator, PickerProps, PanningProvider} from 'react-native-ui-lib';
 
 const options = [
   {label: 'JavaScript', value: 'js'},
@@ -18,13 +19,44 @@ const filters = [
   {value: 6, label: 'Tops'}
 ];
 
+const schemes = [
+  {label: 'Default', value: 1},
+  {label: 'Light', value: 2},
+  {label: 'Dark', value: 3}
+];
+
 const PickerWrapper = () => {
   const [language, setLanguage] = useState(undefined);
   const [filter, setFilter] = useState(undefined);
+  const [customModalValues, setCustomModalValues] = useState(undefined);
+  const renderDialog: PickerProps['renderCustomModal'] = (modalProps: any) => {
+    const {visible, children, toggleModal, onDone} = modalProps;
+
+    return (
+      <Incubator.Dialog
+        visible={visible}
+        onDismiss={() => {
+          onDone();
+          toggleModal(false);
+        }}
+        width="100%"
+        height="45%"
+        bottom
+        useSafeArea
+        containerStyle={{backgroundColor: Colors.$backgroundDefault}}
+        direction={PanningProvider.Directions.DOWN}
+        headerProps={{title: 'Custom modal'}}
+      >
+        <ScrollView>{children}</ScrollView>
+      </Incubator.Dialog>
+    );
+  };
 
   return (
     <View marginT-s2>
-      <Text text80BO marginT-s1 center>Single Value Picker</Text>
+      <Text text80BO marginT-s1 center>
+        Single Value Picker
+      </Text>
       <View marginB-s2>
         <Picker
           placeholder="Favorite Language"
@@ -43,7 +75,9 @@ const PickerWrapper = () => {
           ))}
         </Picker>
       </View>
-      <Text text80BO center>Multi Value Picker</Text>
+      <Text text80BO center>
+        Multi Value Picker
+      </Text>
       <View>
         <Picker
           placeholder="Favorite Filters"
@@ -60,6 +94,22 @@ const PickerWrapper = () => {
         >
           {filters.map(filter => (
             <Picker.Item key={filter.value} value={filter.value} label={filter.label} disabled={filter.disabled}/>
+          ))}
+        </Picker>
+      </View>
+      <Text text80BO center>
+        Dialog Picker
+      </Text>
+      <View>
+        <Picker
+          placeholder="Pick multiple schemes"
+          value={customModalValues}
+          onChange={items => setCustomModalValues(items)}
+          mode={Picker.modes.MULTI}
+          renderCustomModal={renderDialog}
+        >
+          {schemes.map(option => (
+            <Picker.Item key={option.value} value={option.value} label={option.label} disabled={option.disabled}/>
           ))}
         </Picker>
       </View>
