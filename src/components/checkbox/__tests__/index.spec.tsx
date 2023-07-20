b88aa31b05aa41a5141a68149183ba72fe963a95
Inbal Tish <inbalti@wix.com>
2023-07-20 15:12:39 +0300

    tests

--- src/components/checkbox/__tests__/index.spec.tsx
+++ src/components/checkbox/__tests__/index.spec.tsx
@@ -4,6 +4,7 @@ import {CheckboxDriver} from '../Checkbox.driver';
 
 const testID = 'checkbox';
 const onValueChange = jest.fn();
+const onChangeValidity = jest.fn();
 
 const TestCase = (props: CheckboxProps) => {
   const {value, onValueChange, ...others} = props;
@@ -24,57 +25,82 @@ describe('Checkbox renderer test', () => {
   });
   afterEach(() => CheckboxDriver.clear());
 
-  it('Default value is false', async () => {
-    const props = {onValueChange};
-    const component = <TestCase {...props}/>;
-    const driver = new CheckboxDriver({component, testID});
-
-    await driver.press();
-
-    expect(onValueChange).toHaveBeenCalledTimes(1);
-    expect(onValueChange).toHaveBeenCalledWith(true);
+  describe('Value', () => {
+    it('Default value is false', async () => {
+      const props = {onValueChange};
+      const component = <TestCase {...props}/>;
+      const driver = new CheckboxDriver({component, testID});
+  
+      await driver.press();
+  
+      expect(onValueChange).toHaveBeenCalledTimes(1);
+      expect(onValueChange).toHaveBeenCalledWith(true);
+    });
+  
+    it.each`
+    checkboxInitialValue  | checkboxExpectedValue
+    ${false}              | ${true}
+    ${true}               | ${false}
+    `('Send value ($checkboxInitialValue)', async ({checkboxInitialValue, checkboxExpectedValue}: {checkboxInitialValue: boolean; checkboxExpectedValue: boolean}) => {
+      const props = {onValueChange, value: checkboxInitialValue};
+      const component = <TestCase {...props}/>;
+      const driver = new CheckboxDriver({component, testID});
+  
+      await driver.press();
+  
+      expect(onValueChange).toHaveBeenCalledTimes(1);
+      expect(onValueChange).toHaveBeenCalledWith(checkboxExpectedValue);
+    });
   });
 
-  it.each`
-  checkboxInitialValue  | checkboxExpectedValue
-  ${false}              | ${true}
-  ${true}               | ${false}
-  `('Send value ($checkboxInitialValue)', async ({checkboxInitialValue, checkboxExpectedValue}: {checkboxInitialValue: boolean; checkboxExpectedValue: boolean}) => {
-    const props = {onValueChange, value: checkboxInitialValue};
-    const component = <TestCase {...props}/>;
-    const driver = new CheckboxDriver({component, testID});
-
-    await driver.press();
-
-    expect(onValueChange).toHaveBeenCalledTimes(1);
-    expect(onValueChange).toHaveBeenCalledWith(checkboxExpectedValue);
+  describe('Press', () => {
+    it('Multiple clicks', async () => {
+      const props = {onValueChange};
+      const component = <TestCase {...props}/>;
+      const driver = new CheckboxDriver({component, testID});
+  
+      await driver.press();
+      expect(onValueChange).toHaveBeenCalledTimes(1);
+      expect(onValueChange).toHaveBeenCalledWith(true);
+      await driver.press();
+      expect(onValueChange.mock.calls).toEqual([[true], [false]]);
+      await driver.press();
+      expect(onValueChange.mock.calls).toEqual([[true], [false], [true]]);
+    });
+  
+    it.each`
+    checkboxInitialValue
+    ${false}
+    ${true}
+    `('Disabled (value = $checkboxInitialValue)', async ({checkboxInitialValue}: {checkboxInitialValue: boolean}) => {
+      const props = {onValueChange, value: checkboxInitialValue, disabled: true};
+      const component = <TestCase {...props}/>;
+      const driver = new CheckboxDriver({component, testID});
+  
+      await driver.press();
+  
+      expect(onValueChange).not.toHaveBeenCalled();
+    });
   });
 
-  it('Multiple clicks', async () => {
-    const props = {onValueChange};
-    const component = <TestCase {...props}/>;
-    const driver = new CheckboxDriver({component, testID});
+  describe('Validation', () => {
+    describe('onChangeValidity', () => {
+      it('should not been called', async () => {
+        const props = {onChangeValidity};
+        const component = <TestCase {...props}/>;
+        const driver = new CheckboxDriver({component, testID});
+        await driver.press();
 
-    await driver.press();
-    expect(onValueChange).toHaveBeenCalledTimes(1);
-    expect(onValueChange).toHaveBeenCalledWith(true);
-    await driver.press();
-    expect(onValueChange.mock.calls).toEqual([[true], [false]]);
-    await driver.press();
-    expect(onValueChange.mock.calls).toEqual([[true], [false], [true]]);
-  });
+        expect(onChangeValidity).not.toHaveBeenCalled();
+      });
+      it('should not been called with required prop', async () => {
+        const props = {onChangeValidity, required: true};
+        const component = <TestCase {...props}/>;
+        const driver = new CheckboxDriver({component, testID});
+        await driver.press();
 
-  it.each`
-  checkboxInitialValue
-  ${false}
-  ${true}
-  `('Disabled (value = $checkboxInitialValue)', async ({checkboxInitialValue}: {checkboxInitialValue: boolean}) => {
-    const props = {onValueChange, value: checkboxInitialValue, disabled: true};
-    const component = <TestCase {...props}/>;
-    const driver = new CheckboxDriver({component, testID});
-
-    await driver.press();
-
-    expect(onValueChange).not.toHaveBeenCalled();
+        expect(onChangeValidity).not.toHaveBeenCalled();
+      });
+    });
   });
 });
