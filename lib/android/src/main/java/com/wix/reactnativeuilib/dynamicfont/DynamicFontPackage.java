82793f2bba35e952111b7fcf5ab94925c511c188
M-i-k-e-l <michaelle@wix.com>
2023-09-13 11:05:30 +0300

    Add empty lines

--- lib/android/src/main/java/com/wix/reactnativeuilib/dynamicfont/DynamicFontPackage.java
+++ lib/android/src/main/java/com/wix/reactnativeuilib/dynamicfont/DynamicFontPackage.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class DynamicFontPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
