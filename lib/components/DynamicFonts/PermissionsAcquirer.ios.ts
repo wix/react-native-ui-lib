b3fc748da22f9f80fb6e8cf7a98d362fa253e5a5
M-i-k-e-l <michaelle@wix.com>
2023-09-19 10:42:21 +0300

    Remove checkPermissions from ios

--- lib/components/DynamicFonts/PermissionsAcquirer.ios.ts
+++ lib/components/DynamicFonts/PermissionsAcquirer.ios.ts
@@ -1,8 +1,4 @@
 export default class PermissionsAcquirer {
-  public async checkPermissions() {
-    return Promise.resolve(true);
-  }
-
   public async getPermissions() {
     return Promise.resolve();
   }
