8cad1f8288f463e0c02d33a8a2965ea99bc08403
M-i-k-e-l <michaelle@wix.com>
2023-11-01 10:43:42 +0200

    DynamicFonts - fix error in web

--- lib/components/DynamicFonts/index.ts
+++ lib/components/DynamicFonts/index.ts
@@ -1,10 +1,9 @@
-import {Platform} from 'react-native';
 import FontLoader, {FontExtension, LoadFontInput} from './FontLoader';
 import FontDownloader, {FontDownloaderProps} from './FontDownloader';
-import PermissionsAcquirerAndroid, {PermissionsAcquirerProps} from './PermissionsAcquirer.android';
-import PermissionsAcquirerIOS from './PermissionsAcquirer.ios';
+import type {PermissionsAcquirerProps} from './PermissionsAcquirer.android';
+// @ts-expect-error
+import PermissionsAcquirer from './PermissionsAcquirer';
 import NoPermissionsAcquirer from './NoPermissionsAcquirer';
-const PermissionsAcquirer = Platform.OS === 'android' ? PermissionsAcquirerAndroid : PermissionsAcquirerIOS;
 
 const DEFAULT_FONT_LOAD_ERROR_MESSAGE = 'Unable to load this font.';
 
