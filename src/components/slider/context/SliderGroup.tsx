995239e9ff684e08f3822f5593a8a6147fb0c777
Nitzan Yizhar <nitzany@wix.com>
2023-11-29 16:59:37 +0200

    Memoized provider value

--- src/components/slider/context/SliderGroup.tsx
+++ src/components/slider/context/SliderGroup.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {StyleProp, ViewStyle} from 'react-native';
 import {Colors} from '../../../style';
 import SliderContext from './SliderContext';
@@ -16,14 +16,17 @@ const SliderGroup = (props: SliderGroupProps) => {
   const {color, onValueChange, children} = props;
   const [value, setValue] = useState(Colors.getHSL(color));
 
-  const valueSetter = (value: tinycolor.ColorFormats.HSLA) => {
+  const valueSetter = useCallback((value: tinycolor.ColorFormats.HSLA) => {
     setValue(value);
     onValueChange?.(Colors.getHexString(value));
-  };
+  },
+  [onValueChange]);
+
+  const providerValue = useMemo(() => ({value, setValue: valueSetter}), [value, valueSetter]);
 
   return (
     <View {...props}>
-      <SliderContext.Provider value={{value, setValue: valueSetter}}>{children}</SliderContext.Provider>
+      <SliderContext.Provider value={providerValue}>{children}</SliderContext.Provider>
     </View>
   );
 };
