75e5c6cb35d0081b7bbcc1fac9bcf3718ec0c877
Amit Shwarts <amitshw@wix.com>
2024-01-03 14:04:46 +0200

    fix: [WOAUILIB-3903] TextField multiline auto-expanding on web

---
+++ src/components/textField/textInput/index.web.tsx
@@ -0,0 +1,43 @@
+import React, {type BaseSyntheticEvent, useCallback} from 'react';
+import {TextInput as RNTextInput, type TextInputChangeEventData, type TextInputProps, type LayoutChangeEvent} from 'react-native';
+
+// we need this wrapper to TextInput on web because of multiline bug in react-native-web
+// https://github.com/necolas/react-native-web/issues/795
+export const TextInput = (props: TextInputProps) => {
+  const {multiline, onChange, onLayout, ...other} = props;
+
+  const adjustInputHeight = useCallback((element: BaseSyntheticEvent<TextInputProps, TextInputProps>['target']) => {
+    element.style.height = 0;
+    const newHeight = element.offsetHeight - element.clientHeight + element.scrollHeight;
+    element.style.height = `${newHeight}px`;
+  }, []);
+
+  const _onLayout = useCallback((event: LayoutChangeEvent) => {
+    const element = event?.target;
+
+    if (element && multiline) {
+      adjustInputHeight(element);
+    }
+
+    onLayout?.(event);
+  }, [multiline, onLayout, adjustInputHeight]);
+
+  const _onChange = useCallback((event: BaseSyntheticEvent<TextInputChangeEventData>) => {
+    const element = event?.target || event?.nativeEvent?.target;
+    
+    if (element && multiline) {
+      adjustInputHeight(element);
+    }
+
+    onChange?.(event);
+  }, [multiline, onChange, adjustInputHeight]);
+
+  return (
+    <RNTextInput
+      {...other}
+      multiline={multiline}
+      onChange={_onChange}
+      onLayout={_onLayout}
+    />
+  );
+};
