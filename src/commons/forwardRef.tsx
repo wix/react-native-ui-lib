b1149bdfef882e09f88cabff6546e8f8e92763a3
Nitzan Yizhar <nitzany@wix.com>
2024-01-09 12:45:42 +0200

    Changed forwardRef typings

--- src/commons/forwardRef.tsx
+++ src/commons/forwardRef.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {useRef, Ref, ForwardedRef, useImperativeHandle, useEffect} from 'react';
+import {TextInput, View, Text} from 'react-native';
 import hoistStatics from 'hoist-non-react-statics';
 
-export interface ForwardRefInjectedProps {
+export interface ForwardRefInjectedProps<T> {
   /**
    * The forwarded ref of the containing element
    */
-  forwardedRef: any;
+  forwardedRef: Ref<T>;
 }
 
-export default function forwardRef<P = any, STATICS = {}>(WrappedComponent: React.ComponentType<P>): React.ComponentType<P> & STATICS {
-  function forwardRef(props: P, ref: any) {
+export default function forwardRef<P = {}, STATICS extends {[key: string]: true} = {}, T = any>(WrappedComponent: React.ComponentType<P & ForwardRefInjectedProps<T>>) {
+  function forwardRef(props: P, ref: Ref<T>) {
     return <WrappedComponent {...props} forwardedRef={ref}/>;
   }
 
-  const ForwardedComponent = React.forwardRef(forwardRef);
+  const ForwardedComponent = React.forwardRef<T, P>(forwardRef);
 
-  hoistStatics(ForwardedComponent, WrappedComponent);
-  ForwardedComponent.displayName = WrappedComponent.displayName;
+  const FinalComponent = hoistStatics<typeof ForwardedComponent, typeof WrappedComponent, STATICS>(ForwardedComponent,
+    WrappedComponent);
+  FinalComponent.displayName = WrappedComponent.displayName;
   //@ts-ignore
-  ForwardedComponent.propTypes = WrappedComponent.propTypes;
+  FinalComponent.propTypes = WrappedComponent.propTypes;
   //@ts-ignore
-  ForwardedComponent.defaultProps = WrappedComponent.defaultProps;
+  FinalComponent.defaultProps = WrappedComponent.defaultProps;
 
-  return ForwardedComponent as any;
+  return FinalComponent;
 }
+
+type TestProps = {
+  labelColor: string;
+};
+
+type TestRef = {
+  hello: () => void;
+  focus: () => void;
+};
+
+const Test = (props: TestProps & ForwardRefInjectedProps<TestRef>) => {
+  const {labelColor, forwardedRef} = props;
+  useImperativeHandle(forwardedRef, () => ({hello: () => console.log('hello'), focus: () => console.log('focus')}), []);
+  return (
+    <View>
+      <Text style={{color: labelColor}}>Test passing ref</Text>
+      <TextInput/>
+    </View>
+  );
+};
+const T = forwardRef<TestProps, {}, TestRef>(Test);
+
+const App = () => {
+  const inputRef = useRef<TestRef>(null);
+  useEffect(() => {
+    inputRef.current?.hello();
+  }, []);
+  return <T labelColor="red" ref={inputRef}/>;
+};
