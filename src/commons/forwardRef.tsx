76738f68d4bafb1f21e826d03ef4be4186aaadf7
Nitzan Yizhar <nitzany@wix.com>
2024-01-10 14:44:09 +0200

    Added typings to forwardRef and asBaseComponents

--- src/commons/forwardRef.tsx
+++ src/commons/forwardRef.tsx
@@ -1,4 +1,4 @@
-import React, {Ref} from 'react';
+import React, {Ref, ComponentType} from 'react';
 import hoistStatics from 'hoist-non-react-statics';
 
 export interface ForwardRefInjectedProps<T> {
@@ -8,14 +8,16 @@ export interface ForwardRefInjectedProps<T> {
   forwardedRef: Ref<T>;
 }
 
-export default function forwardRef<P = {}, STATICS extends {[key: string]: true} = {}, RefInterface = any>(WrappedComponent: React.ComponentType<P & ForwardRefInjectedProps<RefInterface>>) {
+type DerivedStatics<T> = {[K in keyof T]: true};
+
+export default function forwardRef<P, STATICS extends object, RefInterface = any>(WrappedComponent: ComponentType<P & ForwardRefInjectedProps<RefInterface>>) {
   function forwardRef(props: P, ref: Ref<RefInterface>) {
     return <WrappedComponent {...props} forwardedRef={ref}/>;
   }
 
   const ForwardedComponent = React.forwardRef<RefInterface, P>(forwardRef);
 
-  const FinalComponent = hoistStatics<typeof ForwardedComponent, typeof WrappedComponent, STATICS>(ForwardedComponent,
+  const FinalComponent = hoistStatics<typeof ForwardedComponent, typeof WrappedComponent, DerivedStatics<STATICS>>(ForwardedComponent,
     WrappedComponent);
   FinalComponent.displayName = WrappedComponent.displayName;
   //@ts-ignore
