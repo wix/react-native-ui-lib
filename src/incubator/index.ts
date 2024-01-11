2d3ba2e2985df266c7d6102ae6c386c3f7fa695e
Nitzan Yizhar <nitzany@wix.com>
2024-01-11 14:58:10 +0200

    Fixed Slider interface typings and screen

--- src/incubator/index.ts
+++ src/incubator/index.ts
@@ -13,7 +13,7 @@ export {
 export {default as Toast, ToastProps, ToastPresets} from './toast';
 export {default as TouchableOpacity, TouchableOpacityProps} from './TouchableOpacity';
 export {default as PanView, PanViewProps, PanViewDirections, PanViewDismissThreshold} from './panView';
-export {default as Slider} from './Slider';
+export {default as Slider, SliderRef} from './Slider';
 export {default as Dialog, DialogProps, DialogHeaderProps, DialogStatics, DialogImperativeMethods} from './Dialog';
 // TODO: delete exports after fully removing from private
 export {default as ChipsInput, ChipsInputProps, ChipsInputChangeReason, ChipsInputChipProps} from '../components/chipsInput';
