90daf4e447ebde9944fc94ac9d36e878971feb35
Nitzan Yizhar <nitzany@wix.com>
2024-01-15 12:24:31 +0200

    Tiny change

--- src/incubator/Dialog/Dialog.driver.new.ts
+++ src/incubator/Dialog/Dialog.driver.new.ts
@@ -1,10 +1,10 @@
-import {DialogProps} from '../../components/dialog/index';
+import {DialogProps} from '../index';
 import {useComponentDriver, ComponentProps} from '../../testkit/new/Component.driver';
 import {ModalDriver} from '../../testkit/';
 
 export const DialogDriver = (props: ComponentProps) => {
   const {renderTree, testID} = props;
   const driver = useComponentDriver<DialogProps>(props);
-  const modal = ModalDriver({renderTree, testID: `${testID}.modal`});
-  return {...driver, isVisible: modal.isVisible};
+  const dialogModal = ModalDriver({renderTree, testID: `${testID}.modal`});
+  return {...driver, isVisible: dialogModal.isVisible};
 };
