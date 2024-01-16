ab15328d36aa4420a982de79af948a8cd4d75bde
Inbal Tish <inbalti@wix.com>
2024-01-16 10:17:16 +0200

    removing switch

--- src/testkit/index.ts
+++ src/testkit/index.ts
@@ -4,5 +4,4 @@ export {useScrollableDriver} from './new/useScrollable.driver';
 export {useDraggableDriver} from './new/useDraggable.driver';
 
 export {ViewDriver} from '../components/view/View.driver.new';
-export {SwitchDriver} from '../components/switch/switch.driver';
 export {CheckboxDriver} from '../components/checkbox/Checkbox.driver';
