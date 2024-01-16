0d78cf728c9c545ec62396b8bf1c3d3a6d209e1c
Inbal Tish <inbalti@wix.com>
2024-01-16 10:23:39 +0200

    Adding the new drivares

--- src/testkit/index.ts
+++ src/testkit/index.ts
@@ -4,4 +4,8 @@ export {useScrollableDriver} from './new/useScrollable.driver';
 export {useDraggableDriver} from './new/useDraggable.driver';
 
 export {ViewDriver} from '../components/view/View.driver.new';
+export {TextDriver} from '../components/text/Text.driver.new';
+export {CarouselDriver} from '../components/carousel/Carousel.driver.new';
+export {SortableListDriver} from '../components/sortableList/SortableList.driver.new';
+export {SortableListItemDriver} from '../components/sortableList/SortableListItem.driver.new';
 export {CheckboxDriver} from '../components/checkbox/Checkbox.driver';
