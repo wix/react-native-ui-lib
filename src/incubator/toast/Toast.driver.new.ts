b1e7f0ee8630a6fbb302552151ebb8f14dfda0b7
Nitzan Yizhar <nitzany@wix.com>
2024-01-16 13:30:52 +0200

    Added sanity checks

--- src/incubator/toast/Toast.driver.new.ts
+++ src/incubator/toast/Toast.driver.new.ts
@@ -1,15 +1,19 @@
 import {ToastProps} from '../index';
 import {useComponentDriver, ComponentProps} from '../../testkit/new/Component.driver';
 import {TextDriver} from '../../components/text/Text.driver.new';
-import { fireEvent } from '@testing-library/react-native';
+import {fireEvent} from '@testing-library/react-native';
 
-export const ToastDriver = (args: ComponentProps) => {
-  const {renderTree, testID} = args;
-  const driver = useComponentDriver<ToastProps>(args);
+export const ToastDriver = (props: ComponentProps) => {
+  const {renderTree, testID} = props;
+  const driver = useComponentDriver<ToastProps>(props);
+
+  // const isVisible = () => {
+
+  // };
 
   const getMessage = () => {
     const messageDriver = TextDriver({renderTree, testID: `${testID}-message`});
-    return messageDriver.getText();    
+    return messageDriver;
   };
 
   const pressOnAction = () => {
---
+++ src/incubator/toast/__test__/index.spec.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import Toast, {ToastProps} from '../index';
+import {ToastDriver} from '../Toast.driver.new';
+
+const testID = 'toast';
+
+const TestCase = (props: ToastProps) => {
+  return <Toast visible testID={testID} {...props}/>;
+};
+
+const getDriver = (props?: ToastProps) => {
+  const renderTree = render(<TestCase {...props}/>);
+  const toastDriver = ToastDriver({renderTree, testID});
+  return {render, toastDriver};
+};
+
+describe('Sanity checks', () => {
+  it('Should show toast', () => {
+    const {toastDriver} = getDriver();
+    expect(toastDriver.exists()).toBeTruthy();
+  });
+  it('Should dismiss after time', async () => {
+    const TIME = 50;
+    const dismissFn = jest.fn();
+    const {toastDriver} = getDriver({autoDismiss: TIME, onDismiss: dismissFn});
+    expect(toastDriver.exists()).toBeTruthy();
+    expect(dismissFn).not.toHaveBeenCalled();
+    await new Promise(r => setTimeout(r, TIME + 100));
+    expect(dismissFn).toHaveBeenCalled();
+  });
+  it('Should press on action button', () => {
+    const actionFn = jest.fn();
+    const {toastDriver} = getDriver({action: {onPress: actionFn}});
+    expect(actionFn).not.toHaveBeenCalled();
+    toastDriver.pressOnAction();
+    expect(actionFn).toHaveBeenCalled();
+  });
+});
