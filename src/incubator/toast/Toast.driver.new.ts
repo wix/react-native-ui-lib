88652629906b18408b01cfa91be5aa3e591c5efa
Nitzan Yizhar <nitzany@wix.com>
2024-01-17 12:05:44 +0200

    Added usage of button driver

--- src/incubator/toast/Toast.driver.new.ts
+++ src/incubator/toast/Toast.driver.new.ts
@@ -2,24 +2,18 @@ import {ToastProps} from '../index';
 import {useComponentDriver, ComponentProps} from '../../testkit/new/Component.driver';
 import {TextDriver} from '../../components/text/Text.driver.new';
 import {fireEvent} from '@testing-library/react-native';
+import {ButtonDriver} from '../../components/button/Button.driver.new';
 
 export const ToastDriver = (props: ComponentProps) => {
   const {renderTree, testID} = props;
   const driver = useComponentDriver<ToastProps>(props);
-
-  // const isVisible = () => {
-
-  // };
+  const actionButtonDriver = ButtonDriver({renderTree, testID: `${testID}-action`});
 
   const getMessage = () => {
     const messageDriver = TextDriver({renderTree, testID: `${testID}-message`});
     return messageDriver;
   };
+  
 
-  const pressOnAction = () => {
-    const actionButton = renderTree.queryByTestId(`${testID}-action`);
-    actionButton && fireEvent.press(actionButton);
-  };
-
-  return {...driver, getMessage, pressOnAction};
+  return {...driver, getMessage, pressOnAction: actionButtonDriver.press};
 };
