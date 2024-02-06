f4f67506ef39e20f621d5743c46f57546a7d4023
adids1221 <adids1221@gmail.com>
2024-02-06 11:56:07 +0200

    screen fixes

--- demo/src/screens/componentScreens/SegmentedControlScreen.tsx
+++ demo/src/screens/componentScreens/SegmentedControlScreen.tsx
@@ -1,16 +1,6 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback} from 'react';
 import {StyleSheet} from 'react-native';
-import {
-  Text,
-  View,
-  Colors,
-  Switch,
-  SegmentedControl,
-  Assets,
-  Spacings,
-  BorderRadiuses,
-  Typography
-} from 'react-native-ui-lib';
+import {Text, View, Colors, SegmentedControl, Assets, Spacings, BorderRadiuses, Typography} from 'react-native-ui-lib';
 
 const segments = {
   first: [{label: 'Left'}, {label: 'Right'}],
@@ -26,64 +16,55 @@ const segments = {
   ],
   forth: [{label: 'With'}, {label: 'Custom'}, {label: 'Style'}],
   fifth: [{label: 'Full'}, {label: 'Width'}],
-  sixth: [{label: 'Full'}, {label: 'Width'}, {label: 'With'}, {label: 'A'}, {label: 'Very Long Segment'}]
+  sixth: [{label: 'Full'}, {label: 'Width'}, {label: 'With'}, {label: 'A'}, {label: 'Very Long Segment'}],
+  seventh: [{label: '$'}, {label: '%'}]
 };
 
 const SegmentedControlScreen = () => {
-  const [useCustomTypography, setUseCustomTypography] = useState(false);
-
   const onChangeIndex = useCallback((index: number) => {
     console.warn('Index ' + index + ' of the second segmentedControl was pressed');
   }, []);
 
   return (
-    <View center bottom padding-page>
-      <Text text40 $textDefault marginV-s2>
+    <View flex bottom padding-page>
+      <Text center text40 $textDefault>
         Segmented Control
       </Text>
-      <View marginV-s4 row>
-        <Text text80>Use custom Typography:</Text>
-        <Switch value={useCustomTypography} onValueChange={setUseCustomTypography} marginL-10/>
-      </View>
-      <View marginT-s3 center>
-        <SegmentedControl
-          segments={segments.first}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
-        />
-        <SegmentedControl
-          onChangeIndex={onChangeIndex}
-          containerStyle={styles.container}
-          segments={segments.second}
-          initialIndex={2}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
-        />
-        <SegmentedControl
-          containerStyle={styles.container}
-          activeColor={Colors.$textDangerLight}
-          segments={segments.third}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
-        />
-        <SegmentedControl
-          containerStyle={styles.container}
-          segments={segments.forth}
-          activeColor={Colors.$textDefault}
-          borderRadius={BorderRadiuses.br20}
-          backgroundColor={Colors.$backgroundInverted}
-          activeBackgroundColor={Colors.$backgroundNeutralIdle}
-          inactiveColor={Colors.$textDisabled}
-          style={styles.customStyle}
-          segmentsStyle={styles.customSegmentsStyle}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
-        />
+      <View flex marginT-s8>
+        <View center>
+          <SegmentedControl segments={segments.first}/>
+          <SegmentedControl
+            onChangeIndex={onChangeIndex}
+            containerStyle={styles.container}
+            segments={segments.second}
+            initialIndex={2}
+          />
+          <SegmentedControl
+            containerStyle={styles.container}
+            activeColor={Colors.$textDangerLight}
+            segments={segments.third}
+          />
+          <SegmentedControl
+            containerStyle={styles.container}
+            segments={segments.forth}
+            activeColor={Colors.$textDefault}
+            borderRadius={BorderRadiuses.br20}
+            backgroundColor={Colors.$backgroundInverted}
+            activeBackgroundColor={Colors.$backgroundNeutralIdle}
+            inactiveColor={Colors.$textDisabled}
+            style={styles.customStyle}
+            segmentsStyle={styles.customSegmentsStyle}
+          />
+        </View>
+        <SegmentedControl containerStyle={styles.container} segments={segments.fifth}/>
+        <SegmentedControl containerStyle={styles.container} segments={segments.sixth}/>
+        <Text marginT-s4 center>
+          Custom Typography
+        </Text>
         <SegmentedControl
           containerStyle={styles.container}
-          segments={segments.fifth}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
-        />
-        <SegmentedControl
-          containerStyle={styles.container}
-          segments={segments.sixth}
-          segmentLabelStyle={useCustomTypography && styles.customTypography}
+          segments={segments.seventh}
+          segmentLabelStyle={styles.customTypography}
         />
       </View>
     </View>
@@ -102,7 +83,7 @@ const styles = StyleSheet.create({
     height: 50
   },
   customTypography: {
-    ...Typography.text80BO
+    ...Typography.text70
   }
 });
 
