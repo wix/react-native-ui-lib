0ae58dff47a77342bebf47c25f63437fe5875dcb
adids1221 <adids1221@gmail.com>
2024-02-07 16:24:59 +0200

    icon support in badage

--- demo/src/screens/componentScreens/IconScreen.tsx
+++ demo/src/screens/componentScreens/IconScreen.tsx
@@ -1,23 +1,25 @@
 import React, {useState} from 'react';
-import {Assets, View, Icon, Text, Slider, Switch, GradientSlider} from 'react-native-ui-lib';
+import {Assets, View, Icon, Text, Slider, Switch, GradientSlider, Colors} from 'react-native-ui-lib';
 
 const IconScreen = () => {
   const [size, setSize] = useState(24);
   const [customSize, setCustomSize] = useState(false);
   const [color, setColor] = useState<string | number>();
   const [customColor, setCustomColor] = useState(false);
+  const [useBadge, setUseBadge] = useState(false);
+  const [usePimpleBadge, setUsePimpleBadge] = useState(false);
 
   return (
     <View padding-page>
       <Text h1 marginB-s4>
         Icon Screen
       </Text>
-      <View center>
+      <View center margin-page>
         <Icon
-          margin-30
           size={customSize ? size : undefined}
-          tintColor={customColor ? color as string : undefined}
+          tintColor={customColor ? (color as string) : undefined}
           source={Assets.icons.search}
+          badgeProps={useBadge ? {backgroundColor: Colors.red30, label: !usePimpleBadge ? '5' : undefined} : undefined}
         />
       </View>
 
@@ -36,6 +38,20 @@ const IconScreen = () => {
       </View>
       <GradientSlider type={GradientSlider.types.HUE} color={color as string} onValueChange={setColor}/>
       <Text marginT-s2>Custom color: {color || '#000000'}</Text>
+
+      <View marginV-s5>
+        <Text marginR-s2 text80BO>
+          Badge Settings:
+        </Text>
+        <View marginT-s3 row>
+          <Text marginR-s2>Use Badge:</Text>
+          <Switch value={useBadge} onValueChange={setUseBadge}/>
+        </View>
+        <View marginT-s3 row>
+          <Text marginR-s2>Pimple Badge:</Text>
+          <Switch value={usePimpleBadge} onValueChange={setUsePimpleBadge}/>
+        </View>
+      </View>
     </View>
   );
 };
--- src/components/icon/index.tsx
+++ src/components/icon/index.tsx
@@ -1,10 +1,13 @@
 import isUndefined from 'lodash/isUndefined';
+import get from 'lodash/get';
 import React, {useMemo, forwardRef} from 'react';
-import {Image, ImageProps, StyleSheet} from 'react-native';
+import {Image, ImageProps, StyleSheet, StyleProp, ViewStyle} from 'react-native';
 import {asBaseComponent, BaseComponentInjectedProps, MarginModifiers, Constants} from '../../commons/new';
 import {ComponentStatics} from '../../typings/common';
 import {getAsset, isSvg, isBase64ImageContent} from '../../utils/imageUtils';
 import {RecorderProps} from '../../typings/recorderTypes';
+import Badge, {BadgeProps} from '../badge';
+import View from '../view';
 import SvgImage from '../svgImage';
 
 export type IconProps = Omit<ImageProps, 'source'> &
@@ -18,6 +21,14 @@ export type IconProps = Omit<ImageProps, 'source'> &
      * the asset group, default is "icons"
      */
     assetGroup?: string;
+    /**
+     * Badge props passed down to Badge component
+     */
+    badgeProps?: BadgeProps;
+    /**
+     * Hide badge shown on button
+     */
+    hideBadge?: boolean;
     /**
      * the icon tint
      */
@@ -41,11 +52,11 @@ export type IconProps = Omit<ImageProps, 'source'> &
 
 type Props = IconProps & BaseComponentInjectedProps;
 
-const defaultWebIconSize = 16;
+const DEFAULT_WEB_ICON_SIZE = 16;
 
 const Icon = forwardRef((props: Props, ref: any) => {
   const {
-    size = Constants.isWeb ? defaultWebIconSize : undefined,
+    size = Constants.isWeb ? DEFAULT_WEB_ICON_SIZE : undefined,
     tintColor,
     style,
     supportRTL,
@@ -54,11 +65,49 @@ const Icon = forwardRef((props: Props, ref: any) => {
     assetName,
     modifiers,
     recorderTag,
+    badgeProps,
+    hideBadge,
     ...others
   } = props;
   const {margins} = modifiers;
+  const shouldShowBadge = !hideBadge && badgeProps;
   const iconSize = size ? {width: size, height: size} : undefined;
   const shouldFlipRTL = supportRTL && Constants.isRTL;
+
+  const getBadgeStyling = (): StyleProp<ViewStyle> => {
+    const containerStyle = badgeProps?.containerStyle;
+    const badgePosition: StyleProp<ViewStyle> = {
+      position: 'absolute',
+      top: -3
+    };
+    if (isPimple()) {
+      //badge size 10
+      badgePosition.right = (size || 24) / 10;
+      badgePosition.top = (size || 24) / 10;
+    } else {
+      //badge size 16
+      badgePosition.right = (size || 24) / 16;
+      badgePosition.top = (size || 24) / 16;
+    }
+    return [badgePosition, containerStyle];
+  };
+
+  const getBadgeLabel = () => {
+    const {badgeProps} = props;
+    const label = get(badgeProps, 'label');
+    return label;
+  };
+
+  const getBadgeSize = () => {
+    const size = isPimple() ? 10 : 16;
+    return size;
+  };
+
+  const isPimple = () => {
+    if (getBadgeLabel() === undefined) {
+      return true;
+    }
+  };
 
   const iconSource = useMemo(() => {
     if (!isUndefined(assetName)) {
@@ -83,7 +132,21 @@ const Icon = forwardRef((props: Props, ref: any) => {
     return renderImage();
   }
 
-  return isSvg(source) ? renderSvg() : renderImage();
+  return (
+    <View style={{borderWidth: 1}}>
+      {isSvg(source) ? renderSvg() : renderImage()}
+      {shouldShowBadge && (
+        <Badge
+          pointerEvents={'none'}
+          {...badgeProps}
+          label={getBadgeLabel()}
+          size={getBadgeSize()}
+          containerStyle={getBadgeStyling()}
+          testID={`${props?.testID}.badge`}
+        />
+      )}
+    </View>
+  );
 });
 
 Icon.displayName = 'Icon';
