c67b069b776e660786f42b0680b052ab2b38dc0b
M-i-k-e-l <michaelle@wix.com>
2024-02-08 09:42:18 +0200

    Testing ComponentFactory

---
+++ src/services/ComponentFactory.tsx
@@ -0,0 +1,18 @@
+import React, {ComponentType} from 'react';
+import Button, {ButtonProps} from '../components/button';
+
+type Components = 'Button';
+type ComponentsTypesMap = {Button: ButtonProps};
+const ComponentsMap = new Map<Components, React.ElementType>();
+ComponentsMap.set('Button', Button);
+
+// eslint-disable-next-line max-len
+export function createComponent<c extends Components, Props extends ComponentsTypesMap[c]>(component: Components): ComponentType<Props> {
+  const ModifiedComponent = (props: Props) => {
+    const Component = ComponentsMap.get(component);
+    // @ts-expect-error
+    return <Component {...props}/>;
+  };
+
+  return ModifiedComponent;
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
+export * as ComponentFactory from './ComponentFactory';
 export {default as LogService} from './LogService';
 export {default as HapticService, HapticType} from './HapticService';
