93dd11ed4f48c888d0878df148cfb480321d968b
M-i-k-e-l <michaelle@wix.com>
2024-02-11 14:48:20 +0200

    A few renames

--- src/services/ComponentFactory.tsx
+++ src/services/ComponentFactory.tsx
@@ -1,13 +1,13 @@
 import React, {ComponentType} from 'react';
 import Button, {ButtonProps} from '../components/button';
 
-type Components = 'Button';
+export type SupportedComponents = 'Button';
 type ComponentsTypesMap = {Button: ButtonProps};
-const ComponentsMap = new Map<Components, React.ElementType>();
+const ComponentsMap = new Map<SupportedComponents, React.ElementType>();
 ComponentsMap.set('Button', Button);
 
 // eslint-disable-next-line max-len
-export function createComponent<c extends Components, Props extends ComponentsTypesMap[c]>(component: Components): ComponentType<Props> {
+export function createComponent<c extends SupportedComponents, Props extends ComponentsTypesMap[c]>(component: c): ComponentType<Props> {
   const ModifiedComponent = (props: Props) => {
     const Component = ComponentsMap.get(component);
     if (Component) {
