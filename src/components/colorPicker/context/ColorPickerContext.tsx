122b4a8940c8605054cd08c06bfb2c2be02b2d05
Nitzan Yizhar <nitzany@wix.com>
2024-02-19 15:57:23 +0200

    moved colorpicker to new location

---
+++ src/components/colorPicker/context/ColorPickerContext.tsx
@@ -0,0 +1,85 @@
+import React, {PropsWithChildren, createContext, useCallback, useMemo} from 'react';
+import {SharedValue, runOnJS, useAnimatedReaction, useSharedValue, useWorkletCallback} from 'react-native-reanimated';
+import type {ColorFormats} from 'tinycolor2';
+import {getValidColorString, hslToHex} from '../ColorPickerPresenter';
+
+type HSLA = ColorFormats.HSLA;
+export type SharedColorContextType = {
+  value: SharedValue<HSLA>;
+  hex: SharedValue<string | undefined>;
+  isValid: SharedValue<boolean>;
+  updateAlpha: (a: number) => void;
+  updateHue: (h: number) => void;
+  updateSaturation: (s: number) => void;
+  updateLightness: (l: number) => void;
+  reset: () => void;
+};
+ 
+export const ColorPickerContext = createContext<SharedColorContextType | null>(null);
+
+interface ColorPickerContextProviderProps<T> extends PropsWithChildren {
+  color: T;
+}
+
+const DEFAULT_VALUE = {h: 0, s: 0, l: 0, a: 0};
+
+export const ColorPickerContextProvider = (props: ColorPickerContextProviderProps<SharedValue<HSLA>>) => {
+  const {color, children} = props;
+  // const color = useSharedValue<HSLA>(initialValue ?? DEFAULT_VALUE);
+
+  const reset = useCallback(() => {
+    color.value = DEFAULT_VALUE;
+  }, [color]);
+
+  const updateAlpha = useWorkletCallback((a: number) => {
+    'worklet';
+    color.value = {...color.value, a};
+  },
+  [color]);
+
+  const updateHue = useWorkletCallback((h: number) => {
+    'worklet';
+    color.value = {...color.value, h};
+  },
+  [color]);
+  const updateLightness = useWorkletCallback((l: number) => {
+    'worklet';
+    color.value = {...color.value, l};
+  },
+  [color]);
+  const updateSaturation = useWorkletCallback((s: number) => {
+    'worklet';
+    color.value = {...color.value, s};
+  },
+  [color]);
+
+  const hex = useSharedValue<string | undefined>(undefined);
+  const isValid = useSharedValue<boolean>(false);
+
+  useAnimatedReaction(() => {
+    const {h, s, l} = color.value;
+    return getValidColorString(hslToHex({h, s: s * 100, l: l * 100}));
+  },
+  (current, prev) => {
+    if (current.hex !== prev?.hex) {
+      hex.value = current.hex?.toUpperCase();
+    }
+    if (current.valid !== prev?.valid) {
+      isValid.value = current.valid;
+    }
+  });
+
+  const providerValue = useMemo(() => {
+    return {
+      value: color,
+      hex,
+      isValid,
+      updateAlpha,
+      updateHue,
+      updateLightness,
+      updateSaturation,
+      reset
+    };
+  }, [color, reset, updateAlpha, updateHue, updateLightness, updateSaturation, hex, isValid]);
+  return <ColorPickerContext.Provider value={providerValue}>{children}</ColorPickerContext.Provider>;
+};
