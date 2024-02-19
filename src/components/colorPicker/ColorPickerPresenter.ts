30cecea25d553de043ddc92d5bb3a9fc472c315f
Nitzan Yizhar <nitzany@wix.com>
2024-02-19 16:05:27 +0200

    Made some functions into worklet functions and added functions

--- src/components/colorPicker/ColorPickerPresenter.ts
+++ src/components/colorPicker/ColorPickerPresenter.ts
@@ -10,8 +10,14 @@ export function getColorValue(color?: string) {
   }
   return color.replace('#', '');
 }
+
+export function isTransparent(color?: string) {
+  'worklet';
+  return color && color.toUpperCase() === 'transparent'.toUpperCase();
+}
 export function getHexColor(text: string) {
-  if (!Colors.isTransparent(text)) {
+  'worklet';
+  if (!isTransparent(text)) {
     const trimmed = text.replace(/\s+/g, '');
     const hex = `#${trimmed}`;
     return hex;
@@ -19,15 +25,36 @@ export function getHexColor(text: string) {
   return text;
 }
 
+export function isValidHex(string: string) {
+  'worklet';
+  return /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(string);
+}
+
 export function getValidColorString(text?: string) {
+  'worklet';
   if (text) {
     const hex = getHexColor(text);
 
-    if (Colors.isValidHex(hex)) {
+    if (isValidHex(hex)) {
       return {hex, valid: true};
     }
   }
   return {undefined, valid: false};
+}
+
+export function hslToHex(hsl: {h: number; s: number; l: number}) {
+  'worklet';
+  let {h, s, l} = hsl;
+  l /= 100;
+  const a = (s * Math.min(l, 1 - l)) / 100;
+  const f = (n: number) => {
+    const k = (n + h / 30) % 12;
+    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+    return Math.round(255 * color)
+      .toString(16)
+      .padStart(2, '0'); // convert to Hex and prefix "0" if needed
+  };
+  return `${f(0)}${f(8)}${f(4)}`;
 }
 
 export function getHexString(color: HSLColor) {
