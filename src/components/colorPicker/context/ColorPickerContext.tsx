bffda7df91a62432c97ee1a42c9f0794e49bb5ed
Nitzan Yizhar <nitzany@wix.com>
2024-02-20 10:28:27 +0200

    Added stateful color to the color context so it coudl change the initial value of the sliders

--- src/components/colorPicker/context/ColorPickerContext.tsx
+++ src/components/colorPicker/context/ColorPickerContext.tsx
@@ -1,4 +1,4 @@
-import React, {PropsWithChildren, createContext, useCallback, useMemo} from 'react';
+import React, {PropsWithChildren, createContext, useCallback, useMemo, useState} from 'react';
 import {SharedValue, runOnJS, useAnimatedReaction, useSharedValue, useWorkletCallback} from 'react-native-reanimated';
 import type {ColorFormats} from 'tinycolor2';
 import {getValidColorString, hslToHex} from '../ColorPickerPresenter';
@@ -8,6 +8,7 @@ export type SharedColorContextType = {
   value: SharedValue<HSLA>;
   hex: SharedValue<string | undefined>;
   isValid: SharedValue<boolean>;
+  statefulColor: ColorFormats.HSLA;
   setColor: (hsla: HSLA) => void;
   updateAlpha: (a: number) => void;
   updateHue: (h: number) => void;
@@ -15,7 +16,7 @@ export type SharedColorContextType = {
   updateLightness: (l: number) => void;
   reset: () => void;
 };
- 
+
 export const ColorPickerContext = createContext<SharedColorContextType | null>(null);
 
 interface ColorPickerContextProviderProps<T> extends PropsWithChildren {
@@ -26,7 +27,7 @@ const DEFAULT_VALUE = {h: 0, s: 0, l: 0, a: 0};
 
 export const ColorPickerContextProvider = (props: ColorPickerContextProviderProps<SharedValue<HSLA>>) => {
   const {color, children} = props;
-  // const color = useSharedValue<HSLA>(initialValue ?? DEFAULT_VALUE);
+  const [statefulColor, setStatefulColor] = useState(color.value);
 
   const reset = useCallback(() => {
     color.value = DEFAULT_VALUE;
@@ -40,6 +41,7 @@ export const ColorPickerContextProvider = (props: ColorPickerContextProviderProp
 
   const updateHue = useWorkletCallback((h: number) => {
     'worklet';
+    console.log(`Nitzan - h`, h);
     color.value = {...color.value, h};
   },
   [color]);
@@ -59,20 +61,26 @@ export const ColorPickerContextProvider = (props: ColorPickerContextProviderProp
 
   useAnimatedReaction(() => {
     const {h, s, l} = color.value;
+    console.log(`Nitzan - changed`, h);
+    console.log(`Nitzan - hslToHex({h, s: s * 100, l: l * 100})`, hslToHex({h, s: s * 100, l: l * 100}));
     return getValidColorString(hslToHex({h, s: s * 100, l: l * 100}));
   },
   (current, prev) => {
+    console.log(`Nitzan - current.hex, prev?.hex`, current.hex, prev?.hex);
     if (current.hex !== prev?.hex) {
-      hex.value = current.hex?.toUpperCase();
+      if (current.valid) {
+        console.log(`Nitzan - hex changing`, current.hex);
+        hex.value = current.hex?.toUpperCase();
+      }
     }
     if (current.valid !== prev?.valid) {
-      console.log(`Nitzan - new valid`, current.valid);
       isValid.value = current.valid;
     }
   });
 
   const setColor = useWorkletCallback((hsla: HSLA) => {
     color.value = hsla;
+    runOnJS(setStatefulColor)(hsla);
   });
 
   const providerValue = useMemo(() => {
@@ -80,6 +88,7 @@ export const ColorPickerContextProvider = (props: ColorPickerContextProviderProp
       value: color,
       hex,
       isValid,
+      statefulColor,
       setColor,
       updateAlpha,
       updateHue,
@@ -87,6 +96,6 @@ export const ColorPickerContextProvider = (props: ColorPickerContextProviderProp
       updateSaturation,
       reset
     };
-  }, [color, reset, updateAlpha, updateHue, updateLightness, updateSaturation, hex, isValid, setColor]);
+  }, [color, reset, updateAlpha, updateHue, updateLightness, updateSaturation, hex, isValid, setColor, statefulColor]);
   return <ColorPickerContext.Provider value={providerValue}>{children}</ColorPickerContext.Provider>;
 };
