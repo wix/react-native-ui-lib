b17e540044f958352479a76686683ce57757abab
Nitzan Yizhar <nitzany@wix.com>
2024-03-12 13:51:25 +0200

    changed implementation

--- src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
+++ src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
@@ -1,25 +1,17 @@
-import React, {isValidElement} from 'react';
+import _ from 'lodash';
 import {useComponentDriver, ComponentProps} from '../../testkit/new/Component.driver';
 import {WheelPickerDriver} from '../WheelPicker/WheelPicker.driver';
 import {SectionsWheelPickerProps} from './index';
 
-const isTestable = (props: unknown): props is {testID: string} => {
-  return typeof (props as {testID: string}).testID === 'string';
-};
-
 export const SectionsWheelPickerDriver = (props: ComponentProps) => {
   const driver = useComponentDriver<SectionsWheelPickerProps>(props);
-  const sectionsDrivers = React.Children.toArray(driver.getElement().props.children)
-    .map(section => {
-      if (isValidElement(section) && isTestable(section.props)) {
-        return WheelPickerDriver({
-          renderTree: props.renderTree,
-          testID: section.props.testID
-        });
-      }
-      return undefined;
-    })
-    .filter((driver): driver is ReturnType<typeof WheelPickerDriver> => !!driver);
-
-  return {...driver, sectionsDrivers};
+  const sections = driver.getElement()?.props?.children as SectionsWheelPickerProps['sections'];
+  const sectionsDrivers = _.map(sections, (section, index) => {
+    const sectionTestID = `${props.testID}.${index}`;
+    return WheelPickerDriver({
+      renderTree: props.renderTree,
+      testID: section?.testID || sectionTestID
+    });
+  });
+  return {...driver, sections, sectionsDrivers};
 };
