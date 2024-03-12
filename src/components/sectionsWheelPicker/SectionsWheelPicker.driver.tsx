5e55413b7e102f75d22d203a20dad686a2496453
Nitzan Yizhar <nitzany@wix.com>
2024-03-12 10:13:36 +0200

    Fixed testId and removed unused variable

--- src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
+++ src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
@@ -7,11 +7,11 @@ import {SectionsWheelPickerProps} from './index';
 export const SectionsWheelPickerDriver = (props: ComponentProps) => {
   const driver = useComponentDriver<SectionsWheelPickerProps>(props);
 
-  const sectionsDrivers = React.Children.toArray(driver.getElement().props.children).map((_section, index) => {
-    if (typeof _section === 'object' && 'props' in _section) {
+  const sectionsDrivers = React.Children.toArray(driver.getElement().props.children).map((section) => {
+    if (typeof section === 'object' && 'props' in section) {
       return WheelPickerDriver({
         renderTree: props.renderTree,
-        testID: `${props.testID}.${index}`
+        testID: section.props.testID
       });
     }
     return undefined;
