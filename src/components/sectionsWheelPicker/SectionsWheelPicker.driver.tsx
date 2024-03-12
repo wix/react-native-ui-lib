b778066dce4cf5019cdbcc30728c1c1247615ce7
Nitzan Yizhar <nitzany@wix.com>
2024-03-12 11:43:39 +0200

    change section type cheking to be mroe readable

--- src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
+++ src/components/sectionsWheelPicker/SectionsWheelPicker.driver.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, {isValidElement} from 'react';
 import {useComponentDriver, ComponentProps} from '../../testkit/new/Component.driver';
 import {WheelPickerDriver} from '../WheelPicker/WheelPicker.driver';
 import {SectionsWheelPickerProps} from './index';
 
+const isTestable = (props: unknown): props is {testID: string} => {
+  return typeof (props as {testID: string}).testID === 'string';
+};
+
 export const SectionsWheelPickerDriver = (props: ComponentProps) => {
   const driver = useComponentDriver<SectionsWheelPickerProps>(props);
-  const sectionsDrivers = React.Children.toArray(driver.getElement().props.children).map((section) => {
-    if (typeof section === 'object' && 'props' in section) {
-      return WheelPickerDriver({
-        renderTree: props.renderTree,
-        testID: section.props.testID
-      });
-    }
-    return undefined;
-  }).filter((driver): driver is ReturnType<typeof WheelPickerDriver> => !!driver);
+  const sectionsDrivers = React.Children.toArray(driver.getElement().props.children)
+    .map(section => {
+      if (isValidElement(section) && isTestable(section.props)) {
+        return WheelPickerDriver({
+          renderTree: props.renderTree,
+          testID: section.props.testID
+        });
+      }
+      return undefined;
+    })
+    .filter((driver): driver is ReturnType<typeof WheelPickerDriver> => !!driver);
 
   return {...driver, sectionsDrivers};
 };
