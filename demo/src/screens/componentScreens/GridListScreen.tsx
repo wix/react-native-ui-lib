bbeb393321304c572a0677460857cb5608934538
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2024-03-17 17:16:46 +0200

    Fix/grid list padding issue (#2982)
    * Fix issue with nested list and contentContainerStyle in GridList
    
    * Fix GridListItem itemSize typing to support string for percentage

--- demo/src/screens/componentScreens/GridListScreen.tsx
+++ demo/src/screens/componentScreens/GridListScreen.tsx
@@ -1,42 +1,76 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
-import {View, Text, Constants, GridList, Card, Spacings, BorderRadiuses, GridListProps} from 'react-native-ui-lib';
+import {
+  View,
+  Text,
+  Constants,
+  GridList,
+  Card,
+  Spacings,
+  BorderRadiuses,
+  GridListProps,
+  GridListItem
+} from 'react-native-ui-lib';
 import products from '../../data/products';
+import {renderBooleanOption} from '../ExampleScreenPresenter';
 
 class GridListScreen extends Component {
   state = {
-    orientation: Constants.orientation
+    orientation: Constants.orientation,
+    useGridListItem: false
   };
 
-  renderItem: GridListProps<typeof products[0]>['renderItem'] = ({item}) => {
+  renderHeader = () => {
     return (
-      <Card flex>
-        <Card.Section imageSource={{uri: item.mediaUrl}} imageStyle={styles.itemImage}/>
-        <View padding-s2>
-          <Text $textDefault>{item.name}</Text>
-          <Text $textDefault>{item.formattedPrice}</Text>
-          {item.inventory.status === 'Out of Stock' && (
-            <Text text90M $textDangerLight>
-              {item.inventory.status}
-            </Text>
-          )}
-        </View>
-      </Card>
+      <View>
+        <Text h1 marginB-s5>
+          GridList
+        </Text>
+        {renderBooleanOption.call(this, 'UseGridListItem', 'useGridListItem')}
+      </View>
     );
+  };
+
+  renderItem: GridListProps<(typeof products)[0]>['renderItem'] = ({item}) => {
+    const {useGridListItem} = this.state;
+
+    if (useGridListItem) {
+      return (
+        <GridListItem
+          // containerStyle={{width: '100%', borderWidth: 1}}
+          itemSize={{width: '100%', height: 200}}
+          imageProps={{source: {uri: item.mediaUrl}}}
+          title="Title"
+          subtitle="Subitle"
+          overlayText
+        />
+      );
+    } else {
+      return (
+        <Card flex>
+          <Card.Section imageSource={{uri: item.mediaUrl}} imageStyle={styles.itemImage}/>
+          <View padding-s2>
+            <Text $textDefault>{item.name}</Text>
+            <Text $textDefault>{item.formattedPrice}</Text>
+            {item.inventory.status === 'Out of Stock' && (
+              <Text text90M $textDangerLight>
+                {item.inventory.status}
+              </Text>
+            )}
+          </View>
+        </Card>
+      );
+    }
   };
 
   render() {
     return (
-      <GridList<typeof products[0]>
-        ListHeaderComponent={
-          <Text h1 marginB-s5>
-            GridList
-          </Text>
-        }
+      <GridList<(typeof products)[0]>
+        ListHeaderComponent={() => this.renderHeader()}
         data={products}
         renderItem={this.renderItem}
-        // numColumns={2}
-        maxItemWidth={140}
+        numColumns={2}
+        // maxItemWidth={140}
         itemSpacing={Spacings.s3}
         listPadding={Spacings.s5}
         // keepItemSize
