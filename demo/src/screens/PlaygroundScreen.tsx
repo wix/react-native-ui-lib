5e3921b73dfcb75cb1bbfe9d233368f56026cbc1
adids1221 <adids1221@gmail.com>
2024-04-09 10:48:36 +0300

    revert playground screen

--- demo/src/screens/PlaygroundScreen.tsx
+++ demo/src/screens/PlaygroundScreen.tsx
@@ -1,22 +1,20 @@
-import React, {useState} from 'react';
-import {View, TabController, Card, TextField, Button} from 'react-native-ui-lib';
+import React, {Component} from 'react';
+import {View, Text, Card, TextField, Button} from 'react-native-ui-lib';
 
-const MANY_TABS = [
-  {label: 'Challenges2', testID: 'tabItem.challenges'},
-  {label: 'A6', testID: 'tabItem.A6'},
-  {label: 'Challenges3', testID: 'tabItem.challenges3'}
-];
-
-export default function PlaygroundScreen() {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [tabs, setTabs] = useState(MANY_TABS.map(tab => ({...tab, key: tab.label})));
-  const [spreadItems, setSpreadItems] = useState(false);
-
-  return (
-    <View flex padding-page>
-      <TabController asCarousel key={`key`} items={tabs}>
-        <TabController.TabBar selectedIndex={selectedIndex} centerSelected spreadItems={spreadItems} />
-      </TabController>
-    </View>
-  );
+export default class PlaygroundScreen extends Component {
+  render() {
+    return (
+      <View bg-grey80 flex padding-20>
+        <View marginT-20>
+          <TextField migrate placeholder="Placeholder"/>
+        </View>
+        <Card height={100} center padding-20>
+          <Text text50>Playground Screen</Text>
+        </Card>
+        <View flex center>
+          <Button marginV-20 label="Button"/>
+        </View>
+      </View>
+    );
+  }
 }
