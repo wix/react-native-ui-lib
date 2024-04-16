884623846dd7444572ba659585fb14e2ff009897
Nitzan Yizhar <nitzany@wix.com>
2024-04-16 13:25:03 +0300

    changed driver props typing

--- src/testkit/new/Component.driver.ts
+++ src/testkit/new/Component.driver.ts
@@ -1,8 +1,8 @@
 import {ReactTestInstance} from 'react-test-renderer';
-import {RenderResult} from '@testing-library/react-native';
+import {within} from '@testing-library/react-native';
 
 export interface ComponentProps {
-  renderTree: RenderResult;
+  renderTree: ReturnType<typeof within>;
   testID: string;
 }
 
