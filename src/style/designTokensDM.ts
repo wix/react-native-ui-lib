dba5d6cc85cac7d871349fb5c8737eb112c057fe
Inbal Tish <33805983+Inbal-Tish@users.noreply.github.com>
2024-05-09 09:35:06 +0300

    DesignTokens - add $outlineNeutral token (#3062)

--- src/style/designTokensDM.ts
+++ src/style/designTokensDM.ts
@@ -64,6 +64,7 @@ export default {
   $outlineDefault: colorsPalette.grey5,
   $outlineDisabled: colorsPalette.grey20,
   $outlineDisabledHeavy: colorsPalette.grey40,
+  $outlineNeutral: colorsPalette.grey20,
   $outlineNeutralHeavy: colorsPalette.grey40,
   $outlinePrimary: colorsPalette.violet50,
   $outlineGeneral: colorsPalette.blue50,
