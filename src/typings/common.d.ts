d6b93a24605a24d39fdf69cf93e64ef4d22f0c04
Nitzan Yizhar <nitzany@wix.com>
2024-06-02 15:17:21 +0300

    Revert "handled case of assets union (#3060)"
    This reverts commit 2bff5d1c14fd271caa5f5eac924a5413c9fdedde.

--- src/typings/common.d.ts
+++ src/typings/common.d.ts
@@ -10,3 +10,14 @@ export type ExtendTypeWith<T extends Constructor<any>, OtherObject extends objec
 export type Dictionary<TYPE> = {[key: string]: TYPE};
 
 export type ComponentStatics<T> = Pick<T, keyof T>;
+
+type DotExtendedString<T extends string> = `${T}.`;
+export type PathArray<S extends string> = DotExtendedString<S> extends `${infer A}.${infer B}` ?
+  A extends '' ? [] : [A, ...PathArray<B>]
+  : [];
+
+export type RecRecord<Path extends string[], K = {}> = Path extends [infer Next, ...infer Rest] ?
+  {[P in Next] : RecRecord<Rest, K>}
+  : K;
+
+export type PathRecord<T extends string, K = {}> = RecRecord<PathArray<T>, K>;
