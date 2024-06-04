33c508ce0ae36eec8ff716f52c5d3dcabdf94405
Inbal Tish <inbalti@wix.com>
2024-06-04 12:19:04 +0300

    move all driver tests to one file

--- src/components/textField/__tests__/index.driver.spec.tsx
+++ src/components/textField/__tests__/index.driver.spec.tsx
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import {render} from '@testing-library/react-native';
+import Constants from '../../../commons/Constants';
 import View from '../../../components/view';
 import {TextFieldDriver} from '../TextField.driver.new';
 import TextField from '../index';
 import {TextFieldProps} from '../types';
 
 const TEXT_FIELD_TEST_ID = 'text_field_test_id';
+const placeholder = 'Placeholder';
+const hint = 'Hint';
 
 function TestCase(textFieldProps?: TextFieldProps) {
   const [value, setValue] = useState(textFieldProps?.value);
@@ -120,7 +123,7 @@ describe('TextField', () => {
     });
   });
 
-  describe('Label', () => {
+  describe('label', () => {
     it('should not render label if prop is not passed', () => {
       const renderTree = render(<TestCase/>);
       const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
@@ -172,6 +175,78 @@ describe('TextField', () => {
       expect(textFieldDriver.getValidationMessage().exists()).toBe(true);
       expect(textFieldDriver.getValidationMessage().getText()).toEqual('mock message');
     });
+
+    it('should display validation error message when validation fail after blur', () => {
+      const renderTree = render(<TestCase validate={'email'} validationMessage={'email is invalid'} enableErrors validateOnBlur/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      textFieldDriver.changeText('invalidEmail');
+      textFieldDriver.blur();
+      expect(textFieldDriver.getValidationMessage().getText()).toEqual('email is invalid');
+    });
+
+    it('should remove validation error message after entering a valid input', () => {
+      const renderTree = render(<TestCase value={'invalid'} validate={'email'} validationMessage={'email is invalid'} enableErrors validateOnStart validateOnChange/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getValidationMessage().getText()).toEqual('email is invalid');
+      textFieldDriver.changeText('mail@mail.com');
+      expect(textFieldDriver.getValidationMessage().exists()).toEqual(false);
+    });
+  });
+
+  describe('validateOnBlur', () => {
+    it('validate is called with undefined when defaultValue is not given', () => {
+      const renderTree = render(<TestCase validateOnBlur validationMessage={'Not valid'} validate={[validate]}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      textFieldDriver.focus();
+      textFieldDriver.blur();
+      expect(validate).toHaveBeenCalledTimes(1);
+      expect(validate).toHaveBeenCalledWith(undefined);
+    });
+
+    it('validate is called with defaultValue when defaultValue is given', () => {
+      const defaultValue = '1';
+      const renderTree = render(<TestCase validateOnBlur validationMessage={'Not valid'} validate={[validate]} defaultValue={defaultValue}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      textFieldDriver.focus();
+      textFieldDriver.blur();
+      expect(validate).toHaveBeenCalledTimes(1);
+      expect(validate).toHaveBeenCalledWith(defaultValue);
+    });
+  });
+
+  describe('defaultValue', () => {
+    const props = {
+      testID: TEXT_FIELD_TEST_ID,
+      placeholder: 'Placeholder',
+      defaultValue: 'someDefaultValue',
+      value: 'someValue'
+    };
+
+    it('value should equal defaultValue on first render when value not given', () => {
+      const renderTree = render(<TestCase {...props} value={undefined}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getValue()).toEqual('someDefaultValue');
+    });
+
+    it('value should equal value on first render when given', () => {
+      const renderTree = render(<TestCase {...props} defaultValue={undefined}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getValue()).toEqual('someValue');
+    });
+
+    it.each`
+      platform    | isWeb
+      ${'web'}    | ${true}
+      ${'native'} | ${false}
+    `('on $platform should reset defaultValue when prop changed after first render', args => {
+      Constants.isWeb = args.isWeb;
+
+      const renderTree = render(<TestCase {...props} value={undefined}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      
+      renderTree.rerender(<TestCase {...props} value={undefined} defaultValue={'someUpdatedDefaultValue'}/>);
+      expect(textFieldDriver.getValue()).toEqual('someUpdatedDefaultValue');
+    });
   });
 
   describe('char counter', () => {
@@ -221,26 +296,46 @@ describe('TextField', () => {
     });
   });
 
-  describe('validateOnBlur', () => {
-    it('validate is called with undefined when defaultValue is not given', () => {
-      const renderTree = render(<TestCase validateOnBlur validationMessage={'Not valid'} validate={[validate]}/>);
+  describe('hint', () => {
+    it('should hint text replace placeholder when input is focused', () => {
+      const renderTree = render(<TestCase hint={hint} placeholder={placeholder}/>);
       const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getPlaceholder().getText()).toEqual(placeholder);
       textFieldDriver.focus();
-      textFieldDriver.blur();
-      expect(validate).toHaveBeenCalledTimes(1);
-      expect(validate).toHaveBeenCalledWith(undefined);
+      expect(textFieldDriver.getPlaceholder().getText()).toEqual(hint);
     });
 
-    it('validate is called with defaultValue when defaultValue is given', () => {
-      const defaultValue = '1';
-      const renderTree = render(<TestCase validateOnBlur validationMessage={'Not valid'} validate={[validate]} defaultValue={defaultValue}/>);
+    it('should not show hint when hint prop not passed', () => {
+      const renderTree = render(<TestCase placeholder={placeholder}/>);
       const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getPlaceholder().getText()).toEqual(placeholder);
       textFieldDriver.focus();
-      textFieldDriver.blur();
-      expect(validate).toHaveBeenCalledTimes(1);
-      expect(validate).toHaveBeenCalledWith(defaultValue);
+      expect(textFieldDriver.getPlaceholder().getText()).toEqual(placeholder);
     });
   });
+
+  describe('formatter', () => {
+    const priceFormatter = Intl.NumberFormat('en-US');
+
+    const props = {
+      value: '10000',
+      formatter: value => priceFormatter.format(Number(value))
+    };
+
+    it('should format value while not focused based on formatter prop', () => {
+      const renderTree = render(<TestCase {...props}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getValue()).toEqual('10,000');
+    });
+
+    it('should not format value while focused', () => {
+      const renderTree = render(<TestCase {...props}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      textFieldDriver.focus();
+      expect(textFieldDriver.getValue()).toEqual('10000');
+    });
+  });
+
   describe('Mandatory Indication', () => {
     const getTestCaseDriver = (props: TextFieldProps) => {
       const renderTree = render(<TestCase {...props}/>);
@@ -254,6 +349,7 @@ describe('TextField', () => {
       const labelContent = textFieldDriver.getLabel().getText();
       expect(labelContent).toMatch(starReg);
     });
+
     it('Should show mandatory indication on the label', () => {
       const textFieldDriver = getTestCaseDriver({
         label: 'Label',
@@ -263,28 +359,32 @@ describe('TextField', () => {
       const labelContent = textFieldDriver.getLabel().getText();
       expect(labelContent).toMatch(starReg);
     });
+
     it('Should not show mandatory indication on label', () => {
       const textFieldDriver = getTestCaseDriver({label: 'label', showMandatoryIndication: true});
       const labelText = textFieldDriver.getLabel().getText();
       expect(labelText).not.toMatch(starReg);
     });
+
     it('Should not show mandatory indication on label', () => {
       const textFieldDriver = getTestCaseDriver({label: 'label', validate: 'required'});
       const labelText = textFieldDriver.getLabel().getText();
       expect(labelText).not.toMatch(starReg);
     });
+
     it('Should have mandatory on the placeholder', () => {
       const textFieldDriver = getTestCaseDriver({
-        placeholder: 'placeholder',
+        placeholder,
         showMandatoryIndication: true,
         validate: 'required'
       });
       const placeholderText = textFieldDriver.getPlaceholder().getText();
       expect(placeholderText).toMatch(starReg);
     });
+
     it('Should not have any mandatory - 1', () => {
       const textFieldDriver = getTestCaseDriver({
-        placeholder: 'placeholder',
+        placeholder,
         showMandatoryIndication: true,
         // validate: 'required',
         label: 'label'
@@ -294,9 +394,10 @@ describe('TextField', () => {
       expect(placeholderText).not.toMatch(starReg);
       expect(labelText).not.toMatch(starReg);
     });
+
     it('Should not have any mandatory - 2', () => {
       const textFieldDriver = getTestCaseDriver({
-        placeholder: 'placeholder',
+        placeholder,
         // showMandatoryIndication: true,
         validate: 'required',
         label: 'label'
@@ -306,9 +407,10 @@ describe('TextField', () => {
       expect(placeholderText).not.toMatch(starReg);
       expect(labelText).not.toMatch(starReg);
     });
+
     it('Should have mandatory on the floating placeholder', () => {
       const textFieldDriver = getTestCaseDriver({
-        placeholder: 'placeholder',
+        placeholder,
         floatingPlaceholder: true,
         floatOnFocus: true,
         showMandatoryIndication: true,
@@ -321,7 +423,7 @@ describe('TextField', () => {
     // Special cases
     it('Should have mandatory on the label and not on the placeholder', () => {
       const textFieldDriver = getTestCaseDriver({
-        placeholder: 'placeholder',
+        placeholder,
         showMandatoryIndication: true,
         validate: 'required',
         label: 'label'
@@ -330,6 +432,29 @@ describe('TextField', () => {
       const placeholderText = textFieldDriver.getPlaceholder().getText();
       expect(labelText).toMatch(starReg);
       expect(placeholderText).not.toMatch(starReg);
+    });
+  });
+
+  describe('Mandatory Indication', () => {
+    it('Should show mandatory star indication - 1', () => {
+      const renderTree = render(<TestCase testID={'field'} validate={'required'} label={'Label'} showMandatoryIndication/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getLabel().getText()).toEqual('Label*');
+    });
+    it('Should show mandatory star indication - 2', () => {
+      const renderTree = render(<TestCase testID={'field'} validate={['email', 'required']} label={'Label'} showMandatoryIndication/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getLabel().getText()).toEqual('Label*');
+    });
+    it('Should not show mandatory star indication - 1', () => {
+      const renderTree = render(<TestCase testID={'field'} validate={['email', 'required']} label={'Label'}/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getLabel().getText()).not.toEqual('Label*');
+    });
+    it('Should not show mandatory star indication - 2', () => {
+      const renderTree = render(<TestCase testID={'field'} validate={['email']} label={'Label'} showMandatoryIndication/>);
+      const textFieldDriver = TextFieldDriver({renderTree, testID: TEXT_FIELD_TEST_ID});
+      expect(textFieldDriver.getLabel().getText()).not.toEqual('Label*');
     });
   });
 });
