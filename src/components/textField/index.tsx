8115472a003827a206cbf94e166962fb4020dba8
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2024-06-05 10:42:21 +0300

    TextField - apply marginT only to the OUTLINE preset (#3123)
    * TextField - apply marginT only to the OUTLINE preset
    
    * Remove margin completely

--- src/components/textField/index.tsx
+++ src/components/textField/index.tsx
@@ -202,7 +202,7 @@ const TextField = (props: InternalTextFieldProps) => {
           {trailingAccessory}
           {/* </View> */}
         </View>
-        <View row spread marginT-s1>
+        <View row spread>
           {validationMessagePosition === ValidationMessagePosition.BOTTOM && (
             <ValidationMessage
               enableErrors={enableErrors}
