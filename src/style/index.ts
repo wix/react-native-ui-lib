17488bfe74bd09a9cb464539b94f5dcfa51a51b5
Mendy Edri <mendye@wix.com>
2024-07-11 17:02:59 +0300

    fixing missing exported as type

--- src/style/index.ts
+++ src/style/index.ts
@@ -1,7 +1,7 @@
-export {default as Colors} from './colors';
 export {default as DesignTokens} from './designTokens';
+export {default as Colors} from './colors';
 export {default as DesignTokensDM} from './designTokensDM';
-export {default as Scheme, SchemeType, Schemes, SchemeChangeListener} from './scheme';
+export {type default as Scheme, type SchemeType, type Schemes, type SchemeChangeListener} from './scheme';
 export {default as Typography, TypographyKeys} from './typography';
 export {default as BorderRadiuses} from './borderRadiuses';
 export {default as Shadows} from './shadows';
