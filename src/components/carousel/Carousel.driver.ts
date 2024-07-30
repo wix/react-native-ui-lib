684fa1b05b053de8b9d2d7f75ed557ac456d074d
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2024-07-30 14:17:21 +0300

    Remove some old unused drivers (#3190)

--- src/components/carousel/Carousel.driver.ts
+++
@@ -1,7 +0,0 @@
-import {CarouselProps} from './types';
-import {ComponentDriver} from '../../testkit/Component.driver';
-
-export class CarouselDriver extends ComponentDriver<CarouselProps> {
-  getContentOffset = async () => (await this.getElementProps()).contentOffset;
-  scroll = async (delta: number) => (await this.uniDriver.selectorByTestId(this.testID)).scrollX(delta);
-}
