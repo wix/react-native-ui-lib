d1f1054026143ae3a473b2510d5d3271e7f1021b
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2024-09-11 17:00:27 +0300

    Add example to a floating stepper on top of an image

--- demo/src/screens/componentScreens/StepperScreen.tsx
+++ demo/src/screens/componentScreens/StepperScreen.tsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import {View, Text, Stepper, type StepperType} from 'react-native-ui-lib'; //eslint-disable-line
+import {ScrollView} from 'react-native';
+import {View, Text, Stepper, type StepperType, Image} from 'react-native-ui-lib'; //eslint-disable-line
 import {renderMultipleSegmentOptions} from '../ExampleScreenPresenter';
+
+const IMAGE_URL =
+  'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?auto=compress&cs=tinysrgb&w=600&&dpr=1';
 
 export default class StepperScreen extends Component {
   state = {
@@ -22,76 +26,93 @@ export default class StepperScreen extends Component {
     const {stepperValue, stepperType} = this.state;
 
     return (
-      <View padding-page>
-        <Text text40 $textDefault>
-          Stepper
-        </Text>
+      <ScrollView>
+        <View padding-page>
+          <Text text40 $textDefault>
+            Stepper
+          </Text>
 
-        {renderMultipleSegmentOptions.call(this, 'Stepper Type', 'stepperType', [
-          {label: 'Default', value: 'default'},
-          {label: 'Floating', value: 'floating'}
-        ])}
+          {renderMultipleSegmentOptions.call(this, 'Stepper Type', 'stepperType', [
+            {label: 'Default', value: 'default'},
+            {label: 'Floating', value: 'floating'}
+          ])}
 
-        <View centerV marginT-s3>
-          <View row spread centerV>
-            <Text text70 $textDefault>
-              Default
-            </Text>
-            <Stepper type={stepperType}/>
-          </View>
-
-          <View row spread centerV marginT-30>
-            <Text text70 $textDefault>
-              Disabled
-            </Text>
-            <Stepper disabled type={stepperType}/>
-          </View>
-
-          <View row spread marginT-30>
-            <Text text70 $textDefault>
-              Step (0.5)
-            </Text>
-            <Stepper step={0.5} type={stepperType}/>
-          </View>
-
-          <View row spread marginT-30>
-            <Text text70 $textDefault>
-              Small
-            </Text>
-            <Stepper small type={stepperType}/>
-          </View>
-
-          <View marginT-30>
+          <View centerV marginT-s3>
             <View row spread centerV>
               <Text text70 $textDefault>
-                Custom
+                Default
               </Text>
-              <Stepper
-                type={stepperType}
-                onValueChange={this.onValueChange}
-                maxValue={this.stepperProps.maxValue}
-                minValue={this.stepperProps.minValue}
-                value={stepperValue}
-                testID={'Stepper1'}
-              />
+              <Stepper type={stepperType}/>
             </View>
-            <View padding-5>
-              <Text text80M $textDefault>
-                Stepper value: {stepperValue}
+
+            <View row spread centerV marginT-30>
+              <Text text70 $textDefault>
+                Disabled
               </Text>
-              <Text $textDefault marginT-3>
-                Initial value: {this.stepperProps.value}
+              <Stepper disabled type={stepperType}/>
+            </View>
+
+            <View row spread marginT-30>
+              <Text text70 $textDefault>
+                Step (0.5)
               </Text>
-              <Text $textDefault marginT-3>
-                Min value: {this.stepperProps.minValue}
+              <Stepper step={0.5} type={stepperType}/>
+            </View>
+
+            <View row spread marginT-30>
+              <Text text70 $textDefault>
+                Small
               </Text>
-              <Text $textDefault marginT-3>
-                Max value: {this.stepperProps.maxValue}
-              </Text>
+              <Stepper small type={stepperType}/>
+            </View>
+
+            <View marginT-30>
+              <View row spread centerV>
+                <Text text70 $textDefault>
+                  Custom
+                </Text>
+                <Stepper
+                  type={stepperType}
+                  onValueChange={this.onValueChange}
+                  maxValue={this.stepperProps.maxValue}
+                  minValue={this.stepperProps.minValue}
+                  value={stepperValue}
+                  testID={'Stepper1'}
+                />
+              </View>
+              <View padding-5>
+                <Text text80M $textDefault>
+                  Stepper value: {stepperValue}
+                </Text>
+                <Text $textDefault marginT-3>
+                  Initial value: {this.stepperProps.value}
+                </Text>
+                <Text $textDefault marginT-3>
+                  Min value: {this.stepperProps.minValue}
+                </Text>
+                <Text $textDefault marginT-3>
+                  Max value: {this.stepperProps.maxValue}
+                </Text>
+              </View>
             </View>
           </View>
+
+          <View marginT-s5 centerH>
+            <Text text70BO marginB-s2>
+              Floating Stepper
+            </Text>
+            <Image
+              source={{uri: IMAGE_URL}}
+              style={{width: 200, height: 200, borderRadius: 8, overflow: 'hidden'}}
+              customOverlayContent={
+                <View style={{position: 'absolute', top: 8, right: 8}}>
+                  <Stepper type="floating"/>
+                </View>
+              }
+            />
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
