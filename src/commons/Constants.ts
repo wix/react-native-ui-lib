0058cbefd24ad5e9ddd932c44b4c0baf80355a9e
yovelovadia123 <yovelo@wix.com>
2024-09-12 10:20:11 +0300

    Make Constants.statusBarHeight dynamic on phone orientation change
    Issue:
    When starting IOS application in landscape mode and then changing to portrait statusBarHeight value wont change and remain 0.
    
    Fix:
    Instead of using StatusBarManager.HEIGHT to use StatusBarManager.getHeight() method which provides the right height when called (HEIGHT is declared once when the app initialized)

--- src/commons/Constants.ts
+++ src/commons/Constants.ts
@@ -40,11 +40,11 @@ isTablet = Platform.isPad || (getAspectRatio() < 1.6 && Math.max(screenWidth, sc
 function setStatusBarHeight() {
   const {StatusBarManager} = NativeModules;
   statusBarHeight = StatusBarManager?.HEIGHT || 0; // So there will be a value for any case
-  // statusBarHeight = isIOS ? 20 : StatusBarManager.HEIGHT;
-  // if (isIOS) {
-  //   // override guesstimate height with the actual height from StatusBarManager
-  //   StatusBarManager.getHeight((data: any) => (statusBarHeight = data.height));
-  // }
+  
+  if (isIOS) {
+    // override guesstimate height with the actual height from StatusBarManager
+    StatusBarManager.getHeight((data: any) => (statusBarHeight = data.height));
+  }
 }
 
 function getAspectRatio() {
