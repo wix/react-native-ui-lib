7c0771436ddbcc1a313c80d3f6f524aef8487ed1
M-i-k-e-l <michaelle@wix.com>
2024-09-24 22:05:19 +0300

    Add Carousel

--- docuilib/src/theme/ReactLiveScope/index.js
+++ docuilib/src/theme/ReactLiveScope/index.js
@@ -5,8 +5,10 @@ import {BorderRadiuses, Button, Image, Spacings, Text, TouchableOpacity, View} f
 import ActionBar from 'react-native-ui-lib/actionBar';
 import Assets from 'react-native-ui-lib/assets';
 import Card from 'react-native-ui-lib/card';
+import Carousel from 'react-native-ui-lib/carousel';
 import Checkbox from 'react-native-ui-lib/checkbox';
 import Chip from 'react-native-ui-lib/chip';
+import Constants from 'react-native-ui-lib/constants';
 import Icon from 'react-native-ui-lib/icon';
 import Incubator from 'react-native-ui-lib/incubator';
 import MaskedInput from 'react-native-ui-lib/maskedInput';
@@ -48,8 +50,10 @@ const ReactLiveScope = {
   BorderRadiuses,
   Button,
   Card,
+  Carousel,
   Checkbox,
   Chip,
+  Constants,
   Colors,
   Icon,
   Image,
