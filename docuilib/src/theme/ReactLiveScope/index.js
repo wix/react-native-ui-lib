35e724204200c278f404fe283ea0e22ecad16558
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2024-09-25 09:59:16 +0300

    Live Code - support more components 1 (#3269)
    * Support live code for components
    
    * Add ActionBar
    
    * Add Checkbox
    
    * Add Chip
    
    * Add RadioButton
    
    * Add RadioGroup
    
    * Remove (regular) Slider
    
    * Add Switch

--- docuilib/src/theme/ReactLiveScope/index.js
+++ docuilib/src/theme/ReactLiveScope/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import {Colors} from 'react-native-ui-lib/style';
 import {Button, Image, Text, TouchableOpacity, View} from 'react-native-ui-lib/core';
-import TextField from 'react-native-ui-lib/textField';
+import ActionBar from 'react-native-ui-lib/actionBar';
+import Checkbox from 'react-native-ui-lib/checkbox';
+import Chip from 'react-native-ui-lib/chip';
+import RadioButton from 'react-native-ui-lib/radioButton';
+import RadioGroup from 'react-native-ui-lib/radioGroup';
 import SegmentedControl from 'react-native-ui-lib/segmentedControl';
+import Switch from 'react-native-ui-lib/switch';
+import TextField from 'react-native-ui-lib/textField';
 
 // Add react-live imports you need here
 const ReactLiveScope = {
   React,
   ...React,
   /* uilib components */
+  ActionBar,
   Button,
+  Checkbox,
+  Chip,
   Colors,
   Image,
+  RadioButton,
+  RadioGroup,
   SegmentedControl,
+  Switch,
   Text,
   TextField,
   TouchableOpacity,
