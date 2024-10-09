48590379ab03d5698c836a9dc5370be9ce151bec
Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>
2024-10-09 16:59:21 +0300

    fix tests by adding uilib-native to transformIgnorePatterns

--- jestSetup/jest-setup.js
+++ jestSetup/jest-setup.js
@@ -21,17 +21,6 @@ global._UILIB_TESTING = true;
 jest.spyOn(AccessibilityInfo, 'isScreenReaderEnabled').mockImplementation(() => Promise.resolve(false));
 
 // mock native modules
-jest.mock('uilib-native', () => {
-  return {
-    Keyboard: {
-      KeyboardTrackingView: jest.fn(),
-      KeyboardAwareInsetsView: jest.fn(),
-      KeyboardRegistry: jest.fn(),
-      KeyboardAccessoryView: jest.fn(),
-      KeyboardUtils: jest.fn()
-    }
-  };
-});
 jest.mock('@react-native-community/blur', () => {});
 jest.mock('@react-native-community/netinfo', () => {});
 // TODO: Adding here a todo for package.json: need to remove moduleNameMapper, see this: https://github.com/software-mansion/react-native-reanimated/issues/1196
