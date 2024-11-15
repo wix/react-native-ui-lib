75136e4c124ef2a307540594267983ba4c4f9302
Nitzan Yizhar <nitzany@wix.com>
2024-11-15 15:31:26 +0200

    fixed optional dep CssUri

--- src/optionalDependencies/SvgPackage.ts
+++ src/optionalDependencies/SvgPackage.ts
@@ -1,6 +1,14 @@
 let SvgPackage: any;
 try {
   SvgPackage = require('react-native-svg');
+  let SvgCssUri;
+  try {
+    SvgCssUri = require('react-native-svg/css').SvgCssUri;
+  } catch (error) {
+  }
+  if (SvgCssUri) {
+    SvgPackage.SvgCssUri = SvgCssUri;
+  }
   // Sadly we cannot verify with require('react-native-svg-transformer');
 } catch (error) {}
 
