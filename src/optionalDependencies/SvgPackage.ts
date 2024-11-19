c6369d9e48fd0c5be03364d88ad50931d820b59c
Nitzan Yizhar <nitzany@wix.com>
2024-11-19 10:31:48 +0200

    Fix/react native svg cssuri path (#3394)
    * fixed optional dep CssUri
    
    * make it more readable
    
    * changed the way cssuri is exported so it won't trigger the error
    
    * updated react-native-svg version to ^15.2.0
    
    * updated react-native-svg version to 15.2.0 (exact)

--- src/optionalDependencies/SvgPackage.ts
+++ src/optionalDependencies/SvgPackage.ts
@@ -1,7 +1,10 @@
 let SvgPackage: any;
+let svgCssForPackage14Higher: any;
 try {
   SvgPackage = require('react-native-svg');
+  svgCssForPackage14Higher = require('react-native-svg/css');
   // Sadly we cannot verify with require('react-native-svg-transformer');
 } catch (error) {}
 
+export const SvgCssUri = svgCssForPackage14Higher?.SvgCssUri ?? SvgPackage?.SvgCssUri;
 export default SvgPackage;
