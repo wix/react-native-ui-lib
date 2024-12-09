2621435e75b5f251b61b760e2d27aeeafb8f0aa3
adids1221 <adids1221@gmail.com>
2024-12-09 14:02:49 +0200

    usage section should show LivePreview or CodeBlock

--- docuilib/src/components/pageComponents/Usage.tsx
+++ docuilib/src/components/pageComponents/Usage.tsx
@@ -2,14 +2,18 @@ import _ from 'lodash';
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import '../ComponentPage.module.scss';
+import ReactLiveScope from '../../theme/ReactLiveScope';
+import UILivePreview from '../UILivePreview';
 
 export const Usage = ({component}) => {
+  const componentLivePlaygroundSupport = !!ReactLiveScope[component.name];
+  const code = component.snippet?.map(item => _.replace(item, new RegExp(/\$[1-9]/, 'g'), '')).join('\n');
   if (component.snippet) {
-    return (
+    return componentLivePlaygroundSupport ? (
+      <UILivePreview code={code}/>
+    ) : (
       <div>
-        <CodeBlock language="jsx">
-          {component.snippet?.map(item => _.replace(item, new RegExp(/\$[1-9]/, 'g'), '')).join('\n')}
-        </CodeBlock>
+        <CodeBlock language="jsx">{code}</CodeBlock>
       </div>
     );
   }
