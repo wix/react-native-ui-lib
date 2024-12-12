46c1d1af02d51f98eb957f2198d7c72118b87ef4
Inbal Tish <inbalti@wix.com>
2024-12-12 11:29:02 +0200

    replace accessibilityRole with role and accessibilityLabel

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -85,8 +85,9 @@ class Switch extends Component<SwitchProps> {
 
     return {
       accessible: true,
-      accessibilityRole: 'switch',
-      accessibilityLabel: value ? '1' : '0',
+      role: 'switch',
+      accessibilityLabel: 'switch',
+      accessibilityValue: {text: value ? '1' : '0'},
       accessibilityState: {
         disabled,
         checked: value ? 'checked' : 'unchecked'
