b7c9e8068a37b8a2e7469bf229f964320806f642
Nitzan Yizhar <nitzany@wix.com>
2024-12-26 14:23:15 +0200

    Fixed divider width

--- src/components/piechart/PartialCircle.tsx
+++ src/components/piechart/PartialCircle.tsx
@@ -13,7 +13,7 @@ type PartialCircleProps = {
 };
 
 const DEFAULT_DIVIDER_COLOR = '#FFFFFF';
-const DEFAULT_DIVIDER_WIDTH = 2;
+const DEFAULT_DIVIDER_WIDTH = 4;
 const DEFAULT_PADDING = 0;
 
 const PartialCircle = ({percentage, radius, color, startAngle = 0, padding = DEFAULT_PADDING}: PartialCircleProps) => {
