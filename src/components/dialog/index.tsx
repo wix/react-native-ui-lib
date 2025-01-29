524c95a2c549d4901e8ed03bee307299a087a870
M-i-k-e-l <michaelle@wix.com>
2025-01-29 15:28:59 +0200

    Promote PanView and hooks from Incubator to standard components

--- src/components/dialog/index.tsx
+++ src/components/dialog/index.tsx
@@ -22,7 +22,7 @@ import {asBaseComponent, Constants} from '../../commons/new';
 import View from '../../components/view';
 import Modal from '../../components/modal';
 import {extractAlignmentsValues} from '../../commons/modifiers';
-import useHiddenLocation from '../../incubator/hooks/useHiddenLocation';
+import useHiddenLocation from '../../hooks/useHiddenLocation';
 import DialogHeader from './DialogHeader';
 import useDialogContent from './useDialogContent';
 import {DialogProps, DialogDirections, DialogDirectionsEnum, DialogHeaderProps} from './types';
