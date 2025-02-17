2ec4efe71d43b09e89d2cd8d793e3c489c2478b5
Inbal Tish <inbalti@wix.com>
2025-02-17 12:15:16 +0200

    Switch - fix accessibility

--- src/components/switch/index.tsx
+++ src/components/switch/index.tsx
@@ -83,15 +83,10 @@ class Switch extends Component<SwitchProps> {
   getAccessibilityProps() {
     const {disabled, value} = this.props;
 
-
     return {
       accessible: true,
       accessibilityRole: 'switch',
-      accessibilityState: {
-        disabled,
-        checked: value ? 'checked' : 'unchecked'
-      },
-      accessibilityValue: {text: value ? '1' : '0'}
+      accessibilityState: {disabled, checked: value}
     };
   }
 
