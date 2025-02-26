b2a6e43fae79bc75cd558fb585f4c394093511de
Nitzan Yizhar <nitzany@wix.com>
2025-02-26 14:08:53 +0200

    Revert old slider index changes

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -56,7 +56,7 @@ const defaultProps = {
   minimumValue: 0,
   maximumValue: 1,
   step: 0,
-  thumbHitSlop: {top: 12, bottom: 12, left: 24, right: 24},
+  thumbHitSlop: {top: 10, bottom: 10, left: 24, right: 24},
   useGap: true
 };
 
