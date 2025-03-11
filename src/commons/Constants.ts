b84282967af4aa5c02a62870c82842177e320047
Nitzan Yizhar <nitzany@wix.com>
2025-03-11 14:24:52 +0200

    Made fontScale to getFontScale

--- src/commons/Constants.ts
+++ src/commons/Constants.ts
@@ -194,7 +194,7 @@ const constants = {
   enterKey: 'Enter',
 
   /* Font scale */
-  fontScale: () => PixelRatio.getFontScale()
+  getFontScale: () => PixelRatio.getFontScale()
 };
 
 setStatusBarHeight();
