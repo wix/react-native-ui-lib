0950f3e554d06be139143b5d22d227fed7ac11c7
Adi Mordo <adids1221@gmail.com>
2025-03-12 07:05:15 +0200

    Usage component support live preview for compatible components (#3581)
    * Usage component support live preview for compatible components
    
    * Enhance UILivePreview to support codeBlock when react live scope not supported
    
    * Refactor UILivePreview component to use destructuring for props
    
    * Add default value for componentName prop in UILivePreview
    
    ---------
    
    Co-authored-by: Ethan Sharabi <1780255+ethanshar@users.noreply.github.com>

--- docuilib/src/components/pageComponents/Usage.tsx
+++ docuilib/src/components/pageComponents/Usage.tsx
@@ -2,15 +2,18 @@ import _ from 'lodash';
 import React from 'react';
 import CodeBlock from '@theme/CodeBlock';
 import '../ComponentPage.module.scss';
+import ReactLiveScope from '../../theme/ReactLiveScope';
+import UILivePreview from '../UILivePreview';
 
 export const Usage = ({component}) => {
+  const supportedComponentNames = Object.keys(ReactLiveScope);
+  const componentLivePlaygroundSupport = supportedComponentNames.includes(component.name);
   if (component.snippet) {
-    return (
-      <div>
-        <CodeBlock language="jsx">
-          {component.snippet?.map(item => _.replace(item, new RegExp(/\$[1-9]/, 'g'), '')).join('\n')}
-        </CodeBlock>
-      </div>
+    const code = component.snippet.map(item => _.replace(item, new RegExp(/\$[1-9]/, 'g'), '')).join('\n');
+    return componentLivePlaygroundSupport ? (
+      <UILivePreview code={code} liveScopeSupport/>
+    ) : (
+      <CodeBlock language="jsx">{code}</CodeBlock>
     );
   }
 };
