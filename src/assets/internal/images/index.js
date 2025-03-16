1224520fc31baf6753374d2c662e0e20ea1b8ef9
adids1221 <adids1221@gmail.com>
2025-03-16 14:44:30 +0200

    Add gradient overlay images and update Overlay component to use internal assets

--- src/assets/internal/images/index.js
+++ src/assets/internal/images/index.js
@@ -1,5 +1,14 @@
 export const images = {
   get gradient() {
     return require('./gradient.png');
+  },
+  get gradientOverlayHigh() {
+    return require('./gradientOverlayHigh.png');
+  },
+  get gradientOverlayLow() {
+    return require('./gradientOverlayLow.png');
+  },
+  get gradientOverlayMedium() {
+    return require('./gradientOverlayMedium.png');
   }
 };
