3c4dc791b75d4ffc3f644f88766181334f3cc1e2
adids1221 <adids1221@gmail.com>
2025-03-18 16:57:22 +0200

    Remove unnecessary null return in renderLabel

--- src/components/picker/PickerSelectionStatusToolbar.tsx
+++ src/components/picker/PickerSelectionStatusToolbar.tsx
@@ -80,7 +80,6 @@ export default function PickerSelectionStatusToolbar(props: PickerSelectionStatu
     if (showLabel) {
       return <Text>{label}</Text>;
     }
-    return null;
   };
 
   return (
