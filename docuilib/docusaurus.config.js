35e89de71261e134809242b54cc39af268e79c4d
Inbal Tish <inbalti@wix.com>
2025-03-19 13:47:21 +0200

    add custom footer

--- docuilib/docusaurus.config.js
+++ docuilib/docusaurus.config.js
@@ -120,45 +120,6 @@ const darkCodeTheme = themes.dracula;
           ]
         },
         footer: {
-          links: [
-            {
-              html: `
-                <a href="/docs/getting-started/setup" target="_blank" rel="noreferrer noopener" aria-label="Go to docs">
-                  <img src="https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/mads-docs-assets/assets/site/logo_rnui_footer.png" alt="rnui logo" width="139" height="38" />
-                </a>
-              `
-            },
-            {
-              html: `
-                <a herf="https://github.com/wix/react-native-ui-lib" target="_blank" rel="noreferrer noopener" aria-label="Open gitHub">
-                  <span>GitHub</span>
-                  <image src="https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/mads-docs-assets/assets/site/externalSmall.png" alt="external link icon" width="17" height="16" />
-                </a>
-              `
-            },
-            {
-              html: `
-                <a herf="https://snack.expo.io/@ethanshar/rnuilib_snack?platform=ios&supportedPlatforms=ios,android" target="_blank" rel="noreferrer noopener" aria-label="Open expo snack">
-                  <span>Expo-Snack</span>
-                  <image src="https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/mads-docs-assets/assets/site/externalSmall.png" alt="external link icon" width="17" height="16" />
-                </a>
-              `
-            },
-            {
-              html: `
-                <a href="https://discord.gg/2eW4g6Z" target="_blank" rel="noreferrer noopener" aria-label="Open discord">
-                  <img src="https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/mads-docs-assets/assets/site/discord.png" alt="Discord icon" width="28" height="28" />
-                </a>
-              `
-            },
-            {
-              html: `
-                <a href="https://twitter.com/rnuilib" target="_blank" rel="noreferrer noopener" aria-label="Open x">
-                  <img src="https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/mads-docs-assets/assets/site/xSocial.png" alt="x icon" width="28" height="28" />
-                </a>
-              `
-            }
-          ],
           copyright: `© 2006-${new Date().getFullYear()} Wix.com, Inc.`
         },
         prism: {
