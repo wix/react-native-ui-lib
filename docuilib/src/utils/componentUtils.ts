74ff923952765e1a8e726e9e3a01c48bf600f763
adids1221 <adids1221@gmail.com>
2025-03-27 15:05:21 +0200

    Refactor component support check to use utility function for improved clarity

--- docuilib/src/utils/componentUtils.ts
+++ docuilib/src/utils/componentUtils.ts
@@ -1,8 +1,6 @@
+import get from 'lodash/get';
 import ReactLiveScope from '../theme/ReactLiveScope';
 
-const supportedComponentNames = Object.keys(ReactLiveScope);
-
 export const isComponentSupported = componentName => {
-  const _componentName = componentName.includes('.') ? componentName.split('.')[0] : componentName;
-  return supportedComponentNames.includes(_componentName);
+  return !!get(ReactLiveScope, componentName);
 };
