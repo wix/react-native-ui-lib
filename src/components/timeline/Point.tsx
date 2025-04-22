0effbaf45601877979a2fb1f9e6b1150f6061529
adids1221 <adids1221@gmail.com>
2025-04-22 09:25:13 +0300

    TimeLine Point - text and icon relative color

--- src/components/timeline/Point.tsx
+++ src/components/timeline/Point.tsx
@@ -1,4 +1,3 @@
-
 import React, {useMemo} from 'react';
 import {StyleSheet, LayoutChangeEvent} from 'react-native';
 import {Colors, Spacings} from '../../style';
@@ -37,18 +36,29 @@ const Point = (props: PointPropsInternal) => {
       {borderWidth: OUTLINE_WIDTH, borderColor: color && Colors.getColorTint(color, OUTLINE_TINT)};
     const circleStyle = !hasContent && isCircle && 
       {backgroundColor: 'transparent', borderWidth: CIRCLE_WIDTH, borderColor: color};
-    
+
     return [styles.point, pointSizeStyle, !removeIconBackground && pointColorStyle, outlineStyle, circleStyle];
   }, [type, color, label, removeIconBackground, icon]);
 
   const renderPointContent = () => {
     const {removeIconBackground} = props;
-    const tintColor = removeIconBackground ? Colors.$iconDefault : Colors.$iconDefaultLight;
+    const tintColor = removeIconBackground
+      ? Colors.$iconDefault
+      : color
+        ? Colors.getRelativeContentColor(color)
+        : Colors.$iconDefaultLight;
+    const textColor = color
+      ? Colors.getRelativeContentColor(color) || Colors.$textDefaultLight
+      : Colors.$textDefaultLight;
     const iconSize = removeIconBackground ? undefined : ICON_SIZE;
     if (icon) {
       return <Icon tintColor={tintColor} {...iconProps} size={iconSize} source={icon}/>;
     } else if (label) {
-      return <Text recorderTag={'unmask'} $textDefaultLight subtextBold>{label}</Text>;
+      return (
+        <Text recorderTag={'unmask'} color={textColor} subtextBold>
+          {label}
+        </Text>
+      );
     }
   };
 
