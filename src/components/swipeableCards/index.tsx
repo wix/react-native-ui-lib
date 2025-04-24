fa45d075a4a415b6213369a9d56688d0020fac65
Avishai Nudelman <avishain@wix.com>
2025-04-24 11:31:31 +0300

    Swipeable cards

---
+++ src/components/swipeableCards/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import SwipeableCard, {type SwipeableCardProps} from './SwipeableCard';
+import {useSharedValue} from 'react-native-reanimated';
+import View from '../view';
+
+interface SwipeableCardsViewProps<T> {
+  currentItem: T;
+  nextItems: T[];
+  onSwipe?: SwipeableCardProps['onSwipe'];
+  currentIndex: number;
+  renderPlaceHolderCard: () => JSX.Element;
+  renderRightCard: () => JSX.Element;
+  renderLeftCard: () => JSX.Element;
+  renderMainCard: () => JSX.Element;
+}
+
+const SwipeableCardsView = (props: SwipeableCardsViewProps<any>) => {
+  const {
+    currentItem, nextItems, onSwipe, currentIndex, renderPlaceHolderCard, 
+    renderRightCard, renderLeftCard, renderMainCard
+  } = props;
+
+  const animatedValue = useSharedValue(0);
+
+  return (
+    <View flex>
+      {nextItems.map((nextItem, index) => (
+        nextItem && (
+          <SwipeableCard
+            key={index}
+            {...{onSwipe, animatedValue, currentIndex}} 
+            currentIndex={currentIndex} 
+            itemIndex={currentIndex + index + 1}
+          >
+            {renderPlaceHolderCard()}
+          </SwipeableCard>
+        )
+      ))}
+      <SwipeableCard
+        {...{onSwipe, animatedValue}}
+        currentIndex={currentIndex}
+        itemIndex={currentIndex}
+        key={currentItem?.id}
+        cardContent={{
+          right: renderRightCard(),
+          left: renderLeftCard()
+        }}
+      >
+        <View flex>
+          {renderMainCard()}
+        </View>
+      </SwipeableCard>
+    </View>
+
+  );
+};
+
+export default SwipeableCardsView;
