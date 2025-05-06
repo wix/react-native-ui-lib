74acebcb0092bc92418453365995634fddc05823
adids1221 <adids1221@gmail.com>
2025-05-06 15:05:10 +0300

    removed the todo comments

--- src/components/chip/chip.driver.ts
+++ src/components/chip/chip.driver.ts
@@ -5,12 +5,12 @@ import {ImageDriver} from '../image/Image.driver.new';
 
 export const ChipDriver = (props: ComponentProps) => {
   const driver = useComponentDriver(props);
-  //TODO: add driver for avatar and badge
+
   const labelDriver = TextDriver({
     renderTree: props.renderTree,
     testID: `${props.testID}.label`
   });
-  //TODO: Make dismissIcon pressable and add driver
+
   const dismissIconDriver = ImageDriver({
     renderTree: props.renderTree,
     testID: `${props.testID}.dismissIcon`
