ec09d65d74536c871cbb194cc68374f13adcf19b
Inbal Tish <inbalti@wix.com>
2025-06-10 11:18:49 +0300

    fix types

--- lib/components/SafeArea/SafeAreaSpacerViewNativeComponent.ts
+++ lib/components/SafeArea/SafeAreaSpacerViewNativeComponent.ts
@@ -1,6 +1,10 @@
 import type {ViewProps} from 'react-native';
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 
-// SafeAreaSpacerView doesn't need additional props beyond basic ViewProps
-// The native implementation handles safe area insets automatically
-export default codegenNativeComponent<ViewProps>('SafeAreaSpacerView');
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface NativeProps extends ViewProps {
+  // SafeAreaSpacerView doesn't need additional props beyond basic ViewProps
+  // The native implementation handles safe area insets automatically
+}
+
+export default codegenNativeComponent<NativeProps>('SafeAreaSpacerView');
