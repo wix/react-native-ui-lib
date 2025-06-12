1035645303ef172ad436df4bc2670699faa6d08f
M-i-k-e-l <michaelle@wix.com>
2025-06-12 15:22:25 +0300

    More testing

--- scripts/testSteps.js
+++ scripts/testSteps.js
@@ -1,2 +1,5 @@
 console.log('---------------', process.env.BUILDKITE_MESSAGE, process.env.BUILDKITE_BRANCH);
+console.log('---------------', 'isMaster', process.env.BUILDKITE_BRANCH === 'master');
+console.log('---------------', 'isRelease', process.env.BUILDKITE_BRANCH === 'release');
+console.log('---------------', 'isSnapshot', process.env.BUILDKITE_MESSAGE === 'snapshot');
 throw new Error('test');
