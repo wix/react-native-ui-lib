8ab439800bad521217a5489485321959dc23cffe
sridhar <mailtosridhar01@gmail.com>
2025-06-25 17:25:26 +0000

    Refactor sUIUpdateClosure to use lambda expression for improved readability and performance

--- lib/android/src/main/java/com/wix/reactnativeuilib/keyboardinput/utils/RuntimeUtils.java
+++ lib/android/src/main/java/com/wix/reactnativeuilib/keyboardinput/utils/RuntimeUtils.java
@@ -6,10 +6,14 @@ import com.wix.reactnativeuilib.keyboardinput.ReactContextHolder;
 public class RuntimeUtils {
 
     // TODO Switch to GuardedRunnable when upgrading RN's minimal ver
-    private static final Runnable sUIUpdateClosure = new Runnable() {
-        @Override
-        public void run() {
-            ReactContextHolder.getContext().getNativeModule(UIManagerModule.class).onBatchComplete();
+    private static final Runnable sUIUpdateClosure = () -> {
+        try{
+            UIManagerModule uiManager = ReactContextHolder.getContext().getNativeModule(UIManagerModule.class);
+            if(uiManager != null){
+                uiManager.onBatchComplete();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
         }
     };
 
