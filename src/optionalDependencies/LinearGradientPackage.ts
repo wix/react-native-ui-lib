fc02adc65cfca39e2344dbd08ce400c2b553afb8
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2025-09-03 13:34:54 +0300

    Incubator.Gradient - new component (#3795)
    * Incubator.Gradient - new component
    
    * Oops
    
    * Revert LinearGradientPackage change
    
    * Improve screen

--- src/optionalDependencies/LinearGradientPackage.ts
+++ src/optionalDependencies/LinearGradientPackage.ts
@@ -1,4 +1,4 @@
-let LinearGradientPackage: any;
+let LinearGradientPackage: typeof import('react-native-linear-gradient') | undefined;
 try {
   LinearGradientPackage = require('react-native-linear-gradient');
 } catch (error) {}
