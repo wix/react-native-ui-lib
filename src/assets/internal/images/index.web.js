62bca79c9f84c55676aaa2b7740497acf305f1f0
adids1221 <adids1221@gmail.com>
2025-11-20 11:00:59 +0200

    update: image URIs in index.web.js to use external links for gradient overlays

--- src/assets/internal/images/index.web.js
+++ src/assets/internal/images/index.web.js
@@ -3,16 +3,32 @@ export const images = {
     return {uri: require('./gradient.png'), width: 56, height: 2};
   },
   get gradientOverlay() {
-    return {uri: require('./gradientOverlay.png'), width: 76, height: 48};
+    return {
+      uri: 'https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/one-app-assets/public/v1/images/gradientOverlay.png',
+      width: 76,
+      height: 48
+    };
   },
   get gradientOverlayHigh() {
-    return {uri: require('./gradientOverlayHigh.png'), width: 1, height: 297};
+    return {
+      uri: 'https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/one-app-assets/public/v1/images/gradientOverlayHigh.png',
+      width: 1,
+      height: 297
+    };
   },
   get gradientOverlayLow() {
-    return {uri: require('./gradientOverlayLow.png'), width: 1, height: 297};
+    return {
+      uri: 'https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/one-app-assets/public/v1/images/gradientOverlayLow.png',
+      width: 1,
+      height: 297
+    };
   },
   get gradientOverlayMedium() {
-    return {uri: require('./gradientOverlayMedium.png'), width: 1, height: 297};
+    return {
+      uri: 'https://wixmp-1d257fba8470f1b562a0f5f2.wixmp.com/one-app-assets/public/v1/images/gradientOverlayMedium.png',
+      width: 1,
+      height: 297
+    };
   },
   get hintTipMiddle() {
     return {uri: require('./hintTipMiddle.png'), width: 20, height: 7};
