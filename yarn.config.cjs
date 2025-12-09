7e23c90fac2d65c214a1b5cb5033c84f56131122
Miki Leib <38354019+M-i-k-e-l@users.noreply.github.com>
2025-12-09 16:09:15 +0200

    Check yarnrc for unwanted dep (#3855)

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -2,18 +2,29 @@ const fs = require('fs');
 const {defineConfig} = require(`@yarnpkg/types`);
 const {logError} = require('./scripts/utils');
 
-function checkYarnLock() {
-  const yarnLock = fs.readFileSync('./yarn.lock', 'utf8');
-  const matches = yarnLock.match(/npm.dev/);
+const UNWANTED_DEPENDENCIES = 'npm.dev';
+
+function checkUnwantedDependencies(file) {
+  const fileContent = fs.readFileSync(file, 'utf8');
+  const matches = fileContent.match(new RegExp(`${UNWANTED_DEPENDENCIES}`, 'g'));
 
   if (matches !== null) {
-    logError('Yarn lock contains unwanted dependencies');
+    logError('Unwanted dependencies found in ' + file);
     process.exit(1);
   }
+}
+
+function checkYarnLock() {
+  checkUnwantedDependencies('./yarn.lock');
+}
+
+function checkYarnRc() {
+  checkUnwantedDependencies('./.yarnrc.yml');
 }
 
 module.exports = defineConfig({
   constraints: async () => {
     checkYarnLock();
+    checkYarnRc();
   }
 });
