949293b7d54f46ca7706c267debeb05f860decbf
adids1221 <adids1221@gmail.com>
2025-12-28 17:08:07 +0200

    Revert "Update release script to include --legacy-peer-deps (#3894)"
    This reverts commit ff1b9d9c1d072c770102928976d9c300fb4811ce.

--- packages/react-native-ui-lib/scripts/release/release.js
+++ packages/react-native-ui-lib/scripts/release/release.js
@@ -89,10 +89,8 @@ function tryPublishAndTag(version) {
 
 function tagAndPublish(newVersion) {
   console.log(`trying to publish ${newVersion}...`);
-  // Add --legacy-peer-deps here to bypass the ERESOLVE error
-  exec.execSync(`npm --no-git-tag-version version ${newVersion} --legacy-peer-deps`);
-  // It is also safer to add it to the publish command
-  exec.execSync(`npm publish --tag ${VERSION_TAG} --legacy-peer-deps`);
+  exec.execSync(`npm --no-git-tag-version version ${newVersion}`);
+  exec.execSync(`npm publish --tag ${VERSION_TAG}`);
   if (isRelease) {
     exec.execSync(`git tag -a ${newVersion} -m "${newVersion}"`);
   }
